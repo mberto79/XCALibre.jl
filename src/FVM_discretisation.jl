09465cf7529ae0220e329fa890136982c67b92c9
Humberto <h.medina@aerofluids.org>
2022-02-19 00:18:31 +0000

    clean non macro code

--- src/FVM_discretisation.jl
+++ src/FVM_discretisation.jl
@@ -1,6 +1,7 @@
 # Discretisation schemes and equation terms (types)
 export Linear, Constant, Source, ScalarField, Equation, discretise!,
 aP!, aN!, b!,
+
 Δ, @defineEqn, @discretise, Mesh, apply_boundary_conditions!, clear!, clearAll!, solve!
 
 function generalDiscretise!(type, ϕ, J)
@@ -29,32 +30,6 @@ function generalDiscretise!(type, ϕ, J)
     nothing
 end
 
-abstract type AbstractField end
-abstract type AbstractTerm end
-abstract type AbstractSource end
-abstract type AbstractEquation end
-struct Linear end
-struct Constant end
-
-struct Equation{I,F} <: AbstractEquation
-    # ϕ::ScalarField{I,F}
-    # A::Union{Matrix{F},SparseMatrixCSC{F, I}}
-    A::SparseMatrixCSC{F, I}
-    b::Vector{F}
-end
-
-struct TempEquation{I,F} <: AbstractEquation
-    # ϕ::ScalarField{I,F}
-    # A::Union{Matrix{F},SparseMatrixCSC{F, I}}
-    A::SparseMatrixCSC{F, I}
-    b::Vector{F}
-end
-# function Equation(ϕ) 
-#     nCells = length(ϕ.mesh.cells)
-#     I, J, V = sparse_matrix_connectivity(ϕ.mesh)
-#     Equation(sparse(I,J,V), zeros(nCells))
-# end
-
 struct ScalarField{I,F} <: AbstractField
     values::Vector{F}
     mesh::Mesh{I,F}
@@ -75,10 +50,6 @@ function Δ{Linear}(Γ, ϕ::ScalarField{I,F}) where {I,F}
     type = Δ{Linear}()
     clear!(ϕ.equation)
     generalDiscretise!(type, ϕ, Γ)
-    # temp = ϕ.tempEquation
-    temp = TempEquation(ϕ.equation.A, ϕ.equation.b)
-    # temp.A .= ϕ.equation.A
-    # temp.b .= ϕ.equation.b
     return temp
 end
 @inline aP!(::Δ{Linear}, A, Γ, face, cID) = begin
@@ -93,26 +64,6 @@ end
     b[cID] = 0.0
     nothing
 end
-
-# struct Δ{T,I,F} <: AbstractTerm 
-#     # Γ::Union{Float32,Float64,Int32,Int64}
-#     Γ::F 
-#     ϕ::ScalarField{I,F}
-#     distretisation::T
-#     label::Symbol
-# end
-# @inline aP!(A, term::Δ{Linear}, face, cID) = begin
-#     A[cID, cID] += (term.Γ * face.area * norm(face.normal)) / face.delta
-#     nothing
-# end
-# @inline  aN!(A, term::Δ{Linear}, face, cID, nID) = begin
-#     A[cID, nID] += -(term.Γ * face.area * norm(face.normal)) / face.delta
-#     nothing
-# end
-# @inline  b!(b, term::Δ{Linear}, face, cID) = begin
-#     b[cID] += 0.0
-#     nothing
-# end
 
 struct Source{T} <: AbstractSource
     ϕ::I where I <: Integer
@@ -151,15 +102,11 @@ function sparse_matrix_connectivity(mesh::Mesh)
 end
 
 # Operator overloads
-# struct Model{I<:Integer}
-#     terms::Vector{AbstractTerm}
-#     sources::Vector{AbstractSource}
-#     sign::Vector{I}
-# end
-# Base.:(==)(a::AbstractTerm, b::AbstractSource) = Model(AbstractTerm[a],AbstractSource[b],[1])
-# Base.:(==)(a::Vector{AbstractTerm}, b::AbstractSource) = Model(a, [b], [1])
-# Base.:(+)(a::AbstractTerm, b::AbstractTerm) = AbstractTerm[a,b]
-# Base.:(+)(a::Vector{AbstractTerm}, b::AbstractTerm) = push!(a, b)
+struct Model{I<:Integer}
+    terms::Vector{AbstractTerm}
+    sources::Vector{AbstractSource}
+    sign::Vector{I}
+end
 
 Base.:(==)(a::AbstractTerm, b::AbstractSource) = Model(AbstractTerm[a],AbstractSource[b],[1])
 Base.:(==)(a::Vector{AbstractTerm}, b::AbstractSource) = Model(a, [b], [1])

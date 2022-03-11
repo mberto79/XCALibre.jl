c3c5a6ec258d0abc285a821e3232b7fbec371235
mberto79 <h.medina@aerofluids.org>
2022-03-11 18:10:10 +0000

    checkpoint: linear edge function added

--- dynamic_proto.jl
+++ dynamic_proto.jl
@@ -12,27 +12,34 @@ n_horizontal2   = 3
 # n_vertical      = 200
 # n_horizontal1   = 1000
 # n_horizontal2   = 8000
-
+function test(n_vertical, n_horizontal1, n_horizontal2)
 p1 = Point(0.0,0.0,0.0)
 p2 = Point(1.0,0.0,0.0)
 p3 = Point(1.5,0.0,0.0)
 p4 = Point(0.0,1.0,0.0)
 p5 = Point(0.8,0.8,0.0)
 p6 = Point(1.5,0.7,0.0)
-points = [p1,p2,p3,p4,p5,p6]
+points = typeof(p1)[p1,p2,p3,p4,p5,p6]
+
+# fig = plot(points)
+# plot!(fig, e1.points, colour=:red)
 
 # Edges in x-direction
-e1 = Edge(1,2,n_horizontal1)
-e2 = Edge(2,3,n_horizontal2)
-e3 = Edge(4,5,n_horizontal1)
-e4 = Edge(5,6,n_horizontal2)
+# e1 = Edge(1,2,n_horizontal1)
+e1 = linear(points,1,2,n_horizontal1)
+e2 = linear(points,2,3,n_horizontal2)
+e3 = linear(points,4,5,n_horizontal1)
+e4 = linear(points,5,6,n_horizontal2)
 
 # Edges in y-direction
-e5 = Edge(1,4,n_vertical)
-e6 = Edge(2,5,n_vertical)
-e7 = Edge(3,6,n_vertical)
-edges = [e1,e2,e3,e4,e5,e6,e7]
+e5 = linear(points,1,4,n_vertical)
+e6 = linear(points,2,5,n_vertical)
+e7 = linear(points,3,6,n_vertical)
+edges = typeof(e1)[e1,e2,e3,e4,e5,e6,e7]
 
+end
+
+@time test(n_vertical, n_horizontal1, n_horizontal2)
 b1 = Block([1,3,5,6],n_horizontal1,n_vertical)
 b2 = Block([2,4,6,7],n_horizontal2,n_vertical)
 blocks = [b1,b2]
@@ -61,10 +68,20 @@ fig = plot(multiblock.nodes)
 plot!(fig, c; colour=:red)
 plot!(fig, multiblock.definition.points, colour=:red)
 
-function linear(edge::Edge{I}) where I
-    for pointID ∈ edge.pointsID
-        pointID*2
+function linear(pts::Vector{Point{F}}, p1::I, p2::I, ncells::I) where {I,F}
+    pointsID = fill(zero(I), ncells+1); pointsID[1] = p1; pointsID[end] = p2
+    points = fill(Point(zero(F)), ncells+1)
+    points[1] = pts[p1]; points[end] = pts[p2]
+    for j ∈ 2:ncells
+        points[j] = Point(linear_distribution(pts[p1], pts[p2], ncells, j))
     end
+    return Edge(points, pointsID, ncells, false)
 end
 
-@time linear(e1)
+function linear_distribution(p1, p2, ncells, j)
+    d = p2.coords - p1.coords
+    d_mag = norm(d)
+    e1 = d/d_mag
+    spacing = d_mag/ncells
+    return spacing*e1*(j-1) + p1.coords
+end

1408524404078b6d964e5647b4953fd45121eb9c
Humberto <h.medina@aerofluids.org>
2023-07-13 20:14:08 +0100

    simple clean up of solvers file

--- src/Solve/Solvers_1_api.jl
+++ src/Solve/Solvers_1_api.jl
@@ -39,66 +39,12 @@ function run!(
     (; phi) = phiModel.terms[1]
     (; values, mesh) = phi
 
-    # if correct_term !== nothing 
-    #     bb      = copy(b)
-    #     gradPhi = Grad{Linear}(phi,2)
-    #     phif    = FaceScalarField(mesh)
-    #     gradf   = FaceVectorField(mesh)
-    # end
-
-    # for i ∈ 1:iterations
     solve!(
         solver, opA, b, values; 
         M=opP, itmax=itmax, atol=atol, rtol=rtol
         )
     # relax!(values, solver.x, 1.0)
+    # println(solver.stats.niter)
     @turbo values .= solver.x
 
-    # if correct_term !== nothing
-    #     nonorthogonal_correction!(gradPhi, gradf, phif, BCs)
-    #     b .= bb
-    #     correct!(equation, correct_term, phif)
-    # end
-    # end
-    # if correct_term !== nothing 
-    #     bb      = nothing
-    #     gradPhi = nothing
-    #     phif    = nothing
-    #     gradf   = nothing
-    # end
 end
-
-# @inline function relax!(phi, phi_new, α)
-#     @inbounds for i ∈ eachindex(phi)
-#         phi[i] += α*(phi_new[i] - phi[i])
-#         # phi[i] = α*phi_new[i] + (1.0 - α)*phi[i]
-#     end
-# end
-
-# @inline function update_solution!(phi, solver)
-#     relax!(phi, solver, 1.0)
-# end
-
-# @inline function update_residual!(equation, opA, phi::ScalarField{Ti,Tf}) where {Ti,Tf}
-#     (; b, R, Fx) = equation
-#     mul!(Fx, opA, phi.values)
-#     @inbounds for i ∈ eachindex(R)
-#         R[i] = b[i] - Fx[i]
-#     end
-# end
-
-# function residual(equation::Equation{Ti,Tf}) where {Ti,Tf}
-#     norm(equation.R)/norm(equation.b)
-# end
-
-# function residual_print(equation::Equation{Ti,Tf}) where {Ti,Tf}
-#     println("Residual: ", norm(equation.R)/norm(equation.b))
-# end
-
-# function clear!(phi::ScalarField{I,F}) where {I,F}
-#     values = phi.values
-#     zero_type = zero(F)
-#     @inbounds for i ∈ eachindex(values)
-#         values[i] = zero_type
-#     end
-# end

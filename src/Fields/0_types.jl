84a71aa2d1b1a8fbb3b7c43f499d9a6f4118e52a
Humberto <h.medina@aerofluids.org>
2023-07-26 14:14:41 +0100

    change model fields for velocity

--- src/Fields/0_types.jl
+++ src/Fields/0_types.jl
@@ -45,41 +45,60 @@ end
 
 # (s::AbstractScalarField)(i::Integer) = s.values[i]
 Base.getindex(s::AbstractScalarField, i::Integer) = s.values[i]
+Base.setindex!(s::AbstractScalarField, x, i::Integer) = begin
+    s.values[i] = x
+end
+Base.length(s::AbstractScalarField) = length(s.values)
+Base.eachindex(s::AbstractScalarField) = eachindex(s.values)
 
 struct VectorField{I,F} <: AbstractVectorField
-    x::Vector{F}
-    y::Vector{F}
-    z::Vector{F}
+    x::ScalarField{I,F}
+    y::ScalarField{I,F}
+    z::ScalarField{I,F}
     mesh::Mesh2{I,F}
 end
 VectorField(mesh::Mesh2{I,F}) where {I,F} = begin
     ncells = length(mesh.cells)
-    VectorField(zeros(F, ncells), zeros(F, ncells), zeros(F, ncells), mesh)
+    VectorField(
+        ScalarField(zeros(F, ncells), mesh),
+        ScalarField(zeros(F, ncells), mesh), 
+        ScalarField(zeros(F, ncells), mesh), 
+        mesh)
 end
 
 struct FaceVectorField{I,F} <: AbstractVectorField
-    x::Vector{F}
-    y::Vector{F}
-    z::Vector{F}
+    x::FaceScalarField{I,F}
+    y::FaceScalarField{I,F}
+    z::FaceScalarField{I,F}
     mesh::Mesh2{I,F}
 end
 FaceVectorField(mesh::Mesh2{I,F}) where {I,F} = begin
     nfaces = length(mesh.faces)
-    FaceVectorField(zeros(F, nfaces), zeros(F, nfaces), zeros(F, nfaces), mesh)
+    FaceVectorField(
+        FaceScalarField(zeros(F, nfaces), mesh),
+        FaceScalarField(zeros(F, nfaces), mesh), 
+        FaceScalarField(zeros(F, nfaces), mesh),
+        mesh)
 end
 
 Base.getindex(v::AbstractVectorField, i::Integer) = SVector{3, eltype(v.x)}(v.x[i], v.y[i], v.z[i])
-
-function initialise!(v::AbstractVectorField, vec::Vector{T}) where T
-    n = length(vec)
-    if T !== eltype(v.x)
-        throw("Vectors are not the same type: $(eltype(v.x)) is not $T")
-    elseif n == 3
-        v.x .= vec[1]
-        v.y .= vec[2]
-        v.z .= vec[3]
-    else
-        throw("Vectors should have 3 components")
-    end
-    nothing
+Base.setindex!(v::AbstractVectorField, x::AbstractVector, i::Integer) = begin
+    length(x) == 3 || throw("Vectors must have 3 components")
+    v.x[i] = x[1]
+    v.y[i] = y[2]
+    v.z[i] = z[3]
 end
+
+# function initialise!(v::AbstractVectorField, vec::Vector{T}) where T
+#     n = length(vec)
+#     if T !== eltype(v.x)
+#         throw("Vectors are not the same type: $(eltype(v.x)) is not $T")
+#     elseif n == 3
+#         v.x .= vec[1]
+#         v.y .= vec[2]
+#         v.z .= vec[3]
+#     else
+#         throw("Vectors should have 3 components")
+#     end
+#     nothing
+# end

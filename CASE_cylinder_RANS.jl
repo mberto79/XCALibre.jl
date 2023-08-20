1c4bb6ee7abf7113cd70e819630e0e6c5a47d263
Humberto <h.medina@aerofluids.org>
2023-08-20 18:45:30 +0100

    updated turbulent cylinder case

--- CASE_cylinder_RANS.jl
+++ CASE_cylinder_RANS.jl
@@ -9,14 +9,7 @@ using Krylov
 mesh_file = "unv_sample_meshes/cylinder_d10mm_5mm.unv"
 mesh = build_mesh(mesh_file, scale=0.001)
 
-# Fields 
-U = VectorField(mesh)
-p = ScalarField(mesh)
-k = ScalarField(mesh)
-ω = ScalarField(mesh)
-νt = ScalarField(mesh)
-
-# BOUNDARY CONDITIONS 
+# INLET CONDITIONS 
 
 Umag = 5
 velocity = [Umag, 0.0, 0.0]
@@ -28,7 +21,9 @@ k_inlet = 3/2*(Tu*Umag)^2
 ω_inlet = k_inlet/(νR*nu)
 Re = (0.2*velocity[1])/nu
 
-@assign! U ( 
+model = RANS{KOmega}(mesh=mesh, viscosity=ConstantScalar(nu))
+
+@assign! model U (
     Dirichlet(:inlet, velocity),
     Neumann(:outlet, 0.0),
     # Neumann(:top, 0.0),
@@ -38,7 +33,7 @@ Re = (0.2*velocity[1])/nu
     Dirichlet(:cylinder, noSlip)
 )
 
-@assign! p (
+@assign! model p (
     Neumann(:inlet, 0.0),
     Dirichlet(:outlet, 0.0),
     Neumann(:top, 0.0),
@@ -46,7 +41,7 @@ Re = (0.2*velocity[1])/nu
     Neumann(:cylinder, 0.0)
 )
 
-@assign! k (
+@assign! model turbulence k (
     Dirichlet(:inlet, k_inlet),
     Neumann(:outlet, 0.0),
     Neumann(:top, 0.0),
@@ -54,15 +49,15 @@ Re = (0.2*velocity[1])/nu
     Dirichlet(:cylinder, 1e-15)
 )
 
-@assign! ω (
+@assign! model turbulence omega (
     Dirichlet(:inlet, ω_inlet),
     Neumann(:outlet, 0.0),
     Neumann(:top, 0.0),
     Neumann(:bottom, 0.0),
-    OmegaWallFunction(:cylinder, (κ=0.41, cmu=0.09, k=k))
+    OmegaWallFunction(:cylinder, (κ=0.41, cmu=0.09))
 )
 
-@assign! νt (
+@assign! model turbulence nut (
     Dirichlet(:inlet, k_inlet/ω_inlet),
     Neumann(:outlet, 0.0),
     Neumann(:top, 0.0),
@@ -70,51 +65,71 @@ Re = (0.2*velocity[1])/nu
     Dirichlet(:cylinder, 0.0)
 )
 
-setup_U = SolverSetup(
-    solver      = GmresSolver, # GmresSolver, BicgstabSolver
-    relax       = 0.7,
-    itmax       = 100,
-    rtol        = 1e-3
+schemes = (
+    U = set_schemes(divergence=Upwind),
+    p = set_schemes(),
+    k = set_schemes(divergence=Upwind),
+    omega = set_schemes(divergence=Upwind)
 )
 
-setup_p = SolverSetup(
-    solver      = GmresSolver, #CgSolver, #GmresSolver, #BicgstabSolver,
-    relax       = 0.3,
-    itmax       = 100,
-    rtol        = 1e-3
+solvers = (
+    U = set_solver(
+        model.U;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = ILU0(),
+        convergence = 1e-7,
+        relax       = 0.7,
+    ),
+    p = set_solver(
+        model.p;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = LDL(),
+        convergence = 1e-7,
+        relax       = 0.3,
+    ),
+    k = set_solver(
+        model.turbulence.k;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = ILU0(),
+        convergence = 1e-7,
+        relax       = 0.9,
+    ),
+    omega = set_solver(
+        model.turbulence.omega;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = ILU0(),
+        convergence = 1e-7,
+        relax       = 0.9,
+    )
 )
 
-setup_turb = SolverSetup(
-    solver      = GmresSolver, # BicgstabSolver, GmresSolver
-    relax       = 0.9,
-    itmax       = 100,
-    rtol        = 1e-3
-)
+config = Configuration(solvers=solvers, schemes=schemes, runtime=())
 
 GC.gc()
 
-initialise!(U, velocity)
-initialise!(p, 0.0)
-initialise!(k, k_inlet)
-initialise!(ω, ω_inlet)
-initialise!(νt, k_inlet/ω_inlet)
+initialise!(model.U, velocity)
+initialise!(model.p, 0.0)
+initialise!(model.turbulence.k, k_inlet)
+initialise!(model.turbulence.omega, ω_inlet)
+initialise!(model.turbulence.nut, k_inlet/ω_inlet)
 
-iterations = 2000
+iterations = 1000
 Rx, Ry, Rp = isimple!( 
-    mesh, nu, U, p, k, ω, νt, 
-    setup_U, setup_p, setup_turb, iterations)
+    model,
+    # setup_U, setup_p, iterations, pref=0.0)
+    config, iterations)
 
-Fp = pressure_forces(:cylinder, p, 1.25)
-Reff = stress_tensor(U, nu, νt)
-Fv = viscous_forces(:cylinder, U, 1.25, nu, νt)
+Fp = pressure_forces(:cylinder, model.p, 1.25)
+Reff = stress_tensor(model.U, nu, model.nut)
+Fv = viscous_forces(:cylinder, model.U, 1.25, nu, model.nut)
 
 write_vtk(
     "results", mesh, 
-    ("U", U), 
-    ("p", p),
-    ("k", k),
-    ("omega", ω),
-    ("nut", νt)
+    ("U", model.U), 
+    ("p", model.p),
+    ("k", model.k),
+    ("omega", model.omega),
+    ("nut", model.nut)
     )
 
 plot(; xlims=(0,iterations), ylims=(1e-8,0))

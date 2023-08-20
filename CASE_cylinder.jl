a51bf4423c0d546c5c122aa48e970fead3e5fb77
Humberto <h.medina@aerofluids.org>
2023-08-20 17:17:15 +0100

    updated laminar cylinder case

--- CASE_cylinder.jl
+++ CASE_cylinder.jl
@@ -9,19 +9,16 @@ using Krylov
 mesh_file = "unv_sample_meshes/cylinder_d10mm_5mm.unv"
 mesh = build_mesh(mesh_file, scale=0.001)
 
-# Fields 
-p = ScalarField(mesh)
-U = VectorField(mesh)
-
-# BOUNDARY CONDITIONS 
+# Inlet conditionns
 
 velocity = [0.1, 0.0, 0.0]
 noSlip = [0.0, 0.0, 0.0]
 nu = 1e-3
 Re = (0.2*velocity[1])/nu
 
-U = assign(
-    U, 
+model = RANS{Laminar}(mesh=mesh, viscosity=ConstantScalar(nu))
+
+@assign! model U ( 
     Dirichlet(:inlet, velocity),
     Neumann(:outlet, 0.0),
     Dirichlet(:cylinder, noSlip),
@@ -29,8 +26,7 @@ U = assign(
     Neumann(:top, 0.0)
 )
 
-p = assign(
-    p,
+@assign! model p (
     Neumann(:inlet, 0.0),
     Dirichlet(:outlet, 0.0),
     Neumann(:cylinder, 0.0),
@@ -38,31 +34,43 @@ p = assign(
     Neumann(:top, 0.0)
 )
 
-setup_U = SolverSetup(
-    solver      = GmresSolver, # GmresSolver, BicgstabSolver
-    relax       = 0.8,
-    itmax       = 100,
-    rtol        = 1e-1
+solvers = (
+    U = set_solver(
+        model.U;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = ILU0(),
+        convergence = 1e-7,
+        relax       = 0.7,
+    ),
+    p = set_solver(
+        model.p;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = LDL(),
+        convergence = 1e-7,
+        relax       = 0.3,
+    )
 )
 
-setup_p = SolverSetup(
-    solver      = GmresSolver, #CgSolver, #GmresSolver, #BicgstabSolver,
-    relax       = 0.3,
-    itmax       = 100,
-    rtol        = 1e-1
+schemes = (
+    U = set_schemes(),
+    p = set_schemes()
 )
+
+config = Configuration(solvers=solvers, schemes=schemes, runtime=())
+
 
 GC.gc()
 
-initialise!(U, velocity)
-initialise!(p, 0.0)
+initialise!(model.U, velocity)
+initialise!(model.p, 0.0)
 
-iterations = 500 # 84.86s
-Rx, Ry, Rp = isimple!(
-    mesh, nu, U, p, 
-    setup_U, setup_p, iterations)
+iterations = 1000
+Rx, Ry, Rp = isimple!( 
+    model,
+    # setup_U, setup_p, iterations, pref=0.0)
+    config, iterations)
 
-write_vtk("results", mesh, ("U", U), ("p", p))
+write_vtk("results", mesh, ("U", model.U), ("p", model.p))
 
 plot(; xlims=(0,iterations), ylims=(1e-8,0))
 plot!(1:length(Rx), Rx, yscale=:log10, label="Ux")

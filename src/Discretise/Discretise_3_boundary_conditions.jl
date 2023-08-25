109eb389f41ee2aca8df5e765834c32fe95428ab
Humberto <h.medina@aerofluids.org>
2023-08-25 18:37:27 +0100

    first implementation of highRe wall function for KOmega

--- src/Discretise/Discretise_3_boundary_conditions.jl
+++ src/Discretise/Discretise_3_boundary_conditions.jl
@@ -26,14 +26,14 @@ end
 end
 
 @inline (bc::KWallFunction)(
-    term::Operator{F,P,I,Laplacian{Linear}}, 
-    A, b, cellID, cell, face, fID) where {F,P,I} = begin
+    term::Operator{F,P,I,Laplacian{T}}, 
+    A, b, cellID, cell, face, fID) where {F,P,I,T}  = begin
     nothing
 end
 
 @inline (bc::OmegaWallFunction)(
     term::Operator{F,P,I,Laplacian{T}}, 
-    A, b, cellID, cell, face, fID) where {F,P,I} where T = begin
+    A, b, cellID, cell, face, fID) where {F,P,I,T} = begin
     nothing
 end
 
@@ -88,17 +88,17 @@ end
 end
 
 @inline (bc::KWallFunction)(
-    term::Operator{F,P,I,Divergence{Linear}}, 
-    A, b, cellID, cell, face, fID) where {F,P,I} = begin
-    nothing
+    term::Operator{F,P,I,Divergence{T}}, # use upwind for all
+    A, b, cellID, cell, face, fID) where {F,P,I,T} = begin
+    ap = term.sign[1]*(term.flux[fID])
+    A[cellID,cellID] += max(ap, 0.0)
 end
 
 @inline (bc::OmegaWallFunction)(
-    term::Operator{F,P,I,Divergence{T}}, 
-    A, b, cellID, cell, face, fID) where {F,P,I} where T = begin
+    term::Operator{F,P,I,Divergence{T}}, # might need to change this!!!!
+    A, b, cellID, cell, face, fID) where {F,P,I,T}  = begin
     nothing
 end
-
 
 # IMPLICIT SOURCE
 
@@ -114,24 +114,14 @@ end
     nothing
 end
 
-@inline (bc::KWallFunction)(# a bit hacky for now (should be Src)
+@inline (bc::KWallFunction)(
     term::Operator{F,P,I,Si}, 
     A, b, cellID, cell, face, fID) where {F,P,I} = begin
-    # cmu, κ, k = bc.value
-    # b[cellID] -= k[cellID]^1.5*cmu^0.75/(κ*face.delta)*cell.volume
     nothing
 end
 
-@inline (bc::OmegaWallFunction)(# a bit hacky for now (should be Src)
+@inline (bc::OmegaWallFunction)(
     term::Operator{F,P,I,Si}, 
     A, b, cellID, cell, face, fID) where {F,P,I} = begin
-    # cmu, κ, k = bc.value
-    # ωc = k[cellID]^0.5/(cmu^0.25*κ*face.delta)*cell.volume
-    # y = face.delta
-    # ωc = 6*1e-3/(0.075*y^2)
-    # b[cellID] += A[cellID,cellID]*ωc
-    # A[cellID,cellID] += A[cellID,cellID]
-    # b[cellID] += A[cellID,cellID]*ωc
-    # A[cellID,cellID] += A[cellID,cellID]
     nothing
 end

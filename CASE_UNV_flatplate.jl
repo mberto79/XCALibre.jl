1e5212d9a358afcacbd660e73229909c276b734f
Humberto <h.medina@aerofluids.org>
2023-08-29 18:05:23 +0100

    Flatplate: new case set up

---
+++ CASE_UNV_flatplate.jl
@@ -0,0 +1,89 @@
+using Plots
+
+using FVM_1D
+
+using Krylov
+
+# backwardFacingStep_2mm, backwardFacingStep_10mm
+mesh_file = "unv_sample_meshes/flatplate_2D.unv"
+mesh = build_mesh(mesh_file, scale=0.001)
+
+
+velocity = [0.5, 0.0, 0.0]
+nu = 1e-3
+Re = velocity[1]*0.1/nu
+
+model = RANS{Laminar}(mesh=mesh, viscosity=ConstantScalar(nu))
+
+@assign! model U (
+    Dirichlet(:inlet, velocity),
+    Neumann(:outlet, 0.0),
+    Dirichlet(:wall, [0.0, 0.0, 0.0]),
+    Dirichlet(:top, [0.0, 0.0, 0.0])
+)
+
+ @assign! model p (
+    Neumann(:inlet, 0.0),
+    Dirichlet(:outlet, 0.0),
+    Neumann(:wall, 0.0),
+    Neumann(:top, 0.0)
+)
+
+schemes = (
+    U = set_schemes(),
+    p = set_schemes()
+)
+
+
+solvers = (
+    U = set_solver(
+        model.U;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = DILU(),
+        convergence = 1e-7,
+        relax       = 0.8,
+    ),
+    p = set_solver(
+        model.p;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = LDL(),
+        convergence = 1e-7,
+        relax       = 0.2,
+    )
+)
+
+runtime = set_runtime(iterations=1000, write_interval=0)
+
+config = Configuration(
+    solvers=solvers, schemes=schemes, runtime=runtime)
+
+GC.gc()
+
+initialise!(model.U, velocity)
+initialise!(model.p, 0.0)
+
+Rx, Ry, Rp = isimple!(model, config) # 9.39k allocs
+
+write_vtk("results", mesh, ("U", model.U), ("p", model.p))
+
+plot(; xlims=(0,184))
+plot!(1:length(Rx), Rx, yscale=:log10, label="Ux")
+plot!(1:length(Ry), Ry, yscale=:log10, label="Uy")
+plot!(1:length(Rp), Rp, yscale=:log10, label="p")
+
+# # PROFILING CODE
+
+# using Profile, PProf
+
+# GC.gc()
+# initialise!(U, velocity)
+# initialise!(p, 0.0)
+
+# Profile.Allocs.clear()
+# Profile.Allocs.@profile sample_rate=1 begin Rx, Ry, Rp = isimple!(
+#     mesh, nu, U, p,
+#     # setup_U, setup_p, iterations, pref=0.0)
+#     setup_U, setup_p, iterations)
+# end
+
+# PProf.Allocs.pprof()

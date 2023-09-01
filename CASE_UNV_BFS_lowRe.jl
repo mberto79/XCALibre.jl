5fb551ef1c2b6144debec8475f5953c7ac98d8c1
Humberto <h.medina@aerofluids.org>
2023-09-01 14:55:05 +0100

    renamed case and added highRe file

---
+++ CASE_UNV_BFS_lowRe.jl
@@ -0,0 +1,152 @@
+using Plots
+
+using FVM_1D
+
+using Krylov
+
+# backwardFacingStep_2mm, backwardFacingStep_10mm
+# mesh_file = "unv_sample_meshes/backwardFacingStep_10mm.unv"
+mesh_file = "unv_sample_meshes/backwardFacingStep_5mm.unv"
+# mesh_file = "unv_sample_meshes/backwardFacingStep_2mm.unv"
+mesh = build_mesh(mesh_file, scale=0.001)
+
+
+nu = 1e-3
+u_mag = 1.5
+velocity = [u_mag, 0.0, 0.0]
+k_inlet = 1
+ω_inlet = 1000
+ω_wall = ω_inlet
+Re = velocity[1]*0.1/nu
+
+model = RANS{KOmega}(mesh=mesh, viscosity=ConstantScalar(nu))
+
+@assign! model U (
+    Dirichlet(:inlet, velocity),
+    Neumann(:outlet, 0.0),
+    Dirichlet(:wall, [0.0, 0.0, 0.0]),
+    Dirichlet(:top, [0.0, 0.0, 0.0])
+)
+
+@assign! model p (
+    Neumann(:inlet, 0.0),
+    Dirichlet(:outlet, 0.0),
+    Neumann(:wall, 0.0),
+    Neumann(:top, 0.0)
+)
+
+@assign! model turbulence k (
+    Dirichlet(:inlet, k_inlet),
+    Neumann(:outlet, 0.0),
+    KWallFunction(:wall),
+    KWallFunction(:top)
+    # Dirichlet(:wall, 1e-15),
+    # Dirichlet(:top, 1e-15)
+)
+
+@assign! model turbulence omega (
+    Dirichlet(:inlet, ω_inlet),
+    Neumann(:outlet, 0.0),
+    OmegaWallFunction(:wall),
+    OmegaWallFunction(:top)
+    # Dirichlet(:wall, ω_wall), 
+    # Dirichlet(:top, ω_wall)
+)
+
+@assign! model turbulence nut (
+    Dirichlet(:inlet, k_inlet/ω_inlet),
+    Neumann(:outlet, 0.0),
+    Neumann(:wall, 0.0), 
+    Neumann(:top, 0.0)
+    # Dirichlet(:wall, 0.0), 
+    # Dirichlet(:top, 0.0)
+)
+
+schemes = (
+    U = set_schemes(),
+    p = set_schemes(),
+    k = set_schemes(),
+    omega = set_schemes()
+)
+
+solvers = (
+    U = set_solver(
+        model.U;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = ILU0(),
+        convergence = 1e-7,
+        relax       = 0.7,
+    ),
+    p = set_solver(
+        model.p;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = LDL(),
+        convergence = 1e-7,
+        relax       = 0.3,
+    ),
+    k = set_solver(
+        model.turbulence.k;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = ILU0(),
+        convergence = 1e-7,
+        relax       = 0.7,
+    ),
+    omega = set_solver(
+        model.turbulence.omega;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = ILU0(),
+        convergence = 1e-7,
+        relax       = 0.7,
+    )
+)
+
+runtime = set_runtime(iterations=1000, write_interval=0)
+
+config = Configuration(
+    solvers=solvers, schemes=schemes, runtime=runtime)
+
+GC.gc()
+
+initialise!(model.U, velocity)
+initialise!(model.p, 0.0)
+initialise!(model.turbulence.k, k_inlet)
+initialise!(model.turbulence.omega, ω_inlet)
+initialise!(model.turbulence.nut, k_inlet/ω_inlet)
+
+Rx, Ry, Rp = isimple!(model, config) # 36.90k allocs
+
+write_vtk(
+    "results", mesh, 
+    ("U", model.U), 
+    ("p", model.p),
+    ("k", model.turbulence.k),
+    ("omega", model.turbulence.omega),
+    ("nut", model.turbulence.nut)
+    )
+
+Reff = stress_tensor(model.U, nu, model.turbulence.nut)
+Fp = pressure_force(:wall, model.p, 1.25)
+Fv = viscous_force(:wall, model.U, 1.25, nu, model.turbulence.nut)
+
+
+plot(; xlims=(0,494))
+plot!(1:length(Rx), Rx, yscale=:log10, label="Ux")
+plot!(1:length(Ry), Ry, yscale=:log10, label="Uy")
+plot!(1:length(Rp), Rp, yscale=:log10, label="p")
+
+# # PROFILING CODE
+
+# using Profile, PProf
+
+# GC.gc()
+# initialise!(U, velocity)
+# initialise!(p, 0.0)
+
+# Profile.Allocs.clear()
+# Profile.Allocs.@profile sample_rate=1 begin Rx, Ry, Rp = isimple!(
+#     mesh, nu, U, p,
+#     # setup_U, setup_p, iterations, pref=0.0)
+#     setup_U, setup_p, iterations)
+# end
+
+# PProf.Allocs.pprof()

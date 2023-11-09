0a2581eb99d144a7619d021f34035a6e266a0389
Humberto <h.medina@aerofluids.org>
2023-11-09 16:10:51 +0000

    example of new cell extension and operation with prior mesh

--- SVector Solution.jl
+++ SVector Solution.jl
@@ -1,30 +1,52 @@
 using CUDA
+using FVM_1D
 using StaticArrays
 using Adapt
 
-abstract type AbstractTest end
+# load existing mesh
+# quad and trig 40 and 100
+mesh_file = "unv_sample_meshes/trig40.unv"
+mesh = build_mesh(mesh_file, scale=0.001)
+
+meshgpu = cu(mesh.cells)
+
+cell = mesh.cells[1]
+
+cell.nodesID
+TestCell(
+    cell.nodesID,
+    cell.facesID,
+    cell.neighbours,
+    cell.nsign,
+    cell.centre,
+    cell.volume
+    )
+
+abstract type AbstractMockMesh end
 
 #Mesh structure changes
-struct TestCellVector{I,F,N}
-    nodesID::SVector{N,I}
-    facesID::SVector{N,I}
-    neighbours::SVector{N,I}
-    nsign::SVector{N,I}
+struct TestCell{I,F} # say 60 for now?
+    nnodes::I
+    nfaces::I
+    nneighbours::I
+    nodesID::SVector{24,I}
+    facesID::SVector{24,I}
+    neighbours::SVector{24,I}
+    nsign::SVector{24,I}
     centre::SVector{3, F}
     volume::F
 end
-Adapt.@adapt_structure TestCellVector
+Adapt.@adapt_structure TestCell
 
-struct TestVector{I,F,N} <: AbstractTest
-    TestCellVectors::Vector{TestCellVector{I,F,N}}
-    # TestFaces::Vector{TestFace2D{Int8,Float32}}
+struct MockMesh{C} <: AbstractMockMesh
+    cells::C
 end
 Adapt.@adapt_structure TestVector
 
-function TestCellVector(I, F)
+function TestCell(I, F)
     zf = zero(F)
     vec3F = SVector{3, F}(zf, zf, zf)
-    TestCellVector(SVector(rand(I, 24)...), SVector(rand(I, 24)...), SVector(rand(I, 24)...),SVector(rand(I, 24)...),
+    TestCell(SVector(rand(I, 24)...), SVector(rand(I, 24)...), SVector(rand(I, 24)...),SVector(rand(I, 24)...),
     vec3F, zf)
 end
 
@@ -32,25 +54,33 @@ N = 24
 I = Int8
 F = Float32
 
-cells_vector = [TestCellVector(I, F) for i in 1:10]
+cells = [TestCell(I, F) for i in 1:10]
 
-test = TestVector{I, F, N}(cells_vector)
-
-aCPU = test.TestCellVectors[1].facesID
+aCPU = test.TestCells[1].facesID
 a = CuArray(aCPU)
 b = CUDA.zeros(Int8,length(a))
 
-typeof(test.TestCellVectors[1].facesID)
-
-function TestKernel!(a,b)
+function testKernel!(a,b) # check order
     i = threadIdx().x
     @inbounds if i <= length(b) && i > 0
         b[i] = a[i] + a[i]
     end
     return nothing
 end
-
-
-@cuda threads = length(b) TestKernel!(a,b)
+@cuda threads = length(b) testKernel!(a,b)
 a
 b
+
+@kernel function double_nodesID2!(A) # I wouldn't want to do this in reality :-)
+    I = @index(Global)
+    nodesID = A[I].nodesID
+    # nids = @private Int32 length(nodesID)
+    # for (i, id) ∈ enumerate(nodesID)
+    #     nids[i] = id
+    # end
+    # A[I] = Test0(A[I].centre, nids)
+    for id ∈ nodesID
+        @print(id,"\n")
+    end
+end
+

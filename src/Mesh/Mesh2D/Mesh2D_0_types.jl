26e68b02d88d916fbedbd7876a52a32d9cf784e1
Humberto <h.medina@aerofluids.org>
2023-11-12 22:19:32 +0000

    remove outer constructors for mesh types (now in UNV)

--- src/Mesh/Mesh2D/Mesh2D_0_types.jl
+++ src/Mesh/Mesh2D/Mesh2D_0_types.jl
@@ -10,12 +10,6 @@ struct Face2D{I,F}
     delta::F
     weight::F
 end
-Face2D(I,F) = begin
-    zi = zero(I); zf = zero(F)
-    vec_2I = SVector{2,I}(zi,zi)
-    vec_3F = SVector{3,F}(zf,zf,zf)
-    Face2D(vec_2I, vec_2I, vec_3F, vec_3F, vec_3F, zf, zf, zf)
-end
 Adapt.@adapt_structure Face2D
 
 struct Mesh2{I,F} <: AbstractMesh
--- src/Mesh/Mesh_0_types.jl
+++ src/Mesh/Mesh_0_types.jl
@@ -13,9 +13,6 @@ Node(TF) = begin
     vec_3F = SVector{3,TF}(zf,zf,zf)
     Node(vec_3F, Int64[])
 end
-Node(x::F, y::F, z::F) where F<:AbstractFloat = Node(SVector{3, F}(x,y,z), Int64[])
-Node(zero::F) where F<:AbstractFloat = Node(zero, zero, zero)
-Node(vector::F) where F<:AbstractVector = Node(vector, Int64[])
 
 struct Boundary{I}
     name::Symbol
@@ -36,9 +33,3 @@ struct Cell{I,F}
     volume::F
 end
 Adapt.@adapt_structure Cell
-Cell(I,F) = begin
-    zf = zero(F)
-    vec3F = SVector{3,F}(zf,zf,zf)
-    Cell(I[], I[], I[], I[], vec3F, zf)
-end
-

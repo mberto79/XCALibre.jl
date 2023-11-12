853a38d3396f4c2708a86f0870aefafce60a96b4
Humberto <h.medina@aerofluids.org>
2023-11-12 22:34:10 +0000

    update mesh type definitions: using pointers + long arrays in Mesh2

--- src/Mesh/Mesh2D/Mesh2D_0_types.jl
+++ src/Mesh/Mesh2D/Mesh2D_0_types.jl
@@ -1,7 +1,7 @@
 export Face2D, Mesh2
 
 struct Face2D{I,F}
-    nodesID::SVector{2,I}
+    face2nodes::SVector{2,I}
     ownerCells::SVector{2,I}
     centre::SVector{3, F}
     normal::SVector{3, F}
@@ -14,7 +14,12 @@ Adapt.@adapt_structure Face2D
 
 struct Mesh2{I,F} <: AbstractMesh
     cells::Vector{Cell{I,F}}
+    cell2nodes::Vector{I}
+    cell2faces::Vector{I}
+    cell2neighbours::Vector{I}
+    cell2nsign::Vector{I}
     faces::Vector{Face2D{I,F}}
+    face2nodes::Vector{I}
     boundaries::Vector{Boundary{I}}
     nodes::Vector{Node{I,F}}
 end
--- src/Mesh/Mesh_0_types.jl
+++ src/Mesh/Mesh_0_types.jl
@@ -11,20 +11,19 @@ Adapt.@adapt_structure Node
 
 struct Boundary{I}
     name::Symbol
-    # nodesID::Vector{I}
-    nodesID::Vector{Vector{I}}
+    # nodesID::Vector{I} # can be deduced from face info
     facesID::Vector{I}
     cellsID::Vector{I}
-    # normal::SVector{3, F}
+    # normal::SVector{3, F} # correctly aligned by definition
 end
 Adapt.@adapt_structure Boundary
 
 struct Cell{I,F}
-    nodesID::Vector{I}
-    facesID::Vector{I}
-    neighbours::Vector{I}
-    nsign::Vector{I}
     centre::SVector{3, F}
     volume::F
+    map2nodes::SVector{2,I}
+    map2faces::SVector{2,I}
+    map2neighbours::SVector{2,I}
+    map2nsign::SVector{2,I}
 end
 Adapt.@adapt_structure Cell

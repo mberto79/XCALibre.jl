707abb6b3d393207dd97cd623a6bc5d53b07c2b6
OscarOsborn <oosbornwork@gmail.com>
2023-11-19 16:05:09 +0000

    new structure + areas,centres,volumes

--- src/UNV_3D_NewStructure/sandbox.jl
+++ src/UNV_3D_NewStructure/sandbox.jl
@@ -1,4 +1,4 @@
-unv_mesh="src/UNV_3D/Mesh_tetrasmall.unv"
+unv_mesh="src/UNV_3D_NewStructure/tetra_singlecell.unv"
 
 using StaticArrays
 using LinearAlgebra
@@ -6,37 +6,6 @@ using Setfield
 using Adapt
 include("UNV_3D_NS_Types.jl")
 
-struct Face3D{I,F}
-    nodes_map::SVector{3,I}
-    ownerCells::SVector{2,I}
-    centre::SVector{3, F}
-    normal::SVector{3, F}
-    e::SVector{3, F}
-    area::F
-    delta::F
-    weight::F
-end
-Adapt.@adapt_structure Face3D
-Face3D(I,F) = begin
-    zi = zero(I); zf = zero(F)
-    vec_2I = SVector{2,I}(zi,zi)
-    vec_3I=SVector{3,I}(zi,zi,zi)
-    vec_3F = SVector{3,F}(zf,zf,zf)
-    Face3D(vec_3I, vec_2I, vec_3F, vec_3F, vec_3F, zf, zf, zf)
-end
-
-struct Mesh3{I,F} #<: AbstractMesh
-    cells::Vector{Cell{I,F}}
-    cell_nodes::Vector{I}
-    cell_faces::Vector{I}
-    cell_neighbours::Vector{I}
-    cell_nsign::Vector{I}
-    faces::Vector{Face3D{I,F}}
-    face_nodes::Vector{I}
-    boundaries::Vector{Boundary{I}}
-    nodes::Vector{Node{I,F}}
-end
-Adapt.@adapt_structure Mesh3
 
 #UNV Reader New Structure
 
@@ -83,7 +52,6 @@ currentBoundary=0
 boundaryNumber=0
 
 
-
 #Splits UNV file into sections
 for (indx,line) in enumerate(eachline(unv_mesh))
     sline=split(line)
@@ -98,14 +66,14 @@ for (indx,line) in enumerate(eachline(unv_mesh))
     end
     #BC=2467
     if sline[1]== "2467" && length(sline)==1
-        bcindx=indx
+        boundaryindx=indx
     end
 end
 
 #Check
 pointindx
 elementindx
-bcindx
+boundaryindx
 
 #Extracting Data from UNV file
 for (indx,line) in enumerate(eachline(unv_mesh))
@@ -127,15 +95,12 @@ for (indx,line) in enumerate(eachline(unv_mesh))
     if length(sline)==6 && parse(Int64,sline[end])==2
         edgeCount=parse(Int,sline[end])
         edgeindex=parse(Int,sline[1])
-        #allelementCount=parse(Int,sline[end])
-        #allelementindex=parse(Int,sline[1])
         continue
     end
 
     if length(sline)==2 && indx>elementindx
         edge=[parse(Int,sline[i]) for i=1:length(sline)]
         push!(edges,Edge(edgeindex,edgeCount,edge))
-        #allelement=[parse(Int,sline[i]) for i=1:length(sline)]
         push!(elements,Element(edgeindex,edgeCount,edge))
         continue
     end
@@ -149,7 +114,7 @@ for (indx,line) in enumerate(eachline(unv_mesh))
 
     if length(sline)==3 && indx>elementindx && parse(Int,sline[end]) ≠ 1
         face=[parse(Int,sline[i]) for i=1:length(sline)]
-        push!(faces,Face(faceindex,faceCount,face))
+        push!(faces,Face(faceindex-edgeindex,faceCount,face))
         push!(elements,Element(faceindex,faceCount,face))
         continue
     end
@@ -164,7 +129,7 @@ for (indx,line) in enumerate(eachline(unv_mesh))
 
     if length(sline)==4 && indx>elementindx
         volume=[parse(Int,sline[i]) for i=1:length(sline)]
-        push!(volumes,Volume(volumeindex,volumeCount,volume))
+        push!(volumes,Volume(volumeindex-faceindex,volumeCount,volume))
         push!(elements,Element(volumeindex,volumeCount,volume))
         continue
     end
@@ -177,51 +142,17 @@ for (indx,line) in enumerate(eachline(unv_mesh))
         push!(boundaryElements, newBoundary)
         boundaryNumber=boundaryNumber+1
         boundaryElements[currentBoundary].boundaryNumber=currentBoundary
-        boundaryElements[currentBoundary].name=sline[1]
+        boundaryElements[currentBoundary].name=boundaryindex
         continue
     end
 
     if length(sline)==8 && indx>boundaryindx && parse(Int64,sline[2])!=0
         boundary=[parse(Int,sline[i]) for i=1:length(sline)]
         push!(boundarys,(boundaryindex,boundary))
-        #push!(boundaryElements[currentBC].elements,parse(Int,sline[2]))
-        #push!(boundaryElements[currentBC].elements,parse(Int,sline[6]))
-        #push!(elements,parse(Int64,sline[2]))
-        
-        #push!(boundaryElements,BoundaryLoader(name,groupNumber,parse(Int,sline[2])))
-        #push!(boundaryElements,BoundaryLoader(name,groupNumber,parse(Int,sline[6])))
-        
-        #push!(boundaryElements,BoundaryLoader(name,groupNumber,elements))
-        #push!(elements,parse(Int64,sline[6]))
-        #push!(boundaryElements,BoundaryLoader(name,groupNumber,elements))
-        #element1=parse(Int64,sline[2])
-        #element2=parse(Int64,sline[6])
-        #push!(elements,element1)
-        #push!(elements,element2)
-        
-        #elements=[parse(Int64,sline[4*i-2]) for i=1:2]
-        #push!(boundaryElements,BoundaryLoader(name,groupNumber,elements))
-        
         push!(boundaryElements[currentBoundary].elements,parse(Int64,sline[2]))
         push!(boundaryElements[currentBoundary].elements,parse(Int64,sline[6]))
         continue
     end
-
-
-    #if length(sline)==1 && indx>bcindx
-        #currentBC=currentBC+1
-        #boundaryElements[currentBC].groupNumber=currentBC
-        #boundaryElements[currentBC].name=sline[1]
-        #continue
-    #end
-
-    #if length(sline)==8 && indx>bcindx && parse(Int64,sline[2])!=0
-        #push!(boundaryElements,BoundaryLoader(name,groupNumber,parse(Int,sline[2])))
-        #push!(boundaryElements,BoundaryLoader(name,groupNumber,parse(Int,sline[6])))
-        # push!(boundaryElements[currentBC].elements,parse(Int,sline[2]))
-        #push!(boundaryElements[currentBC].elements,parse(Int,sline[6]))
-        #continue
-    #end
 
 end
 
@@ -245,29 +176,125 @@ volumeindex
 volume
 volumes
 
+
 boundaryindex
 boundaryindx
 boundary
 boundarys
-length(boundarys)*2
 
 boundaryElements
 boundaryNumber
 currentBoundary
 elements
 
-#Generate Nodes
+mesh=Mesh3[]
+#Generate nodes
 nodes=Node[]
 @inbounds for i ∈ 1:length(points)
-    point1= points[i].xyz
-    push!(nodes,Node(point1))
+    point=points[i].xyz
+    push!(nodes,Node(point))
 end
 nodes
 
-#convert
-boundaries1 = Vector{Boundary{integer}}(undef, (length(boundarys)*2))
-cells1 = Vector{Cell{integer,float}}(undef, length(volumes))
-faces1 = Vector{Face3D{integer,float}}(undef, length(faces))
-nodes1 = Vector{Node{integer,float}}(undef, length(points))
+#Generate Faces
+face_nodes=[] #Vector(undef,length(faces)*3)
+
+for n=1:length(faces)
+    for i=1:3
+        push!(face_nodes,faces[n].faces[i])
+    end
+end
+face_nodes
+
+#Generate cells
+cell_nodes=[]
+cell_faces=[]
+
+for n=1:length(volumes)
+    for i=1:4
+        push!(cell_nodes,volumes[n].volumes[i])
+    end
+end
+cell_nodes
+
+for n=1:length(faces)
+    #for i=1:4
+        push!(cell_faces,faces[n].faceindex)
+    #end
+end
+cell_faces
+
+#Nodes Range
+cell_nodes_range=UnitRange(0,0)
+for i=1:length(volumes)
+    cell_nodes_range=UnitRange(volumes[i].volumes[1],volumes[i].volumes[end])
+end
+cell_nodes_range
+
+face_nodes_range=UnitRange(0,0)
+for i=1:length(faces)
+    face_nodes_range=UnitRange(faces[i].faces[1],faces[i].faces[end])
+end
+face_nodes_range
 
 
+#Faces Range
+cell_faces_range=UnitRange(faces[1].faceindex,faces[4].faceindex)
+
+#Face Area
+A=(nodes[1].coords)
+B=(nodes[2].coords)
+C=(nodes[3].coords)
+
+AB=[B[1]-A[1],B[2]-A[2],B[3]-A[3]]
+AC=[C[1]-A[1],C[2]-A[2],C[3]-A[3]]
+
+face_area=0.5*norm(cross(AB,AC))
+
+#Face centre
+
+function centre_of_face(nodes)
+    x_centre = (nodes[1].coords[1] + nodes[2].coords[1] + nodes[3].coords[1]) / 3
+    y_centre = (nodes[1].coords[2]+nodes[2].coords[2]+nodes[3].coords[2]) / 3
+    z_centre = (nodes[1].coords[3]+nodes[2].coords[3]+nodes[3].coords[3]) / 3
+    return (x_centre, y_centre, z_centre)
+end
+
+centre_of_face(nodes)
+
+#Cell centre
+function centre_of_cell(nodes)
+    x_centre = (nodes[1].coords[1]+nodes[2].coords[1]+nodes[3].coords[1]+nodes[4].coords[1]) / 4
+    y_centre = (nodes[1].coords[2]+nodes[2].coords[2]+nodes[3].coords[2]+nodes[4].coords[2]) / 4
+    z_centre = (nodes[1].coords[3]+nodes[2].coords[3]+nodes[3].coords[3]+nodes[4].coords[3]) / 4
+    return (x_centre, y_centre, z_centre)
+end
+
+centre_of_cell(nodes)
+
+#Cell Volume
+function volume_of_cell(nodes)
+    A=(nodes[1].coords)
+    B=(nodes[2].coords)
+    C=(nodes[3].coords)
+    D=(nodes[4].coords)
+    AB=[B[1]-A[1],B[2]-A[2],B[3]-A[3]]
+    AC=[C[1]-A[1],C[2]-A[2],C[3]-A[3]]
+    AD=[D[1]-A[1],D[2]-A[2],D[3]-A[3]]
+    return abs(dot(AB, cross(AC, AD))) / 6
+end
+
+volume_of_cell(nodes)
+
+#Face Normals
+function face_normal(nodes)
+    A=(nodes[1].coords)
+    B=(nodes[2].coords)
+    C=(nodes[3].coords)
+    AB=[B[1]-A[1],B[2]-A[2],B[3]-A[3]]
+    AC=[C[1]-A[1],C[2]-A[2],C[3]-A[3]]
+    cross_product = cross(AB, AC)
+    return normalize(cross_product)
+end
+
+face_normal(nodes)

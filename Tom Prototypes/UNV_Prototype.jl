fa5f29f14b4eb80df26b60a098c5a99f4463e81b
TomMazin <egytm1@nottingham.ac.uk>
2023-11-24 19:42:04 +0000

    Tested nodes in kernel and moved UNV conversion function to its own file

--- Tom Prototypes/UNV_Prototype.jl
+++ Tom Prototypes/UNV_Prototype.jl
@@ -4,112 +4,6 @@ using Krylov
 using StaticArrays
 using CUDA
 
-mesh2_from_UNV(mesh; integer=Int64, float=Float64) = begin
-    boundaries = Vector{Boundary{Vector{integer}}}(undef, length(mesh.boundaries))
-    cells = Vector{Cell{integer,float}}(undef, length(mesh.cells))
-    faces = Vector{Face2D{integer,float}}(undef, length(mesh.faces))
-    nodes = Vector{Node{Vector{integer},float}}(undef, length(mesh.nodes))
-
-    for (i, b) ∈ enumerate(mesh.boundaries)
-        boundaries[i] = Boundary{Vector{integer}}(b.name, b.facesID, b.cellsID)
-    end
-
-    for (i, n) ∈ enumerate(mesh.nodes)
-        nodes[i] = Node(n.coords, n.neighbourCells)
-    end
-
-    # PROCESSING CELLS
-    # Calculate array size needed for nodes and faces
-    nnodes = zero(integer)
-    nfaces = zero(integer)
-    for cell ∈ mesh.cells
-        (; nodesID, facesID) = cell 
-        nnodes += length(nodesID)
-        nfaces += length(facesID)
-    end
-
-    # Initialise arrays 
-    cell_nodes = Vector{integer}(undef, nnodes)
-    cell_faces = Vector{integer}(undef, nfaces)
-    cell_neighbours = Vector{integer}(undef, nfaces)
-    cell_nsign = Vector{integer}(undef, nfaces)
-
-    ni = 1 # node counter
-    fi = 1 # face counter
-    for (i, cell) ∈ enumerate(mesh.cells)
-        (;nodesID, facesID, neighbours, nsign) = cell
-        # node array loop
-        nodes_range = ni:(ni + length(nodesID) - 1) #SVector{2,integer}(length(nodesID), ni)
-        for nodeID ∈ nodesID
-            cell_nodes[ni] = nodeID 
-            ni += 1
-        end
-        # cell array loop
-        # faces_range = SVector{2,integer}(length(facesID), fi)
-        faces_range = fi:(fi + length(facesID) - 1) 
-        for j ∈ eachindex(facesID)
-            cell_faces[fi] = facesID[j]
-            cell_neighbours[fi] = neighbours[j]
-            cell_nsign[fi] = nsign[j]
-            fi += 1
-        end
-
-        # cell assignment
-        cells[i] = Cell{integer,float}(
-            cell.centre,
-            cell.volume,
-            nodes_range,
-            faces_range
-        ) |> cu
-    end
-
-    # PROCESSING FACES
-    # Calculate array size needed for all face nodes
-    nnodes = zero(integer)
-    for face ∈ mesh.faces
-        (; nodesID) = face 
-        nnodes += length(nodesID)
-    end
-
-    # Initialise arrays 
-    face_nodes = Vector{integer}(undef, nnodes)
-
-    ni = 1 # node counter
-    for (i, face) ∈ enumerate(mesh.faces)
-        (;nodesID) = face
-        # node array loop
-        nodes_range = ni:(ni + length(nodesID) - 1) #SVector{2,integer}(length(nodesID), ni)
-        for nodeID ∈ nodesID
-            face_nodes[ni] = nodeID 
-            ni += 1
-        end
-
-        # face assignment
-        faces[i] = Face2D{integer,float}(
-            nodes_range,
-            face.ownerCells,
-            face.centre,
-            face.normal,
-            face.e,
-            face.area,
-            face.delta,
-            face.weight
-        ) |> cu
-    end
-
-    Mesh2{Vector{Cell{integer,float}}, Vector{integer}, Vector{Face2D{integer,float}}, Vector{Boundary{Vector{integer}}}, Vector{Node{Vector{integer},float}}}(
-        cells,
-        cell_nodes,
-        cell_faces,
-        cell_neighbours,
-        cell_nsign,
-        faces,
-        face_nodes,
-        boundaries,
-        nodes,
-    ) |> cu
-end
-
 function test_kernel_cells!(mesh)
     i = threadIdx().x + (blockIdx().x-1)*blockDim().x
 
@@ -214,7 +108,7 @@ function test_kernel_boundaries!(mesh)
 
     (; boundaries) = mesh
 
-    for j in 1:length(boundaries)
+    for j in eachindex(boundaries)
 
         (; name, facesID, cellsID) = boundaries[j]
 
@@ -223,11 +117,11 @@ function test_kernel_boundaries!(mesh)
         @inbounds if i <= length(facesID) && i > 0
 
             facesID[i] = facesID[i] + facesID[i]
-            cellsID[j] = cellsID[j] + cellsID[j]
+            cellsID[i] = cellsID[i] + cellsID[i]
 
         end
 
-        boundaries[j] = Boundary(
+        boundaries[j] = Boundary{Vector{Int64}}(
             name,
             facesID,
             cellsID
@@ -251,6 +145,42 @@ function test_kernel_boundaries!(mesh)
 
 end
 
+function test_kernel_nodes!(mesh)
+    i = threadIdx().x + (blockIdx().x-1)*blockDim().x
+
+    (; nodes) = mesh
+
+    @inbounds if i <= length(nodes) && i > 0
+        (; coords, neighbourCells) = nodes[i]
+
+        coords = coords + coords
+
+        for j in eachindex(neighbourCells)
+            neighbourCells[j] = neighbourCells[j] + neighbourCells[j]
+        end
+
+        nodes[i] = Node{Vector{Int64},Float64}(
+            coords,
+            neighbourCells
+        )
+
+    end
+
+    mesh = Mesh2(
+        mesh.cells,
+        mesh.cell_nodes,
+        mesh.cell_faces,
+        mesh.cell_neighbours,
+        mesh.cell_nsign,
+        mesh.faces,
+        mesh.face_nodes,
+        mesh.boundaries,
+        nodes,
+    )
+
+    return nothing
+
+end
 # backwardFacingStep_2mm, backwardFacingStep_10mm
 mesh_file = "unv_sample_meshes/backwardFacingStep_10mm.unv"
 unv_mesh = build_mesh(mesh_file, scale=0.001)
@@ -268,8 +198,6 @@ mesh.cell_nodes
 mesh.cell_faces
 mesh.cells[1].centre
 
-
-
 # FACES
 mesh.faces[1].ownerCells
 mesh.face_nodes
@@ -280,9 +208,20 @@ mesh.faces[1].ownerCells
 mesh.face_nodes
 
 
-# boundaries
-
-mesh.boundaries[1].facesID
-
+# BOUNDARIES
+mesh.boundaries
 @cuda threads = 1024 test_kernel_boundaries!(mesh)
+mesh.boundaries
 
+# NODES
+#=
+    potential to use pointer/range method to index neighbour cells since the vectors are not of the same size
+        possibly more performant than eachindex()
+=#
+mesh.nodes[1].coords
+mesh.nodes[1].neighbourCells
+
+@cuda threads = 1024 blocks = cld(length(mesh.nodes),1024) test_kernel_nodes!(mesh)
+
+mesh.nodes[1].coords
+mesh.nodes[1].neighbourCells

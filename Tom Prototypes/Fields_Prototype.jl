b7d3d345874e01e7fbfe500f3d93d80c6c8032f3
TomMazin <egytm1@nottingham.ac.uk>
2023-11-25 18:19:47 +0000

    Tested new FaceScalarField struct in kernel

--- Tom Prototypes/Fields_Prototype.jl
+++ Tom Prototypes/Fields_Prototype.jl
@@ -16,7 +16,6 @@ scalarField = ScalarField(mesh)
 (; values, mesh, BCs) = scalarField
 
 values = cu(values)
-mesh = cu(mesh)
 BCs = cu(BCs)
 
 scalarField = ScalarField(values, mesh, BCs)
@@ -39,7 +38,89 @@ function test_kernel_SF!(scalarField)
 end
 
 scalarField.values
-
-@device_code_warntype @cuda threads = 1024 blocks = cld(length(scalarField.values),1024) test_kernel_SF!(scalarField)
-
+@cuda threads = 1024 blocks = cld(length(scalarField.values),1024) test_kernel_SF!(scalarField)
 scalarField.values
+
+## FACE SCALAR FIELD
+faceScalarField = FaceScalarField(mesh)
+
+(; values, mesh) = faceScalarField
+values = cu(values)
+faceScalarField = FaceScalarField(values, mesh)
+
+function test_kernel_FSF!(faceScalarField)
+    i = threadIdx().x + (blockIdx().x-1)*blockDim().x
+
+    (; values) = faceScalarField
+    
+    @inbounds if i <= length(values) && i > 0
+
+
+        if values[i] == 0
+            values[i] = values[i] + 1
+        else
+            values[i] = values[i] + values[i]
+        end
+
+    end
+
+    # (; mesh) = faceScalarField
+
+    # (; cells, cell_nodes, cell_faces, cell_neighbours, cell_nsign) = mesh
+    
+    # @inbounds if i <= length(cells) && i > 0
+
+    #     (; centre, volume, nodes_range, faces_range) = cells[i]
+        
+    #     for j in nodes_range
+
+    #     cell_nodes[j] = cell_nodes[j] + cell_nodes[j]
+
+    #     end
+
+    #     for k in faces_range
+
+    #         cell_faces[k] = cell_faces[k]+cell_faces[k]
+    #         cell_neighbours[k] = cell_neighbours[k] + cell_neighbours[k]
+    #         cell_nsign[k] = cell_nsign[k] + cell_nsign[k]
+
+    #     end
+
+    #     centre = centre + centre
+    #     volume = volume + volume
+
+    #     cells[i] = Cell{Int64, Float64}(
+    #         centre,
+    #         volume,
+    #         nodes_range,
+    #         faces_range
+    #     )
+    # end
+
+    # mesh = Mesh2(
+    #     cells,
+    #     cell_nodes,
+    #     cell_faces,
+    #     cell_neighbours,
+    #     cell_nsign,
+    #     mesh.faces,
+    #     mesh.face_nodes,
+    #     mesh.boundaries,
+    #     mesh.nodes,
+    # )
+
+    return nothing
+
+end
+
+faceScalarField.mesh.cell_nodes
+faceScalarField.mesh.cell_faces
+faceScalarField.mesh.cells[1].centre
+faceScalarField.values
+
+@cuda threads = 1024 blocks = cld(length(faceScalarField.values),1024) test_kernel_FSF!(faceScalarField)
+
+faceScalarField.mesh.cell_nodes
+faceScalarField.mesh.cell_faces
+faceScalarField.mesh.cells[1].centre
+faceScalarField.values

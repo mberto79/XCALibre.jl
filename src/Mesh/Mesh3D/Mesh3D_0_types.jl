9b85721e3fbd0d8b41e7b95fe912967138125cf2
OscarOsborn <oosbornwork@gmail.com>
2023-12-04 19:46:50 +0000

    change

--- src/Mesh/Mesh3D/Mesh3D_0_types.jl
+++ src/Mesh/Mesh3D/Mesh3D_0_types.jl
@@ -11,13 +11,8 @@ struct Face3D{I,F}
     weight::F
 end
 Adapt.@adapt_structure Face3D
-Face3D(I,F) = begin
-    zi = zero(I); zf = zero(F)
-    vec_2I = SVector{2,I}(zi,zi)
-    vec_3I=SVector{3,I}(zi,zi,zi)
-    vec_3F = SVector{3,F}(zf,zf,zf)
-    Face3D(vec_3I, vec_2I, vec_3F, vec_3F, vec_3F, zf, zf, zf)
-end
+
+abstract type AbstractMesh end
 
 struct Mesh3{VC<:AbstractArray{Cell},VN<:AbstractArray{Node},VF<:AbstractArray{Face3D},VB<:AbstractArray{Boundary},VI<:AbstractArray{Int}} <: AbstractMesh
     cells::VC

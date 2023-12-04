5a0c4cde1cd3eac973aaaef2bd8936e0b3d2f9b7
OscarOsborn <oosbornwork@gmail.com>
2023-12-04 19:59:59 +0000

    modified UNV_3D types

--- src/UNV_3D/UNV_3D_NS_Types.jl
+++ src/UNV_3D/UNV_3D_NS_Types.jl
@@ -1,35 +1,3 @@
-struct Node{TI, TF}
-    coords::SVector{3, TF}
-    neighbourCells::Vector{TI}
-end
-Adapt.@adapt_structure Node
-Node(TF) = begin
-    zf = zero(TF)
-    vec_3F = SVector{3,TF}(zf,zf,zf)
-    Node(vec_3F, Int64[])
-end
-Node(x::F, y::F, z::F) where F<:AbstractFloat = Node(SVector{3, F}(x,y,z), Int64[])
-Node(zero::F) where F<:AbstractFloat = Node(zero, zero, zero)
-Node(vector::F) where F<:AbstractVector = Node(vector, Int64[])
-
-struct Boundary{I}
-    name::Symbol
-    # nodesID::Vector{I} # can be deduced from face info
-    facesID::Vector{I}
-    cellsID::Vector{I}
-    # normal::SVector{3, F} # correctly aligned by definition
-end
-Adapt.@adapt_structure Boundary
-
-struct Cell{I,F}
-    centre::SVector{3, F}
-    volume::F
-    nodes_range::UnitRange{I}
-    # faces_map::SVector{2,I}
-    faces_range::UnitRange{I}
-end
-Adapt.@adapt_structure Cell
-
 struct Point{TF<:AbstractFloat}
     xyz::SVector{3, TF}
 end
@@ -69,35 +37,3 @@ mutable struct Element{TI<:Integer}
     elements::Vector{TI}
 end
 Element(z::TI) where TI<:Integer = Element(0,0,TI[])
-
-struct Face3D{I,F}
-    nodes_range::UnitRange{I}
-    ownerCells::SVector{2,I}
-    centre::SVector{3, F}
-    normal::SVector{3, F}
-    e::SVector{3, F}
-    area::F
-    delta::F
-    weight::F
-end
-Adapt.@adapt_structure Face3D
-Face3D(I,F) = begin
-    zi = zero(I); zf = zero(F)
-    vec_2I = SVector{2,I}(zi,zi)
-    vec_3I=SVector{3,I}(zi,zi,zi)
-    vec_3F = SVector{3,F}(zf,zf,zf)
-    Face3D(vec_3I, vec_2I, vec_3F, vec_3F, vec_3F, zf, zf, zf)
-end
-
-struct Mesh3{I,F} #<: AbstractMesh
-    cells::Vector{Cell{I,F}}
-    cell_nodes::Vector{I}
-    cell_faces::Vector{I}
-    cell_neighbours::Vector{I}
-    cell_nsign::Vector{I}
-    faces::Vector{Face3D{I,F}}
-    face_nodes::Vector{I}
-    boundaries::Vector{Boundary{I}}
-    nodes::Vector{Node{I,F}}
-end
-Adapt.@adapt_structure Mesh3

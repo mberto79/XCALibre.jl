4a67301a03ef78da0bd44ef2675d2b08d6777091
OscarOsborn <oosbornwork@gmail.com>
2023-12-04 19:58:29 +0000

    UNV_3D module

--- src/UNV_3D/UNV_3D.jl
+++ src/UNV_3D/UNV_3D.jl
@@ -1,13 +1,14 @@
+module UNV_3D
+
 using StaticArrays
 using LinearAlgebra
 using Setfield
 using Adapt
 using Printf
+using FVM_1D.Mesh3D
 
 include("UNV_3D_NS_Types.jl")
 include("UNV_3D_NS_Reader.jl")
 include("UNV_3D_NS_Builder.jl")
 
-unv_mesh="src/UNV_3D_NewStructure/tetra_singlecell.unv"
-
-build_mesh3D(unv_mesh)
+end

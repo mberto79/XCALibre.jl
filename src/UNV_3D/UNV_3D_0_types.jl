35d38eac84d8914ba7ff42295908d66285ebdb9e
OscarOsborn <oosbornwork@gmail.com>
2023-12-04 20:33:01 +0000

    all types now parametric

--- src/UNV_3D/UNV_3D_0_types.jl
+++ src/UNV_3D/UNV_3D_0_types.jl
@@ -1,39 +1,39 @@
-struct Point{TF<:AbstractFloat}
-    xyz::SVector{3, TF}
+struct Point{F<:AbstractFloat, SV3<:SVector{3,F}}
+    xyz::SV3
 end
 Point(z::TF) where TF<:AbstractFloat = Point(SVector{3, TF}(zero(TF), zero(TF), zero(TF)))
 
-mutable struct Edge{TI<:Integer} 
-    edgeindex::TI
-    edgeCount::TI
-    edges::Vector{TI}
+mutable struct Edge{I<:Integer,VI<:AbstractArray{I}} 
+    edgeindex::I
+    edgeCount::I
+    edges::VI
 end
 Edge(z::TI) where TI<:Integer = Edge(0 , 0, TI[])
 
-mutable struct Face{TI<:Integer} 
-    faceindex::TI
-    faceCount::TI
-    faces::Vector{TI}
+mutable struct Face{I<:Integer, VI<:AbstractArray{I}} 
+    faceindex::I
+    faceCount::I
+    faces::VI
 end
 Face(z::TI) where TI<:Integer = Face(0 , 0, TI[])
 
-mutable struct Volume{TI<:Integer}
-    volumeindex::TI
-    volumeCount::TI
-    volumes::Vector{TI}
+mutable struct Volume{I<:Integer,VI<:AbstractArray{I}}
+    volumeindex::I
+    volumeCount::I
+    volumes::VI
 end
 Volume(z::TI) where TI<:Integer = Volume(0 , 0, TI[])
 
-mutable struct BoundaryElement{TI<:Integer}
-    name::String
-    boundaryNumber::TI
-    elements::Vector{TI}
+mutable struct BoundaryElement{S<:String,I<:Integer,VI<:AbstractArray{I}}
+    name::S
+    boundaryNumber::I
+    elements::VI
 end
 BoundaryElement(z::TI) where TI<:Integer = BoundaryElement("default", 0, TI[])
 
-mutable struct Element{TI<:Integer}
-    index::TI
-    elementCount::TI
-    elements::Vector{TI}
+mutable struct Element{I<:Integer,VI<:AbstractArray{I}}
+    index::I
+    elementCount::I
+    elements::VI
 end
 Element(z::TI) where TI<:Integer = Element(0,0,TI[])

b18d34ef90db501f82d19fc0b909d6d8f71d39ef
Humberto Medina <h.medina@aerofluids.org>
2024-01-15 18:32:31 +0000

    update proto file

--- proto_new_mesh_adaptation.jl
+++ proto_new_mesh_adaptation.jl
@@ -1,1 +1,89 @@
+using Plots
 using FVM_1D
+using Krylov
+
+# backwardFacingStep_2mm, backwardFacingStep_10mm
+mesh_file = "unv_sample_meshes/backwardFacingStep_10mm.unv"
+mesh = build_mesh(mesh_file, scale=0.001)
+
+# using FVM_1D.Mesh
+update_mesh_format(mesh)
+test()
+
+velocity = [0.5, 0.0, 0.0]
+nu = 1e-3
+Re = velocity[1]*0.1/nu
+
+model = RANS{Laminar}(mesh=mesh, viscosity=ConstantScalar(nu))
+
+@assign! model U (
+    Dirichlet(:inlet, velocity),
+    Neumann(:outlet, 0.0),
+    Dirichlet(:wall, [0.0, 0.0, 0.0]),
+    Dirichlet(:top, [0.0, 0.0, 0.0])
+)
+
+ @assign! model p (
+    Neumann(:inlet, 0.0),
+    Dirichlet(:outlet, 0.0),
+    Neumann(:wall, 0.0),
+    Neumann(:top, 0.0)
+)
+
+schemes = (
+    U = set_schemes(),
+    p = set_schemes()
+)
+
+
+solvers = (
+    U = set_solver(
+        model.U;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = DILU(),
+        convergence = 1e-7,
+        relax       = 0.8,
+    ),
+    p = set_solver(
+        model.p;
+        solver      = GmresSolver, # BicgstabSolver, GmresSolver
+        preconditioner = LDL(),
+        convergence = 1e-7,
+        relax       = 0.2,
+    )
+)
+
+runtime = set_runtime(
+    iterations=1000, time_step=1, write_interval=100)
+
+config = Configuration(
+    solvers=solvers, schemes=schemes, runtime=runtime)
+
+GC.gc()
+
+initialise!(model.U, velocity)
+initialise!(model.p, 0.0)
+
+Rx, Ry, Rp = simple!(model, config) # 9.39k allocs
+
+plot(; xlims=(0,184))
+plot!(1:length(Rx), Rx, yscale=:log10, label="Ux")
+plot!(1:length(Ry), Ry, yscale=:log10, label="Uy")
+plot!(1:length(Rp), Rp, yscale=:log10, label="p")
+
+# # PROFILING CODE
+
+# using Profile, PProf
+
+# GC.gc()
+# initialise!(U, velocity)
+# initialise!(p, 0.0)
+
+# Profile.Allocs.clear()
+# Profile.Allocs.@profile sample_rate=1 begin Rx, Ry, Rp = isimple!(
+#     mesh, nu, U, p,
+#     # setup_U, setup_p, iterations, pref=0.0)
+#     setup_U, setup_p, iterations)
+# end
+
+# PProf.Allocs.pprof()

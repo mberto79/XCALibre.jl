e5becb254df99516c1586e6c38a0d08f7a3cacbe
Humberto Medina <h.medina@aerofluids.org>
2024-01-15 18:32:16 +0000

    Add new file to hold new update mesh function

---
+++ src/Mesh/Mesh2D/Mesh2D_0_update_mesh.jl
@@ -0,0 +1,11 @@
+import FVM_1D.UNV2
+
+export update_mesh_format, test
+
+update_mesh_format(mesh::UNV2.Mesh2) = begin
+    println("Update to new mesh format - temp solution")
+end
+
+test() = begin
+    println("I'm here")
+end

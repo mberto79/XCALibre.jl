75348c5fa50d7eea3d892d8aca49266a24165503
Humberto Medina <h.medina@aerofluids.org>
2024-01-23 16:18:40 +0000

    update tutorial to prevent writing to file

--- CASE_UNV_BFS.jl
+++ CASE_UNV_BFS.jl
@@ -3,7 +3,7 @@ using FVM_1D
 using Krylov
 
 # backwardFacingStep_2mm, backwardFacingStep_10mm
-mesh_file = "unv_sample_meshes/backwardFacingStep_2mm.unv"
+mesh_file = "unv_sample_meshes/backwardFacingStep_10mm.unv"
 mesh = build_mesh(mesh_file, scale=0.001)
 # mesh = update_mesh_format(mesh; integer=Int32, float=Float32)
 mesh = update_mesh_format(mesh)
@@ -52,7 +52,7 @@ solvers = (
 )
 
 runtime = set_runtime(
-    iterations=1000, time_step=1, write_interval=100)
+    iterations=1000, time_step=1, write_interval=-1)
 
 config = Configuration(
     solvers=solvers, schemes=schemes, runtime=runtime)

e996ad9bbe839e3e9388908a460b8db11506fbc5
TomMazin <egytm1@nottingham.ac.uk>
2024-02-20 19:41:56 +0000

    Added _conert_array() function to generalise array conversion to GPU based on backend

--- src/Mesh/Mesh_1_functions.jl
+++ src/Mesh/Mesh_1_functions.jl
@@ -1,4 +1,4 @@
-export _get_float, _get_int, _get_backend
+export _get_float, _get_int, _get_backend, _convert_array
 export total_boundary_faces, boundary_index
 # export number_symbols
 export x, y, z # access cell centres
@@ -7,6 +7,13 @@ export xf, yf, zf # access face centres
 _get_int(mesh) = eltype(mesh.get_int)
 _get_float(mesh) = eltype(mesh.get_float)
 _get_backend(mesh) = get_backend(mesh.cells)
+
+function _convert_array(arr, backend::CPU)
+    return arr
+end
+function _convert_array(arr, backend::CUDABackend)
+    return adapt(CuArray, arr)
+end
 
 # function total_boundary_faces(mesh::Mesh2{I,F}) where {I,F}
 function total_boundary_faces(mesh::Mesh2)

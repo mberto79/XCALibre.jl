936669116d046bc52944b74a90ec7c42e3ca37a7
TomMazin <egytm1@nottingham.ac.uk>
2024-02-20 19:42:40 +0000

    Prototyping update

--- prototyping.jl
+++ prototyping.jl
@@ -138,13 +138,13 @@ using KernelAbstractions
     end
 
     CUDA.allowscalar(false)
-    model = adapt(CuArray, model)
-    ∇p = adapt(CuArray, ∇p)
-    ux_eqn = adapt(CuArray, ux_eqn)
-    uy_eqn = adapt(CuArray, uy_eqn)
-    p_eqn = adapt(CuArray, p_eqn)
-    turbulence = adapt(CuArray, turbulence)
-    config = adapt(CuArray, config)
+    # model = adapt(CuArray, model)
+    # ∇p = adapt(CuArray, ∇p)
+    # ux_eqn = adapt(CuArray, ux_eqn)
+    # uy_eqn = adapt(CuArray, uy_eqn)
+    # p_eqn = adapt(CuArray, p_eqn)
+    # turbulence = adapt(CuArray, turbulence)
+    # config = adapt(CuArray, config)
     
     # Extract model variables and configuration
     (;mesh, U, p, nu) = model
@@ -188,80 +188,259 @@ using KernelAbstractions
     R_uy = ones(TF, iterations)
     R_p = ones(TF, iterations)
 
-    Uf = adapt(CuArray,Uf)
-    rDf = adapt(CuArray, rDf)
-    rD = adapt(CuArray, rD)
+    # Uf = adapt(CuArray,Uf)
+    # rDf = adapt(CuArray, rDf)
+    # rD = adapt(CuArray, rD)
 
-    interpolate!(Uf, U) 
-    @time begin interpolate!(rDf, rD) end  
+    interpolate!(Uf, U)
+    correct_boundaries!(Uf, U, U.BCs)
 
-## SCALAR INTERPOLATION
+    sum = 0
 
-CUDA.allowscalar(true)
+    CUDA.allowscalar(true)
 
-rDf.values[100]
-rD.values[100]
+    for i in eachindex(U)
+        num = U[i][1]
+        if num != 0.5
+            sum += 1
+            println("Sum incremented")
+        end
+    end
 
-CUDA.allowscalar(false)
+    println("$sum")
+    length(U)
+    length(Uf.x)
+    
+    # IDs = Array{typeof(BCs[1].ID)}(undef,length(BCs))
+    # values = _get_float(mesh)[]
 
-@time begin interpolate_gpu!(rDf, rD) end
+    function _convert_array(arr, backend::CPU)
+        return arr
+    end
+    function _convert_array(arr, backend::CUDABackend)
+        return adapt(CuArray, arr)
+    end
 
-CUDA.allowscalar(true)
+    BCs = U.BCs
+    psif = Uf
+    psi = U
+    (; x, y, z, mesh) = psif
+    (; boundaries) = mesh 
+    
 
-rDf.values[100]
-rD.values[100]
+    boundaries_cpu = Array{eltype(mesh.boundaries)}(undef, length(boundaries))
+    copyto!(boundaries_cpu, boundaries)
+
+    backend = _get_backend(psif.mesh)
+    kernel_dirichlet = adjust_boundary_dirichlet_vector!(backend)
+    kernel_neumann = adjust_boundary_neumann_vector!(backend)
+
+    for i in eachindex(BCs)
+        (; ID) = BCs[i]
+        (; facesID, cellsID) = boundaries_cpu[ID]
+        facesID = _convert_array(facesID, backend)
+        cellsID = _convert_array(cellsID, backend)
+        #KERNEL LAUNCH
+        call_adjust_boundary(BCs[i], psif, psi, facesID, cellsID, x, y, z)
+    end
+
+    CUDA.allowscalar(true)
+
+    psif.x[50]
+    sum = 0
+
+    for i in eachindex(psi)
+        num = psi[i][1]
+        if num != 0.5
+            sum += 1
+        end
+    end
+
+    println("$sum") # aim for 158 and 0
+    length(x)
+
+    function call_adjust_boundary(BC::Dirichlet, psif::FaceVectorField, psi::VectorField, facesID, cellsID, x, y, z)
+        kernel_dirichlet(BC, psif, psi, facesID, cellsID, x, y, z, ndrange = length(facesID))
+    end
+
+    function call_adjust_boundary(BC::Neumann, psif, psi, facesID, cellsID, x, y, z)
+        kernel_neumann(BC, psif, psi, facesID, cellsID, x, y, z, ndrange = length(facesID))
+    end
+
+    @kernel function adjust_boundary_dirichlet_vector!(BC, psif, psi, facesID, cellsID, x, y, z)
+        i = @index(Global)
+        @inbounds begin
+            fID = facesID[i]
+            x[fID] = BC.value[1]
+            y[fID] = BC.value[2]
+            z[fID] = BC.value[3]
+        end
+    end
+
+    @kernel function adjust_boundary_neumann_vector!(BC, psif, psi, facesID, cellsID, x, y, z)
+        i = @index(Global)
+        @inbounds begin
+            fID = facesID[i]
+            cID = cellsID[i]
+            psi_cell = psi[cID]
+            # normal = faces[fID].normal
+            # Line below needs sorting out for general user-defined gradients
+            # now only works for zero gradient
+            # psi_boundary =   psi_cell - (psi_cell⋅normal)*normal
+            x[fID] = psi_cell[1]
+            y[fID] = psi_cell[2]
+            z[fID] = psi_cell[3]
+        end
+    end
+
+    function adjust_boundary!( 
+        BC::Neumann, psif::FaceVectorField, psi::VectorField, boundary, faces
+        ) 
+    
+        (; x, y, z) = psif
+        (; facesID, cellsID) = boundary
+    
+        @inbounds for fi ∈ eachindex(facesID)
+            fID = facesID[fi]
+            cID = cellsID[fi]
+            psi_cell = psi[cID]
+            # normal = faces[fID].normal
+            # Line below needs sorting out for general user-defined gradients
+            # now only works for zero gradient
+            # psi_boundary =   psi_cell - (psi_cell⋅normal)*normal
+            x[fID] = psi_cell[1]
+            y[fID] = psi_cell[2]
+            z[fID] = psi_cell[3]
+        end
+    end
 
 
-# function interpolate!(phif::FaceScalarField, phi::ScalarField) 
-#     vals = phi.values 
-#     fvals = phif.values
-#     mesh = phi.mesh 
-#     faces = mesh.faces
-#     @inbounds for fID ∈ eachindex(faces)
-#         # (; weight, ownerCells) = faces[fi]
-#         face = faces[fID]
-#         weight = face.weight
-#         ownerCells = face.ownerCells
-#         phi1 = vals[ownerCells[1]]
-#         phi2 = vals[ownerCells[2]]
-#         one_minus_weight = 1 - weight
-#         fvals[fID] = weight*phi1 + one_minus_weight*phi2 # check weight is used correctly!
-#     end
-# end
+    CUDA.allowscalar(true)
+
+    (; facesID) = mesh.boundaries[IDs[1]]
+
+
+    values
+
+
+    kernel(BC, IDs, ndrange = length(IDs))
+
+    @kernel function adjust_boundary(BC::Dirichlet, IDs)
+        i = @index(Global)
+        
+        # boundary = boundaries[ID]
+
+        ID = IDs[i]
+
+        @cushow(ID)
+        
+        # adjust_boundary!(BCs[i], phif, phi, boundary, faces)
+    end
+    
+    function adjust_boundary!( 
+        BC::Dirichlet, psif::FaceVectorField, psi::VectorField, boundary, faces
+        )
+    
+        (; x, y, z) = psif
+        (; facesID) = boundary
+    
+        @inbounds for fID ∈ facesID
+            x[fID] = BC.value[1]
+            y[fID] = BC.value[2]
+            z[fID] = BC.value[3]
+        end
+    end
+
+    U.BCs.ID
+    flux!(mdotf, Uf)
+    grad!(∇p, pf, p, p.BCs)
 
 
 
-function interpolate_gpu!(phif::FaceScalarField, phi::ScalarField)
-    # Extract values arrays from scalar fields 
-    vals = phi.values
-    fvals = phif.values
+## Tuple Test
 
-    # Extract faces from mesh
-    mesh = rDf.mesh
-    faces = mesh.faces
+using KernelAbstractions
+using Adapt
+using CUDA
 
-    # Launch interpolate kernel
-    backend = _get_backend(mesh)
-    kernel! = interpolate_Scalar!(backend)
-    kernel!(fvals, vals, faces, ndrange = length(faces))
+A = (1, 2, 3, 4, 5)
+typeof(A)
+
+A = cu(A)
+result = CUDA.zeros(eltype(A), length(A))
+
+backend = get_backend(result)
+kernel! = test1(backend)
+kernel!(A, result, ndrange = length(A))
+
+result
+
+@kernel function test1(A, result)
+    i = @index(Global)
+
+    @inbounds result[i] = A[i]
+
 end
 
-@kernel function interpolate_Scalar!(fvals, vals, faces)
-    # Define index for thread
+
+struct tuple_parent1{V<:AbstractArray{tuple_test}}
+    tuple_test::V
+    int1::Integer
+    int2::Integer
+end
+Adapt.@adapt_structure tuple_parent1
+
+struct tuple_test{S<:Symbol, NT<:NTuple}
+    name::S
+    vec1::NT
+    vec2::NT
+end
+Adapt.@adapt_structure tuple_test
+
+child = tuple_test[]
+
+for i in 1:5
+    name = Symbol("child")
+    I = rand(5:10)
+    type = NTuple{I, Integer}
+    vec1 = type(rand(1:10) for j in 1:I)
+    vec2 = type(rand(1:10) for j in 1:I)
+    push!(child, tuple_test(name, vec1, vec2))
+end
+
+child
+
+parent = tuple_parent1(child, 100, 200)
+
+parent.tuple_test = adapt(CuArray, parent.tuple_test)
+
+parent = adapt(CuArray, parent)
+tuple = parent.tuple_test
+
+result1 = CUDA.zeros(Int64, 5)
+result2 = similar(result1)
+
+backend = get_backend(tuple[1].vec1)
+kernel! = tuple_test_kernel(backend)
+@device_code_warntype kernel!(tuple, result1, result2, ndrange = length(tuple))
+
+
+@kernel function tuple_test_kernel(tuple, result1, result2)
     i = @index(Global)
 
     @inbounds begin
-        # Deconstruct faces to use weight and ownerCells in calculations
-        (; weight, ownerCells) = faces[i]
-
-        # Calculate initial values based on index queried from ownerCells
-        phi1 = vals[ownerCells[1]]
-        phi2 = vals[ownerCells[2]]
-
-        # Calculate one minus weight
-        one_minus_weight = 1 - weight
-
-        # Update phif values array for interpolation
-        fvals[i] = weight*phi1 + one_minus_weight*phi2 # check weight is used correctly!
+        (; vec1, vec2) = tuple[i]
+        result1[i] = length(vec1)
+        result2[i] = length(vec2)
     end
 end
+
+
+
+A = CUDA.zeros(Int64, 100)
+B = []
+for i in 1:10
+    push!(B, A)
+end
+B
+cu(B)

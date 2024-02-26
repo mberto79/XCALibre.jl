e8fa4ad84ce7c08092335bad20de592a20669b75
OscarOsborn <oosbornwork@gmail.com>
2024-02-26 10:06:10 +0000

    test

---
+++ sandbox.jl
@@ -0,0 +1,21 @@
+using FVM_1D.UNV_3D
+
+using StaticArrays
+
+using Statistics
+
+using LinearAlgebra
+
+#unv_mesh="src/UNV_3D/internalface_test.unv"
+unv_mesh="src/UNV_3D/5_cell_new_boundaries.unv"
+#unv_mesh="src/UNV_3D/700_cell_case.unv"
+
+points,edges,faces,volumes,boundaryElements=load_3D(unv_mesh)
+
+points
+edges
+faces
+volumes
+boundaryElements
+
+mesh=build_mesh3D(unv_mesh)

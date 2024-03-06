b51f589cee734fdf3abe77d7054c98947f7099d0
TomMazin <egytm1@nottingham.ac.uk>
2024-03-06 16:22:33 +0000

    Updated VTK writer to be compatible with GPU and tested with CPU

--- src/VTK/VTK.jl
+++ src/VTK/VTK.jl
@@ -3,6 +3,8 @@ module VTK
 using FVM_1D.Mesh
 using FVM_1D.Fields
 using FVM_1D.RANSModels
+using CUDA
+using KernelAbstractions
 # using FVM_1D.Discretise
 # using FVM_1D.Models
 # using FVM_1D.Calculate

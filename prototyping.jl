db17b5b2ce199ce027ffd2ccce68f1c67ef5d28d
TomMazin <egytm1@nottingham.ac.uk>
2024-03-07 20:05:21 +0000

    Prototyping update

--- prototyping.jl
+++ prototyping.jl
@@ -1,44 +1,41 @@
-# using Plots
+using Plots
 using FVM_1D
 using Krylov
 using CUDA
 using KernelAbstractions
 
+# Select backend
+# backend = CPU()
+backend = CUDABackend()
 
-# Backend selection
-
-backend = CPU()
-# backend = CUDABackend()
-iteration = 1
-
-# quad, backwardFacingStep_2mm, backwardFacingStep_10mm, trig40
-
-mesh_file = "unv_sample_meshes/cylinder_d10mm_5mm.unv"
+# backwardFacingStep_2mm, backwardFacingStep_10mm
+mesh_file = "unv_sample_meshes/backwardFacingStep_10mm.unv"
 mesh = build_mesh(mesh_file, scale=0.001)
 mesh = update_mesh_format(mesh)
 
-# Inlet conditions
-velocity = [0.50, 0.0, 0.0]
-noSlip = [0.0, 0.0, 0.0]
+velocity = [0.5, 0.0, 0.0]
 nu = 1e-3
-Re = (0.2*velocity[1])/nu
+Re = velocity[1]*0.1/nu
 
 model = RANS{Laminar}(mesh=mesh, viscosity=ConstantScalar(nu))
 
-@assign! model U ( 
+@assign! model U (
     Dirichlet(:inlet, velocity),
     Neumann(:outlet, 0.0),
-    Dirichlet(:cylinder, noSlip),
-    Neumann(:bottom, 0.0),
+    Dirichlet(:wall, [0.0, 0.0, 0.0]),
+    Dirichlet(:top, [0.0, 0.0, 0.0])
+)
+
+ @assign! model p (
+    Neumann(:inlet, 0.0),
+    Dirichlet(:outlet, 0.0),
+    Neumann(:wall, 0.0),
     Neumann(:top, 0.0)
 )
 
-@assign! model p (
-    Neumann(:inlet, 0.0),
-    Dirichlet(:outlet, 0.0),
-    Neumann(:cylinder, 0.0),
-    Neumann(:bottom, 0.0),
-    Neumann(:top, 0.0)
+schemes = (
+    U = set_schemes(time=Euler),
+    p = set_schemes()
 )
 
 solvers = (
@@ -47,23 +44,19 @@ solvers = (
         solver      = GmresSolver, # BicgstabSolver, GmresSolver
         preconditioner = Jacobi(),
         convergence = 1e-7,
-        relax       = 0.6,
+        relax       = 1.0,
     ),
     p = set_solver(
         model.p;
         solver      = GmresSolver, # BicgstabSolver, GmresSolver
         preconditioner = Jacobi(),
         convergence = 1e-7,
-        relax       = 0.4,
+        relax       = 1.0,
     )
 )
 
-schemes = (
-    U = set_schemes(divergence=Upwind, gradient=Midpoint),
-    p = set_schemes(divergence=Upwind, gradient=Midpoint)
-)
-
-runtime = set_runtime(iterations=600, write_interval=-1, time_step=1)
+runtime = set_runtime(
+    iterations=350, time_step=0.005, write_interval=10)
 
 config = Configuration(
     solvers=solvers, schemes=schemes, runtime=runtime)
@@ -132,7 +125,8 @@ using Printf
 
     @reset ux_eqn.preconditioner = set_preconditioner(
                     solvers.U.preconditioner, ux_eqn, U.x.BCs, runtime)
-    @reset uy_eqn.preconditioner = ux_eqn.preconditioner
+    @reset uy_eqn.preconditioner = set_preconditioner(
+                    solvers.U.preconditioner, uy_eqn, U.y.BCs, runtime)
     @reset p_eqn.preconditioner = set_preconditioner(
                     solvers.p.preconditioner, p_eqn, p.BCs, runtime)
 
@@ -157,8 +151,12 @@ using Printf
     (; solvers, schemes, runtime) = config
     (; iterations, write_interval) = runtime
     
-    mdotf = get_flux(ux_eqn, 2)
-    nueff = get_flux(ux_eqn, 3)
+    mdotfx = get_flux(ux_eqn, 2)
+    nueffx = get_flux(ux_eqn, 3)
+
+    mdotfy = get_flux(uy_eqn, 2)
+    nueffy = get_flux(uy_eqn, 3)
+    
     rDf = get_flux(p_eqn, 1)
     divHv = get_source(p_eqn, 1)
     
@@ -185,6 +183,7 @@ using Printf
 
     TF = _get_float(mesh)
     prev = zeros(TF, n_cells)
+    prev = _convert_array!(prev, backend)
 
     # Pre-allocate vectors to hold residuals 
 
@@ -192,16 +191,17 @@ using Printf
     R_uy = ones(TF, iterations)
     R_p = ones(TF, iterations)
 
-    Uf = _convert_array!(Uf, backend)
-    rDf = _convert_array!(rDf, backend)
-    rD = _convert_array!(rD, backend)
-    pf = _convert_array!(pf, backend)
-    Hv = _convert_array!(Hv, backend)
-    prev = _convert_array!(prev, backend)
+    # Uf = _convert_array!(Uf, backend)
+    # rDf = _convert_array!(rDf, backend)
+    # rD = _convert_array!(rD, backend)
+    # pf = _convert_array!(pf, backend)
+    # Hv = _convert_array!(Hv, backend)
+    
 
     interpolate!(Uf, U)
     correct_boundaries!(Uf, U, U.BCs)
-    flux!(mdotf, Uf)
+    flux!(mdotfx, Uf)
+    flux!(mdotfy, Uf)
     grad!(∇p, pf, p, p.BCs)
 
 
@@ -209,50 +209,282 @@ using Printf
 
     update_nueff!(nueff, nu, turbulence)
 
-    @. prev = U.x.values
+    # uy_eqn.preconditioner.A.nzval
     
-    for i in 1:600
+    # for i in 1:length(R_ux)
+        @. prev = U.x.values
+        # type = typeof(ux_eqn)
+        # println("$type")
         discretise!(ux_eqn, prev, runtime)
-        # KernelAbstractions.synchronize(backend)
-        apply_boundary_conditions!(ux_eqn, U.x.BCs)
-        # KernelAbstractions.synchronize(backend)
-        implicit_relaxation!(ux_eqn, prev, solvers.U.relax, mesh)
-        # KernelAbstractions.synchronize(backend)
-        update_preconditioner!(ux_eqn.preconditioner, mesh)
-        run!(ux_eqn, solvers.U, U.x)
-        residual!(R_ux, ux_eqn.equation, U.x, i) 
+        # apply_boundary_conditions!(ux_eqn, U.x.BCs)
+        # ux_eqn.b .-= divUTx
+        # implicit_relaxation!(ux_eqn, prev, solvers.U.relax, mesh)
+        # update_preconditioner!(ux_eqn.preconditioner, mesh)
+        # run!(ux_eqn, solvers.U, U.x) #opP=Pu.P, solver=solver_U)
+        # residual!(R_ux, ux_eqn.equation, U.x, i)
 
         @. prev = U.y.values
         discretise!(uy_eqn, prev, runtime)
-        apply_boundary_conditions!(uy_eqn, U.y.BCs)
-        # uy_eqn.b .-= divUTy
-        implicit_relaxation!(uy_eqn, prev, solvers.U.relax, mesh)
-        update_preconditioner!(uy_eqn.preconditioner, mesh)
-        run!(uy_eqn, solvers.U, U.y)
-        residual!(R_uy, uy_eqn.equation, U.y, i)
-    end
-    # ncpu = ux_eqn.equation.A.nzVal
-    # bcpu = ux_eqn.equation.b
-    # npreconcpu = ux_eqn.preconditioner.A.nzVal
-    # valuescpu = U.x.values
-    Ruycpu = R_uy
 
-    # error_check(valuesgpu, U.x.values, 10^-15)
-    error_check(Ruxcpu, R_ux, 10^-20)
+
+        model = uy_eqn.model
+        eqn = uy_eqn
+        
+            # quote
+                # Extract number of terms and sources
+                nTerms = length(model.terms)
+                nSources = length(model.sources)
+        
+                # Define variables for function
+                mesh = model.terms[1].phi.mesh
+                # precon = eqn.preconditioner
+        
+                # Deconstructors to get lower-level variables for function
+                # (; A, b) = eqn.equation
+                A_array = _A(eqn)
+                b_array = _b(eqn)
+                (; terms, sources) = model
+                (; faces, cells, cell_faces, cell_neighbours, cell_nsign) = mesh
+                
+                # Get types and create float(zero) and integer(one)
+                integer = _get_int(mesh)
+                float = _get_float(mesh)
+                backend = _get_backend(mesh)
+                fzero = zero(float) # replace with func to return mesh type (Mesh module)
+                ione = one(integer)
+        
+                # Deconstruct sparse array dependent on sparse arrays type
+                rowval_array = _rowval(A_array)
+                colptr_array = _colptr(A_array)
+                nzval_array = _nzval(A_array)
+                
+                # Kernel to set nzval array to 0
+                kernel! = set_nzval(backend)
+                kernel!(nzval_array, fzero, ndrange = length(nzval_array))
+                KernelAbstractions.synchronize(backend)
+                nzval_array
+        
+                # Set initial values for indexing of nzval array
+                cIndex = zero(integer) # replace with func to return mesh type (Mesh module)
+                nIndex = zero(integer) # replace with func to return mesh type (Mesh module)
+                offset = zero(integer)
+        
+                # Assign storage for sources arrays
+                # sources_field = Array{typeof(sources[1].field)}(undef, length(sources))
+                # sources_sign = Array{typeof(sources[1].sign)}(undef, length(sources))
+        
+                # # Populate sources arrays
+                # for i in eachindex(sources)
+                #     sources_field[i] = sources[i].field
+                #     sources_sign[i] = sources[i].sign
+                # end
+        
+                # # Copy sources arrays to required backend
+                # sources_field = _convert_array!(sources_field, backend)
+                # sources_sign = _convert_array!(sources_sign, backend)
+                # KernelAbstractions.synchronize(backend)
+        
+                # Set b array to 0
+                kernel! = set_b!(backend)
+                kernel!(fzero, b_array, ndrange = length(b_array))
+                KernelAbstractions.synchronize(backend)
+                b_array
+                # Run schemes and sources calculations on all terms
+        
+                for i in 1:nTerms
+                    schemes_and_sources!(model.terms[i], 
+                                        nTerms, nSources, offset, fzero, ione, terms, rowval_array,
+                                        colptr_array, nzval_array, cIndex, nIndex, b_array,
+                                        faces, cells, cell_faces, cell_neighbours, cell_nsign, integer,
+                                        float, backend, runtime, prev)
+                    KernelAbstractions.synchronize(backend)
+                end
+        
+                # Free unneeded backend memory
+                sources_field = nothing
+                sources_sign = nothing 
+                nzval_array = nothing
+                rowval_array = nothing
+                colptr_array = nothing
+        
+                # Run sources calculations on all sources
+                kernel! = sources!(backend)
+                for i in 1:nSources
+                    (; field, sign) = sources[i]
+                    kernel!(field, sign, cells, b_array, ndrange = length(cells))
+                    KernelAbstractions.synchronize(backend)
+                end
+                nothing
+            # end
+        
+        
+        # function sparse_array_deconstructor(arr::SparseArrays.SparseMatrixCSC)
+        #     (; rowval, colptr, nzval) = arr
+        #     return rowval, colptr, nzval
+        # end
+        
+        # function sparse_array_deconstructor(arr::CUDA.CUSPARSE.CuSparseMatrixCSC)
+        #     (; rowVal, colPtr, nzVal) = arr
+        #     return rowVal, colPtr, nzVal
+        # end
+        
+        @kernel function set_nzval(nzval, fzero)
+            i = @index(Global)
+        
+            @inbounds begin
+                nzval[i] = fzero
+            end
+        end
+        
+        # function check_for_precon!(nzval, precon::Tuple, backend)
+        #     nothing
+        # end
+        
+        # function check_for_precon!(nzval, precon, backend)
+        #     (; A) = precon
+        #     A_precon = A
+        #     copy_to_precon!(nzval, A_precon, backend)
+        # end
+        
+        # function copy_to_precon!(nzval, A::SparseArrays.SparseMatrixCSC, backend)
+        #     nothing
+        # end
+        
+        # function copy_to_precon!(nzval, A::CUDA.CUSPARSE.CuSparseMatrixCSC, backend)
+        #     rowval_precon, colptr_precon, nzval_precon = sparse_array_deconstructor(A)
+        
+        #     kernel! = copy_to_precon_kernel!(backend)
+        #     kernel!(nzval, nzval_precon, ndrange = length(nzval))
+        # end
+        
+        # @kernel function copy_to_precon_kernel!(nzval, nzval_precon)
+        #     i = @index(Global)
+        
+        #     @inbounds begin
+        #         nzval_precon[i] = nzval[i]
+        #     end
+        # end
+        using SparseArrays
+        _nzval(A::CUDA.CUSPARSE.CuSparseMatrixCSC) = A.nzVal
+        _nzval(A::SparseArrays.SparseMatrixCSC) = A.nzval
+        
+        _colptr(A::CUDA.CUSPARSE.CuSparseMatrixCSC) = A.colPtr
+        _colptr(A::SparseArrays.SparseMatrixCSC) = A.colptr
+        
+        _rowval(A::CUDA.CUSPARSE.CuSparseMatrixCSC) = A.rowVal
+        _rowval(A::SparseArrays.SparseMatrixCSC) = A.rowval 
+
+
+
+
+
+        # apply_boundary_conditions!(uy_eqn, U.y.BCs)
+        # # uy_eqn.b .-= divUTy
+        # implicit_relaxation!(uy_eqn, prev, solvers.U.relax, mesh)
+        # update_preconditioner!(uy_eqn.preconditioner, mesh)
+        # run!(uy_eqn, solvers.U, U.y)
+        # residual!(R_uy, uy_eqn.equation, U.y, iteration)
+          
+        # inverse_diagonal!(rD, ux_eqn.equation)
+        # interpolate!(rDf, rD)
+        # remove_pressure_source!(ux_eqn, uy_eqn, ∇p)
+        # H!(Hv, U, ux_eqn, uy_eqn)
+        
+        # interpolate!(Uf, Hv) # Careful: reusing Uf for interpolation
+        # correct_boundaries!(Uf, Hv, U.BCs)
+        # div!(divHv, Uf)
+        
+        # @. prev = p.values
+        # discretise!(p_eqn, prev, runtime)
+        # apply_boundary_conditions!(p_eqn, p.BCs)
+        # setReference!(p_eqn, nothing, 1)
+        # update_preconditioner!(p_eqn.preconditioner, mesh)
+        # run!(p_eqn, solvers.p, p)
+
+        # explicit_relaxation!(p, prev, solvers.p.relax)
+        # residual!(R_p, p_eqn.equation, p, i)
+
+        # grad!(∇p, pf, p, p.BCs) 
+
+        # correct = false
+        # if correct
+        #     ncorrectors = 1
+        #     for i ∈ 1:ncorrectors
+        #         discretise!(p_eqn)
+        #         apply_boundary_conditions!(p_eqn, p.BCs)
+        #         setReference!(p_eqn.equation, pref, 1)
+        #         # grad!(∇p, pf, p, pBCs) 
+        #         interpolate!(gradpf, ∇p, p)
+        #         nonorthogonal_flux!(pf, gradpf) # careful: using pf for flux (not interpolation)
+        #         correct!(p_eqn.equation, p_model.terms.term1, pf)
+        #         run!(p_model, solvers.p)
+        #         grad!(∇p, pf, p, pBCs) 
+        #     end
+        # end
+
+        # correct_velocity!(U, Hv, ∇p, rD)
+        # interpolate!(Uf, U)
+        # correct_boundaries!(Uf, U, U.BCs)
+        # flux!(mdotf, Uf)
+    # end
+
+    xncpu = ux_eqn.equation.A.nzval
+    xpreconcpu = ux_eqn.preconditioner.A.nzval
+    xbcpu = ux_eqn.equation.b
+
+    yncpu = uy_eqn.equation.A.nzVal
+    ypreconcpu = uy_eqn.preconditioner.A.nzVal
+    ybcpu = uy_eqn.equation.b
+
+    (; A) = uy_eqn.equation
+    nzval_array = _nzval(A)
+
+    kernel! = test(backend)
+    kernel!(nzval_array, ndrange = length(nzval_array))
+    nzval_array
+
+    @kernel function test(nzval_test)
+        i = @index(Global)
+
+        nzval_test[i] += nzval_test[i] + 1
+    end
+
+
+    pcpu = p_eqn.equation.A.nzval
+    ppreconcpu = p_eqn.preconditioner.A.nzval
+    # npreconcpu = p_eqn.preconditioner.A.nzval
+    # pstoragecpu = p_eqn.preconditioner.storage
+    # xstoragecpu = ux_eqn.preconditioner.storage
+    # ystoragecpu = uy_eqn.preconditioner.storage
+    # valuescpu = U.x.values
+    # Ruycpu = R_uy
+    # valscpu = rD.values
+    # vals2cpu = rDf.values
+    # vals = divHv.values
+    # yvals = Uf.y.values
+    cyncpu
+    uy_eqn.equation.A.nzVal
+    uy_eqn.preconditioner.A.nzVal
+
+    error_check(xncpu, ux_eqn.equation.A.nzVal, 10^-15)
+    error_check(xpreconcpu, ux_eqn.preconditioner.A.nzVal, 10^-15)
+    error_check(xbcpu, ux_eqn.equation.b, 10^-15)
+
+
+    error_check(yncpu, uy_eqn.equation.A.nzVal, 0)
+    error_check(ypreconcpu, uy_eqn.preconditioner.A.nzVal, 0)
+    error_check(ybcpu, uy_eqn.equation.b, 10^-15)
 
     function error_check(arr1, arr2, target)
         CUDA.allowscalar(true)
         sum = 0
         for i in eachindex(arr1)
-            # if arr1[i] != arr2[i]
-                diff = arr1[i]-arr2[i]
-                if abs(diff) > target
-                    sum += 1
-                    println("At index $i: diff = $diff")
-                end
-            # end
+            diff = arr1[i]-arr2[i]
+            if abs(diff) > target
+                sum += 1
+                println("At index $i: diff = $diff")
+            end
         end
-        
         println("$sum elements are out of the specified error")
     end
 
@@ -410,3 +642,6 @@ using Printf
     mul!(res2, A, mulvec)
 
     error_check(res1, res2, 10^-15)
+
+
+    model_cpu = similar(model)

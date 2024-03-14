c36bb4ab3c99c540239a689e08092cfe4316233c
Humberto <h.medina@aerofluids.org>
2024-03-14 02:25:38 +0000

    generated functions for BCs

--- src/Discretise/Discretise_4_apply_bcs.jl
+++ src/Discretise/Discretise_4_apply_bcs.jl
@@ -6,8 +6,48 @@ end
 
 function _apply_boundary_conditions!(
     model::Model{TN,SN,T,S}, BCs::B, eqn) where {T,S,TN,SN,B}
+    nTerms = length(model.terms)
 
-    # Unpack terms that make up the model (not sources)
+    # Define variables for function
+    mesh = model.terms[1].phi.mesh
+    A = _A(eqn)
+    b = _b(eqn)
+    (; boundaries, faces, cells, boundary_cellsID) = mesh
+
+    # Deconstruct sparse array dependent on sparse arrays type
+    rowval = _rowval(A)
+    colptr = _colptr(A)
+    nzval = _nzval(A)
+
+    # Get types and create integer(one)
+    backend = _get_backend(mesh)
+    integer = _get_int(mesh)
+    ione = one(integer)
+
+    # Execute function to apply boundary conditions for all terms and boundaries
+    # for bci in 1:length(BCs)
+    #     for t in 1:nTerms
+    #         Execute_apply_boundary_condition_kernel!(BCs[bci], model.terms[t], 
+    #                                                 backend, boundaries, faces, cells,
+    #                                                 boundary_cellsID, ione, rowval_array,
+    #                                                 colptr_array, nzval_array, b_array)
+    #         KernelAbstractions.synchronize(backend)
+    #     end
+    # end
+    for BC ∈ BCs
+        CUDA.@allowscalar start_ID = mesh.boundaries[BC.ID].IDs_range[1]
+        CUDA.@allowscalar facesID_range = mesh.boundaries[BC.ID].IDs_range
+        kernel! = apply_boundary_conditions_kernel!(backend)
+        kernel!(
+            model, BC, model.terms, faces, cells, start_ID, boundary_cellsID, rowval, colptr, nzval, b, ione, ndrange=length(facesID_range)
+            )
+        KernelAbstractions.synchronize(backend)
+    end
+    nothing
+    # end
+end
+
+   # Unpack terms that make up the model (not sources)
     # nTerms = model.parameters[3]
     # nTerms = TN
 
@@ -37,40 +77,40 @@ function _apply_boundary_conditions!(
 
     # quote
     # Extract number of terms
-    nTerms = length(model.terms)
 
-    # Define variables for function
-    precon = eqn.preconditioner
-    mesh = model.terms[1].phi.mesh
+@kernel function apply_boundary_conditions_kernel!(
+    model::Model{TN,SN,T,S}, BC, terms, 
+    faces, cells, start_ID, boundary_cellsID, rowval, colptr, nzval, b, ione
+    ) where {TN,SN,T,S}
 
-    # Deconstructors to get lower-level variables for function
-    # (; A, b) = eqn.equation
-    A_array = _A(eqn)
-    b_array = _b(eqn)
-    (; boundaries, faces, cells, boundary_cellsID) = mesh
+    i = @index(Global)
+    # (; IDs_range) = boundaries[BC.ID]
+    j = i + start_ID - 1
+    # i = BC.ID
+    # (; IDs_range) = boundaries[i]
+    
+    faceID = j
+    cellID = boundary_cellsID[j]
+    face = faces[faceID]
+    cell = cells[cellID] 
+    apply!(model, BC, terms, rowval, colptr, nzval, b, cellID, cell, face, faceID, ione)
+end
 
-    # Deconstruct sparse array dependent on sparse arrays type
-    rowval_array = _rowval(A_array)
-    colptr_array = _colptr(A_array)
-    nzval_array = _nzval(A_array)
+@generated function apply!(
+    model::Model{TN,SN,T,S}, BC, terms, 
+    rowval, colptr, nzval, b, cellID, cell, face, fID, ione) where {TN,SN,T,S}
 
-    # Get types and create integer(one)
-    backend = _get_backend(mesh)
-    integer = _get_int(mesh)
-    ione = one(integer)
-
-    # Execute function to apply boundary conditions for all terms and boundaries
-    for bci in 1:length(BCs)
-        for t in 1:nTerms
-            Execute_apply_boundary_condition_kernel!(BCs[bci], model.terms[t], 
-                                                    backend, boundaries, faces, cells,
-                                                    boundary_cellsID, ione, rowval_array,
-                                                    colptr_array, nzval_array, b_array)
-            KernelAbstractions.synchronize(backend)
+    # Definition of main assignment loop (one per patch)
+    func_calls = Expr[]
+    for t ∈ 1:TN 
+        call = quote
+            (BC)(terms[$t], rowval, colptr, nzval, b, cellID, cell, face, fID, ione)
         end
+        push!(func_calls, call)
     end
-    nothing
-    # end
+    quote
+        $(func_calls...)
+    end
 end
 
 @generated function boundary_indices(mesh::M, BCs::B) where {M<:Mesh2,B}

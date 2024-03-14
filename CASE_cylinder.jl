30358b68fdd2c0abf588852a29c5effd1424448d
Humberto <h.medina@aerofluids.org>
2024-03-14 02:36:49 +0000

    use standard relaxation

--- CASE_cylinder.jl
+++ CASE_cylinder.jl
@@ -4,16 +4,15 @@ using Krylov
 using CUDA
 using KernelAbstractions
 
-# quad, backwardFacingStep_2mm, backwardFacingStep_10mm, trig40
-mesh_file = "unv_sample_meshes/cylinder_d10mm_2mm.unv"
 # mesh_file = "unv_sample_meshes/cylinder_d10mm_5mm.unv"
-# mesh_file = "unv_sample_meshes/cylinder_d10mm_10-7.5-2mm.unv"
+# mesh_file = "unv_sample_meshes/cylinder_d10mm_2mm.unv"
+mesh_file = "unv_sample_meshes/cylinder_d10mm_10-7.5-2mm.unv"
 mesh = build_mesh(mesh_file, scale=0.001)
 mesh = update_mesh_format(mesh)
 
 # Inlet conditions
 
-velocity = [0.50, 0.0, 0.0]
+velocity = [0.5, 0.0, 0.0]
 noSlip = [0.0, 0.0, 0.0]
 nu = 1e-3
 Re = (0.2*velocity[1])/nu
@@ -42,16 +41,16 @@ solvers = (
         solver      = BicgstabSolver, # BicgstabSolver, GmresSolver
         preconditioner = Jacobi(),
         convergence = 1e-7,
-        relax       = 0.6,
-        rtol = 1e-4
+        relax       = 0.7,
+        rtol = 1e-3
     ),
     p = set_solver(
         model.p;
         solver      = CgSolver, #CgLanczosSolver, #CgSolver, # BicgstabSolver, GmresSolver
         preconditioner = Jacobi(),
         convergence = 1e-7,
-        relax       = 0.4,
-        rtol = 1e-4
+        relax       = 0.3,
+        rtol = 1e-3
     )
 );
 

32d1588eed0e208a7d78921632b23808a85aad5c
OscarOsborn <oosbornwork@gmail.com>
2024-04-06 13:56:00 -0700

    test_environment for faces

---
+++ sandbox_face_calculations.jl
@@ -0,0 +1,24 @@
+using FVM_1D
+using StaticArrays
+using Statistics
+using LinearAlgebra
+#include("src/VTK_3D/VTU.jl")
+
+
+unv_mesh="src/UNV_3D/TET_PRISM.unv"
+unv_mesh="src/UNV_3D/Quad_cell_new_boundaries.unv"
+unv_mesh="src/UNV_3D/5_cell_new_boundaries.unv"
+
+# @time mesh = build_mesh3D(unv_mesh)
+# mesh.faces
+# mesh.cells
+# mesh.boundaries
+
+points, edges, efaces, volumes, boundaryElements = load_3D(unv_mesh,scale=1, integer=Int64, float=Float64)
+
+points
+edges
+efaces
+volumes
+boundaryElements
+

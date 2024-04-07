7ba495c60fa19d9a364a5df88b3860679ab55437
OscarOsborn <oosbornwork@gmail.com>
2024-04-07 14:55:03 -0700

    testing functions with hexa to check

--- sandbox_hexa.jl
+++ sandbox_hexa.jl
@@ -6,7 +6,8 @@ using LinearAlgebra
 
 
 unv_mesh="src/UNV_3D/Quad_cell_new_boundaries.unv"
-unv_mesh="src/UNV_3D/5_cell_new_boundaries.unv"
+
+unv_mesh="src/UNV_3D/HEXA_HM.unv"
 
 
 points, edges, efaces, volumes, boundaryElements = load_3D(unv_mesh,scale=1, integer=Int64, float=Float64)
@@ -17,39 +18,56 @@ efaces
 volumes
 boundaryElements
 
-@time mesh = build_mesh3D(unv_mesh)
-
 cell_nodes, cell_nodes_range = FVM_1D.UNV_3D.generate_cell_nodes(volumes) # Should be Hybrid compatible, tested for hexa. Using push instead of allocating vector.
-node_cells, node_cells_range = FVM_1D.UNV_3D.generate_node_cells(points, volumes)
-nodes = FVM_1D.UNV_3D.build_nodes(points, node_cells_range)
-boundaries = FVM_1D.UNV_3D.build_boundaries(boundaryElements)
+node_cells, node_cells_range = FVM_1D.UNV_3D.generate_node_cells(points, volumes)  # Should be Hybrid compatible, tested for hexa.
+nodes = FVM_1D.UNV_3D.build_nodes(points, node_cells_range) # Hyrbid compatible, works for Tet and Hexa
+boundaries = FVM_1D.UNV_3D.build_boundaries(boundaryElements) # Hybrid compatible
 
-nbfaces = sum(length.(getproperty.(boundaries, :IDs_range)))
+nbfaces = sum(length.(getproperty.(boundaries, :IDs_range))) # total boundary faces
 
-bface_nodes, bface_nodes_range, bface_owners_cells, boundary_cellsID = FVM_1D.UNV_3D.generate_boundary_faces(boundaryElements, efaces, nbfaces, node_cells, node_cells_range, volumes)
+bface_nodes, bface_nodes_range, bface_owners_cells, boundary_cellsID = 
+begin
+    FVM_1D.UNV_3D.generate_boundary_faces(boundaryElements, efaces, nbfaces, node_cells, node_cells_range, volumes) # Hybrid compatible, tested with hexa
+end
 
-iface_nodes, iface_nodes_range, iface_owners_cells = FVM_1D.UNV_3D.generate_internal_faces(volumes, nbfaces, nodes, node_cells)
+iface_nodes, iface_nodes_range, iface_owners_cells = 
+begin 
+    FVM_1D.UNV_3D.generate_internal_faces(volumes, nbfaces, nodes, node_cells) # Hybrid compatible, tested with hexa.
+end
 
+# NOTE: A function will be needed here to reorder the nodes IDs of "faces" to be geometrically sound! (not needed for tet cells though)
+bface_nodes,iface_nodes=FVM_1D.UNV_3D.order_face_nodes(bface_nodes_range,iface_nodes_range,bface_nodes,iface_nodes,nodes)
+#2 methods, using old as new function produced negative volumes?
 
-get_data(array, range, index) = @view array[range[index]]
-get_data(array, range) =  array[range] #@view array[range] # 
-nodeIDs = get_data
-faceIDs = get_data
-cellIDs = get_data
-
+# Shift range of nodes_range for internal faces (since it will be appended)
 iface_nodes_range .= [
-iface_nodes_range[i] .+ length(bface_nodes) for i ∈ eachindex(iface_nodes_range)
-]
+    iface_nodes_range[i] .+ length(bface_nodes) for i ∈ eachindex(iface_nodes_range)
+    ]
 
 # Concatenate boundary and internal faces
 face_nodes = vcat(bface_nodes, iface_nodes)
 face_nodes_range = vcat(bface_nodes_range, iface_nodes_range)
 face_owner_cells = vcat(bface_owners_cells, iface_owners_cells)
 
+# Sort out cell to face connectivity
+cell_faces, cell_nsign, cell_faces_range, cell_neighbours = begin
+    FVM_1D.UNV_3D.generate_cell_face_connectivity(volumes, nbfaces, face_owner_cells) # Hybrid compatible. Hexa and tet tested.
+end
 
-cell_faces, cell_nsign, cell_faces_range, cell_neighbours=FVM_1D.UNV_3D.generate_cell_face_connectivity(volumes, nbfaces, face_owner_cells)
+# Build mesh (without calculation of geometry/properties)
+cells = FVM_1D.UNV_3D.build_cells(cell_nodes_range, cell_faces_range) # Hybrid compatible. Hexa tested.
+faces = FVM_1D.UNV_3D.build_faces(face_nodes_range, face_owner_cells) # Hybrid compatible. Hexa tested.
 
-cells = FVM_1D.UNV_3D.build_cells(cell_nodes_range, cell_faces_range)
+mesh = Mesh3(
+    cells, cell_nodes, cell_faces, cell_neighbours, cell_nsign, 
+    faces, face_nodes, boundaries, 
+    nodes, node_cells,
+    SVector{3, Float64}(0.0, 0.0, 0.0), UnitRange{Int64}(0, 0), boundary_cellsID
+) # Hexa tested.
 
-faces = FVM_1D.UNV_3D.build_faces(face_nodes_range, face_owner_cells)
+# Update mesh to include all geometry calculations required
+FVM_1D.UNV_3D.calculate_centres!(mesh) # Uses centroid instead of geometric. Will need changing, should work fine for regular cells and faces
+FVM_1D.UNV_3D.calculate_face_properties!(mesh) # Touched up face properties, double check values.
+FVM_1D.UNV_3D.calculate_area_and_volume!(mesh)
+mesh.cells
 

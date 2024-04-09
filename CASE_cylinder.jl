b3f7b16bc63c951a29f7e7f45b8ee6bf4579e70f
TomMazin <egytm1@nottingham.ac.uk>
2024-04-09 18:01:56 +0300

    Simulation Case Updates

--- CASE_cylinder.jl
+++ CASE_cylinder.jl
@@ -1,4 +1,4 @@
-using Plots
+# using Plots
 using FVM_1D
 using Krylov
 using CUDA
@@ -6,8 +6,8 @@ using CUDA
 
 # quad, backwardFacingStep_2mm, backwardFacingStep_10mm, trig40
 mesh_file = "unv_sample_meshes/cylinder_d10mm_5mm.unv"
-mesh_file = "unv_sample_meshes/cylinder_d10mm_2mm.unv"
-mesh_file = "unv_sample_meshes/cylinder_d10mm_10-7.5-2mm.unv"
+# mesh_file = "unv_sample_meshes/cylinder_d10mm_2mm.unv"
+# mesh_file = "unv_sample_meshes/cylinder_d10mm_10-7.5-2mm.unv"
 mesh = build_mesh(mesh_file, scale=0.001)
 # mesh = update_mesh_format(mesh, integer=Int32, float=Float32)
 mesh = update_mesh_format(mesh)
@@ -69,8 +69,8 @@ GC.gc()
 initialise!(model.U, velocity)
 initialise!(model.p, 0.0)
 
-backend = CUDABackend() # 357 s
-# backend = CPU()
+# backend = CUDABackend()
+backend = CPU()
 
 Rx, Ry, Rp, model1 = simple!(model, config, backend); #, pref=0.0)
 

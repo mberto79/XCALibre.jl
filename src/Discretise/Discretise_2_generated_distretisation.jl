7f6d68e055886ada37d67717162dd427233b2251
Humberto <h.medina@aerofluids.org>
2024-04-10 18:21:29 +0100

    local memory works only if using static kernel launches with preallocated workgroup sizes

--- src/Discretise/Discretise_2_generated_distretisation.jl
+++ src/Discretise/Discretise_2_generated_distretisation.jl
@@ -35,7 +35,7 @@ function discretise!(eqn, prev, runtime, nfaces, nbfaces)
     # nfaces = length(mesh.faces)
     internalfaces = nfaces - nbfaces
 
-    kernel! = _discretise_face!(backend)
+    kernel! = _discretise_face!(backend, 256, internalfaces)
     kernel!(model, model.terms, model.sources, mesh, nzval_array, rowval_array, colptr_array, b_array, prev, runtime, fzero, ione; ndrange = internalfaces)
     KernelAbstractions.synchronize(backend)
 
@@ -44,12 +44,38 @@ function discretise!(eqn, prev, runtime, nfaces, nbfaces)
     KernelAbstractions.synchronize(backend)
 end
 
+function spindex(rowval, colptr, i, j)
+    # view_range = @view rowval[colptr[j]:colptr[j+1]]
+    # findnext(isequal(i), view_range, i) + colptr[j]
+
+    start_ind = colptr[j]
+    end_ind = colptr[j+1]
+
+    ind = zero(typeof(start_ind))
+    for nzi in start_ind:end_ind
+        if rowval[nzi] == i
+            ind = nzi
+            break
+        end
+    end
+    return ind
+end
+
 @kernel function _discretise_face!(
     model::Model{TN,SN,T,S}, terms, sources, mesh, nzval_array, @Const(rowval_array), @Const(colptr_array), b_array, prev, @Const(runtime), fzero, ione) where {TN,SN,T,S}
-    i = @index(Global)
+    ic = @index(Global,Linear)
+    li = @index(Local,Linear)
+
+
+    N = @groupsize()[1]
+    test = @localmem Int32 N
+
+
     @uniform nbfaces = mesh.boundaries[end].IDs_range[end]
-    fID = i + nbfaces
+    fID = ic + nbfaces
+    # backend = _get_backend(mesh)
     @uniform (; faces, cells, cell_faces, cell_neighbours, cell_nsign) = mesh
+  
     
     face = faces[fID]
     owners = face.ownerCells
@@ -58,14 +84,22 @@ end
     cell1 = cells[cID1]
     cell2 = cells[cID2]
 
-    cIndex1 = nzval_index(colptr_array, rowval_array, cID1, cID1, ione)
-    cIndex2 = nzval_index(colptr_array, rowval_array, cID2, cID2, ione)
+    # cIndex1 = nzval_index(colptr_array, rowval_array, cID1, cID1, ione)
+    # cIndex2 = nzval_index(colptr_array, rowval_array, cID2, cID2, ione)
 
-    nIndex1 = nzval_index(colptr_array, rowval_array, cID2, cID1, ione)
-    nIndex2 = nzval_index(colptr_array, rowval_array, cID1, cID2, ione)
+    # nIndex1 = nzval_index(colptr_array, rowval_array, cID2, cID1, ione)
+    # nIndex2 = nzval_index(colptr_array, rowval_array, cID1, cID2, ione)
 
-    _scheme!(model, terms, nzval_array, cell1, face,  cell2, ione, cIndex1, nIndex1, fID, prev, runtime)
-    _scheme!(model, terms, nzval_array, cell2, face,  cell1, -ione, cIndex2, nIndex2, fID, prev, runtime)
+    cIndex1 = spindex(rowval_array, colptr_array, cID1, cID1)
+    cIndex2 = spindex(rowval_array, colptr_array,cID2, cID2)
+
+    nIndex1 = spindex(rowval_array, colptr_array, cID1, cID2)
+    nIndex2 = spindex(rowval_array, colptr_array, cID2, cID1)
+
+    # _scheme!(model, terms, nzval_array, cell1, face,  cell2, ione, cIndex1, nIndex1, fID, prev, runtime)
+    # _scheme!(model, terms, nzval_array, cell2, face,  cell1, -ione, cIndex2, nIndex2, fID, prev, runtime)
+
+    _scheme!(model, terms, nzval_array, face, cell1,  cell2, ione, cIndex1, nIndex1, cIndex2, nIndex2, fID, prev, runtime)
 
     # @inbounds begin
         # (; faces_range, volume) = cell
@@ -97,7 +131,8 @@ end
     cell = cells[i]
     (; volume) = cell
 
-    cIndex = nzval_index(colptr_array, rowval_array, i, i, ione)
+    # cIndex = nzval_index(colptr_array, rowval_array, i, i, ione)
+    cIndex = spindex(rowval_array, colptr_array, i, i)
 
     _scheme_source!(model, terms, b_array, nzval_array, cell, i, cIndex, prev, runtime)
     _sources!(model, sources, b_array, volume, i)
@@ -134,14 +169,15 @@ end
     # end
 end
 
-@generated function _scheme!(model::Model{TN,SN,T,S}, terms, nzval_array, cell, face,  cellN, ns, cIndex, nIndex, fID, prev, runtime) where {TN,SN,T,S}
+@generated function _scheme!(model::Model{TN,SN,T,S}, terms, nzval_array, face, cell1,   cell2, ns, cIndex1, nIndex1, cIndex2, nIndex2, fID, prev, runtime) where {TN,SN,T,S}
     
     # # Implementation 2
     out = Expr(:block)
     for t in 1:TN
         function_call_scheme = quote
             # scheme!(model.terms[$t], nzval_array, cell, face,  cellN, ns, cIndex, nIndex, fID, prev, runtime)
-            scheme!(terms[$t], nzval_array, cell, face,  cellN, ns, cIndex, nIndex, fID, prev, runtime)
+            scheme!(terms[$t], nzval_array, cell1, face,  cell2, ns, cIndex1, nIndex1, fID, prev, runtime)
+            scheme!(terms[$t], nzval_array, cell2, face,  cell1, -ns, cIndex2, nIndex2, fID, prev, runtime)
         end
         push!(out.args, function_call_scheme)
     end

60f029df0ccf51ab8cb752d5babfde28e04f0d2c
Humberto <h.medina@aerofluids.org>
2024-04-12 18:03:09 +0100

    play with boundary conditions - this needs more work

--- src/Discretise/Discretise_3_boundary_conditions.jl
+++ src/Discretise/Discretise_3_boundary_conditions.jl
@@ -34,15 +34,15 @@ end
     # A[cellID,cellID] += fzero
     # b[cellID] += fzero
 
-    values = phi.values
-    J = term.flux[fID]
-    (; area, delta) = face 
-    flux = J*area/delta
-    ap = term.sign[1]*(-flux)
-    # A[cellID,cellID] += ap
-    nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
-    Atomix.@atomic nzval[nIndex] += ap
-    Atomix.@atomic b[cellID] += ap*values[cellID]
+    # values = phi.values
+    # J = term.flux[fID]
+    # (; area, delta) = face 
+    # flux = J*area/delta
+    # ap = term.sign[1]*(-flux)
+    # # A[cellID,cellID] += ap
+    # nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
+    # Atomix.@atomic nzval[nIndex] += ap
+    # Atomix.@atomic b[cellID] += ap*values[cellID]
     nothing
 end
 

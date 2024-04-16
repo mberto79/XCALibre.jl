d229ca55982a64f75854493d8c75bd27519db4a7
Humberto <h.medina@aerofluids.org>
2024-04-16 14:17:13 +0100

    test BCs

--- src/Discretise/Discretise_3_boundary_conditions.jl
+++ src/Discretise/Discretise_3_boundary_conditions.jl
@@ -14,11 +14,20 @@ end
     term::Operator{F,P,I,Laplacian{Linear}}, 
     rowval, colptr, nzval, b, cellID, cell, face, fID, ione
     ) where {F,P,I} = begin
+    # Previous
+    # J = term.flux[fID]
+    # (; area, delta) = face 
+    # flux = J*area/delta
+    # ap = term.sign[1]*(-flux)
+    # nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
+    # Atomix.@atomic nzval[nIndex] += ap
+    # Atomix.@atomic b[cellID] += ap*bc.value
+
+    # test
     J = term.flux[fID]
     (; area, delta) = face 
-    flux = J*area/delta
+    flux = J*area/(2*delta)
     ap = term.sign[1]*(-flux)
-    # A[cellID,cellID] += ap
     nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
     Atomix.@atomic nzval[nIndex] += ap
     Atomix.@atomic b[cellID] += ap*bc.value
@@ -34,15 +43,25 @@ end
     # A[cellID,cellID] += fzero
     # b[cellID] += fzero
 
-    values = phi.values
-    J = term.flux[fID]
-    (; area, delta) = face 
-    flux = J*area/delta
-    ap = term.sign[1]*(-flux)
-    # A[cellID,cellID] += ap
-    nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
-    Atomix.@atomic nzval[nIndex] += ap
-    Atomix.@atomic b[cellID] += ap*values[cellID]
+    # previous (semi-implicit)
+    # values = phi.values
+    # J = term.flux[fID]
+    # (; area, delta) = face 
+    # flux = J*area/delta
+    # ap = term.sign[1]*(-flux)
+    # nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
+    # Atomix.@atomic nzval[nIndex] += ap
+    # Atomix.@atomic b[cellID] += ap*values[cellID]
+
+    # Test
+    # values = phi.values
+    # J = term.flux[fID]
+    # (; area, delta) = face 
+    # flux = J*area/delta
+    # ap = term.sign[1]*(-flux)
+    # nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
+    # Atomix.@atomic nzval[nIndex] += ap
+    # Atomix.@atomic b[cellID] += ap*values[cellID]
     nothing
 end
 
@@ -125,11 +144,18 @@ end
     # ap = term.sign[1]*(term.flux[fID])
     # A[cellID,cellID] += ap
     # b[cellID] -= ap*phi[cellID]
+
+    # previous
+    # ap = term.sign[1]*(term.flux[fID])
+    # nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
+    # Atomix.@atomic nzval[nIndex] += max(ap, 0.0)
+    # Atomix.@atomic b[cellID] += max(-ap*phi[cellID], 0.0)
+
+    # test
     ap = term.sign[1]*(term.flux[fID])
-    # A[cellID,cellID] += max(ap, 0.0)
-    nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
-    Atomix.@atomic nzval[nIndex] += max(ap, 0.0)
-    Atomix.@atomic b[cellID] += max(-ap*phi[cellID], 0.0)
+    cIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
+    Atomix.@atomic nzval[cIndex] += max(ap, 0.0)
+    # Atomix.@atomic b[cellID] += max(-ap*phi[cellID], 0.0)
     nothing
 end
 
@@ -185,253 +211,3 @@ end
     nothing
 end
 
-
-## GPU CODE
-
-export Execute_apply_boundary_condition_kernel!
-
-# TRANSIENT TERM 
-function Execute_apply_boundary_condition_kernel!(
-    bc::AbstractBoundary, term::Operator{F,P,I,Time{T}}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I,T}
-    nothing
-end
-
-# LAPLACIAN TERM (NON-UNIFORM)
-function Execute_apply_boundary_condition_kernel!(
-    bc::Dirichlet, term::Operator{F,P,I,Laplacian{Linear}}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    
-    kernel! = Dirichlet_laplacian_linear!(backend)
-    kernel!(term, bc, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b, ndrange = 1)
-    KernelAbstractions.synchronize(backend)
-end
-
-@kernel function Dirichlet_laplacian_linear!(term, BC, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b)
-    i = @index(Global)
-    i = BC.ID
-    
-    @inbounds begin
-        (; IDs_range) = boundaries[i]
-        for j ∈ IDs_range
-            faceID = j
-            cellID = boundary_cellsID[j]
-            face = faces[faceID]
-            cell = cells[cellID] 
-            # (BCs[$bci])(model.terms[$t], A, b, cellID, cell, face, faceID)
-            J = term.flux[faceID]
-            (; area, delta) = face 
-            flux = J*area/delta
-            ap = term.sign[1]*(-flux)
-            
-            # start = colptr[cellID]
-            # offset = 0
-            # for j in start:length(rowval)
-            #     offset += 1
-            #     if rowval[j] == cellID
-            #         break
-            #     end
-            # end
-            # nIndex = start + offset - ione
-            nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
-
-            nzval[nIndex] += ap
-            b[cellID] += ap*BC.value
-        end
-    end
-end
-
-function Execute_apply_boundary_condition_kernel!(
-    bc::Neumann, term::Operator{F,P,I,Laplacian{Linear}}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    
-    kernel! = Neumann_laplacian_linear!(backend)
-    kernel!(term, bc, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b, ndrange = 1)
-    KernelAbstractions.synchronize(backend)
-end
-
-@kernel function Neumann_laplacian_linear!(term, BC, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b)
-    i = @index(Global)
-    i = BC.ID
-    
-    @inbounds begin
-        (; IDs_range) = boundaries[i]
-        for j ∈ IDs_range
-            faceID = j
-            cellID = boundary_cellsID[j]
-            face = faces[faceID]
-            cell = cells[cellID] 
-            # (BCs[$bci])(model.terms[$t], A, b, cellID, cell, face, faceID)
-            # phi = term.phi
-        end
-    end
-end
-
-# DIVERGENCE TERM (NON-UNIFORM)
-function Execute_apply_boundary_condition_kernel!(
-    bc::Dirichlet, term::Operator{F,P,I,Divergence{Linear}}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    
-    kernel! = Dirichlet_divergence_linear!(backend)
-    kernel!(term, bc, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b, ndrange = 1)
-    KernelAbstractions.synchronize(backend)
-end
-
-@kernel function Dirichlet_divergence_linear!(term, BC, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b)
-    i = @index(Global)
-    i = BC.ID
-    
-    @inbounds begin
-        (; IDs_range) = boundaries[i]
-        for j ∈ IDs_range
-            faceID = j
-            cellID = boundary_cellsID[j]
-            face = faces[faceID]
-            cell = cells[cellID] 
-            # (BCs[$bci])(model.terms[$t], A, b, cellID, cell, face, faceID)
-            b[cellID] += term.sign[1]*(-term.flux[faceID]*BC.value)
-        end
-    end
-end
-
-function Execute_apply_boundary_condition_kernel!(
-    bc::Neumann, term::Operator{F,P,I,Divergence{Linear}}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    
-    kernel! = Neumann_divergence_linear!(backend)
-    kernel!(term, bc, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b, ndrange = 1)
-    KernelAbstractions.synchronize(backend)
-end
-
-@kernel function Neumann_divergence_linear!(term, BC, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b)
-    i = @index(Global)
-    i = BC.ID
-
-    @inbounds begin
-        (; IDs_range) = boundaries[i]
-        for j ∈ IDs_range
-            faceID = j
-            cellID = boundary_cellsID[j]
-            face = faces[faceID]
-            cell = cells[cellID] 
-            # (BCs[$bci])(model.terms[$t], A, b, cellID, cell, face, faceID)
-            ap = term.sign[1]*(term.flux[faceID])
-
-            # start = colptr[cellID]
-            # offset = 0
-            # for j in start:length(rowval)
-            #     offset += 1
-            #     if rowval[j] == cellID
-            #         break
-            #     end
-            # end
-            # nIndex = start + offset - ione
-            nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
-
-            nzval[nIndex] += ap
-        end
-    end
-end
-
-function Execute_apply_boundary_condition_kernel!(
-    bc::Dirichlet, term::Operator{F,P,I,Divergence{Upwind}}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    
-    kernel! = Dirichlet_divergence_upwind!(backend)
-    kernel!(term, bc, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b, ndrange = 1)
-    KernelAbstractions.synchronize(backend)
-end
-
-@kernel function Dirichlet_divergence_upwind!(term, BC, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b)
-    i = @index(Global)
-    i = BC.ID
-    
-    @inbounds begin
-        (; IDs_range) = boundaries[i]
-        for j ∈ IDs_range
-            faceID = j
-            cellID = boundary_cellsID[j]
-            face = faces[faceID]
-            cell = cells[cellID]
-            # (BCs[$bci])(model.terms[$t], A, b, cellID, cell, face, faceID)
-            # A[cellID,cellID] += 0.0 
-            # b[cellID] += term.sign[1]*(-term.flux[fID]*bc.value)
-
-            ap = term.sign[1]*(term.flux[faceID])
-            # A[cellID,cellID] += max(ap, 0.0)
-            b[cellID] -= ap*BC.value
-
-            # ap = term.sign[1]*(term.flux[fID])
-            # b[cellID] += A[cellID,cellID]*bc.value
-            # A[cellID,cellID] += A[cellID,cellID]
-        end
-    end
-end
-
-function Execute_apply_boundary_condition_kernel!(
-    bc::Neumann, term::Operator{F,P,I,Divergence{Upwind}}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    kernel! = Neumann_divergence_upwind!(backend)
-    kernel!(term, bc, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b, ndrange = 1)
-    KernelAbstractions.synchronize(backend)
-end
-
-@kernel function Neumann_divergence_upwind!(term, BC, ione, boundaries, faces, cells, boundary_cellsID, rowval, colptr, nzval, b)
-    i = @index(Global)
-    i = BC.ID
-    
-    @inbounds begin
-    (; IDs_range) = boundaries[i]
-        for j ∈ IDs_range
-            faceID = j
-            cellID = boundary_cellsID[j]
-            face = faces[faceID]
-            cell = cells[cellID]
-
-            # phi = term.phi 
-            # ap = term.sign[1]*(term.flux[fID])
-            # A[cellID,cellID] += ap
-            # ap = term.sign[1]*(term.flux[fID])
-            # A[cellID,cellID] += ap
-            # b[cellID] -= ap*phi[cellID]
-            ap = term.sign[1]*(term.flux[faceID])
-
-            # start = colptr[cellID]
-            # offset = 0
-            # for j in start:length(rowval)
-            #     offset += 1
-            #     if rowval[j] == cellID
-            #         break
-            #     end
-            # end
-            # nIndex = start + offset - ione
-            nIndex = nzval_index(colptr, rowval, cellID, cellID, ione)
-
-            nzval[nIndex] += ap
-            # b[cellID] -= max(ap*phi[cellID], 0.0)
-        end
-    end
-end
-
-# IMPLICIT SOURCE
-
-function Execute_apply_boundary_condition_kernel!(
-    bc::Dirichlet, term::Operator{F,P,I,Si}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    nothing
-end
-
-function Execute_apply_boundary_condition_kernel!(
-    bc::Neumann, term::Operator{F,P,I,Si}, 
-    backend, boundaries, faces, cells,
-    boundary_cellsID, ione, rowval, colptr, nzval, b) where {F,P,I}
-    nothing
-end

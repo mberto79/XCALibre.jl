b770b12f2fc0b41ba0881b95f7b87551fd2f31a0
Humberto <h.medina@aerofluids.org>
2024-05-13 00:20:11 +0100

    add face constructor for building UNV3 meshes

--- src/Mesh/Mesh_0_types.jl
+++ src/Mesh/Mesh_0_types.jl
@@ -5,23 +5,32 @@ export AbstractMesh
 
 abstract type AbstractMesh end
 
-# MESH CONSTITUENTS
+# struct UnitVectors{F<:AbstractFloat}
+#     i::SVector{3, F}
+#     j::SVector{3, F}
+#     k::SVector{3, F}
+# end
+# Adapt.@adapt_structure UnitVectors
 
-# Node structure
+# UnitVectors(T) = UnitVectors{T}(
+#         SVector{3,T}(1.0,0.0,0.0), 
+#         SVector{3,T}(0.0,1.0,0.0), 
+#         SVector{3,T}(0.0,0.0,1.0)
+#         )
+
 struct Node{SV3<:SVector{3,<:AbstractFloat}, UR<:UnitRange{<:Integer}}
     coords::SV3
     cells_range::UR # to access neighbour cells (can be dummy entry for now)
 end
 Adapt.@adapt_structure Node
 
-# Boundary structure
 struct Boundary{S<:Symbol, UR<:UnitRange{<:Integer}}
     name::S
     IDs_range::UR
+    # cellsID::VI
 end
 Adapt.@adapt_structure Boundary
 
-# Cell structure
 struct Cell{F<:AbstractFloat, SV3<:SVector{3,F},UR<:UnitRange{<:Integer}}
     centre::SV3
     volume::F
@@ -38,7 +47,6 @@ Cell(TI::T, TF::T) where T<:DataType = begin
         UnitRange{TI}(0,0)
         )
 end
-
 # 2D and 3D Face types
 
 struct Face2D{
@@ -77,7 +85,18 @@ struct Face3D{
 end
 Adapt.@adapt_structure Face3D
 
-# MESH TYPES
+Face3D(TI::T, TF::T) where T<:DataType = begin
+    Face3D(
+        UnitRange{TI}(0,0),
+        SVector{2,TI}(0,0),
+        SVector{3,TF}(0.0,0.0,0.0),
+        SVector{3,TF}(0.0,0.0,0.0),
+        SVector{3,TF}(0.0,0.0,0.0),
+        zero(TF),
+        zero(TF),
+        zero(TF)
+    )
+end
 
 # 2D and 3D Mesh types
 
@@ -91,12 +110,35 @@ struct Mesh2{VC, VI, VF<:AbstractArray{<:Face2D}, VB, VN, SV3, UR} <: AbstractMe
     face_nodes::VI
     boundaries::VB
     nodes::VN
-    node_cells::VI
+    node_cells::VI # can be empty for now
     get_float::SV3
     get_int::UR
     boundary_cellsID::VI
 end
 Adapt.@adapt_structure Mesh2
+# function Adapt.adapt_structure(to, itp::Mesh2)
+#     cells = Adapt.adapt_structure(to, itp.cells)
+#     cell_nodes = Adapt.adapt_structure(to, itp.cell_nodes)
+#     cell_faces = Adapt.adapt_structure(to, itp.cell_faces)
+#     cell_neighbours = Adapt.adapt_structure(to, itp.cell_neighbours)
+#     cell_nsign = Adapt.adapt_structure(to, itp.cell_nsign)
+#     # boundaries = Adapt.adapt_structure(to, itp.boundaries)
+#     faces = Adapt.adapt_structure(to, itp.faces)
+#     face_nodes = Adapt.adapt_structure(to, itp.face_nodes)
+
+#     boundaries_arr = Boundary[]
+#     for i in eachindex(itp.boundaries)
+#         boundary = Adapt.adapt_structure(to, itp.boundaries[i])
+#         push!(boundaries_arr, boundary)
+#     end
+
+#     nodes = Adapt.adapt_structure(to, itp.nodes)
+#     node_cells = Adapt.adapt_structure(to, itp.node_cells)
+#     get_float = Adapt.adapt_structure(to, itp.get_float)
+#     get_int = Adapt.adapt_structure(to, itp.get_int)
+
+#     Mesh2(cells, cell_nodes, cell_faces, cell_neighbours, cell_nsign, faces, face_nodes, boundaries_arr, nodes, node_cells, get_float, get_int)
+# end
 
 struct Mesh3{VC, VI, VF<:AbstractArray{<:Face3D}, VB, VN, SV3, UR} <: AbstractMesh
     cells::VC
@@ -108,7 +150,7 @@ struct Mesh3{VC, VI, VF<:AbstractArray{<:Face3D}, VB, VN, SV3, UR} <: AbstractMe
     face_nodes::VI
     boundaries::VB
     nodes::VN
-    node_cells::VI
+    node_cells::VI # can be empty for now
     get_float::SV3
     get_int::UR
     boundary_cellsID::VI

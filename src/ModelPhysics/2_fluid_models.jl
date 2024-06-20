0e827a85fa3627454eb2223ae5d9d805b68b3e56
Humberto <h.medina@aerofluids.org>
2024-06-20 16:35:37 +0100

    type structure definition for compressible fluids

--- src/ModelPhysics/2_fluid_models.jl
+++ src/ModelPhysics/2_fluid_models.jl
@@ -1,10 +1,31 @@
-export Incompressible
+export Incompressible, WeaklyCompressible, Compressible
 export _nu
+export _R
 
-@kwdef struct Incompressible{T}
+abstract type AbstractFluid end
+abstract type AbstractIncompressible <: AbstractFluid end
+abstract type AbstractCompressible <: AbstractFluid end
+
+@kwdef struct Incompressible{T} <: AbstractIncompressible
     nu::T
 end
 Adapt.@adapt_structure Incompressible
 
-_nu(fluid::Incompressible) = fluid.nu
+_nu(fluid::AbstractIncompressible) = fluid.nu
 # rho(fluid::Incompressible) = fluid.nu
+
+@kwdef struct WeaklyCompressible{T} <: AbstractCompressible
+    mu::T
+    cp::T
+    gamma::T
+end
+Adapt.@adapt_structure WeaklyCompressible
+
+@kwdef struct Compressible{T} <: AbstractCompressible
+    mu::T
+    cp::T
+    gamma::T
+end
+Adapt.@adapt_structure Compressible
+
+_R(fluid::AbstractCompressible) = fluid.cp*(1 - (1/fluid.gamma))

33e2a92b51fee580061c49e9d6e6ca79a050d5b1
Humberto Medina <h.medina@aerofluids.org>
2024-06-24 23:38:34 +0100

    cell-face connectivity implemented

--- prototyping.jl
+++ prototyping.jl
@@ -6,21 +6,6 @@ mesh_file = "unv_sample_meshes/OF_cavity_hex/constant/polyMesh"
 
 foamdata = read_foamMesh(mesh_file, Int64, Float64)
 
-@time fowners = connect_mesh(foamdata, Int64, Float64)
+@time out = connect_mesh(foamdata, Int64, Float64)
 
-fowners[2]
-
-a = findall(==(1), fowners[1])
-b = findall(==(1), fowners[2])
-
-a = Vector{Int}[]
-
-push!(a, [1,2,3])
-a[1][3] = 5
-a
-
-for i ∈ eachindex(a)
-    println(i)
-end
-
-fowners[2]
+sum(length.(out.out4))
--- src/FoamMesh/FoamMesh_1_connectivity.jl
+++ src/FoamMesh/FoamMesh_1_connectivity.jl
@@ -5,39 +5,67 @@ function connect_mesh(foamdata, TI, TF)
 
     # OpenFOAM provides face information: sort out face connectivity first
     
-    cell_nfaces = connect_cell_faces(foamdata, TI, TF)
+    cell_faces, cell_faces_range, cell_neighbours, cell_nsign  = connect_cell_faces(foamdata, TI, TF)
 
     (
-        out1 = cell_nfaces,
+        out1 = cell_faces,
+        out2 = cell_faces_range,
+        out3 = cell_neighbours,
+        out4 = cell_nsign,
     )
 end
 
 function connect_cell_faces(foamdata, TI, TF)
     (;n_cells, n_ifaces, n_bfaces, faces) = foamdata 
-    cell_nfaces = zeros(TI, n_cells)
 
-    # find number of internal faces per cell
-    for fi ∈ 1:n_ifaces
-        face = faces[fi]
-        ownerID = face.owner
-        neighbourID = face.neighbour
-        cell_nfaces[ownerID] += one(TI)
-        cell_nfaces[neighbourID] += one(TI)
-    end
+    cell_facesIDs = Vector{TI}[TI[] for _ ∈ 1:n_cells]
+    cell_normalSign = Vector{TI}[TI[] for _ ∈ 1:n_cells]
+    cell_neighbourCells = Vector{TI}[TI[] for _ ∈ 1:n_cells]
 
-    cell_faces = Vector{TI}[TI[] for _ ∈ 1:n_cells]
-
+    # collect internal faces IDs for all cells
     for fi ∈ 1:n_ifaces 
         face = faces[fi]
         ownerID = face.owner
         neighbourID = face.neighbour
-        fID = fi + n_bfaces # fID is shifted to later move boundary faces to the start
-        push!(cell_faces[ownerID], fID)
-        push!(cell_faces[neighbourID], fID)
+        fID = fi + n_bfaces # fID is shifted to accommodate boundary faces at the start
+        push!(cell_facesIDs[ownerID], fID)
+        push!(cell_facesIDs[neighbourID], fID)
+
+        push!(cell_normalSign[ownerID], one(TI))
+        push!(cell_normalSign[neighbourID], -one(TI))
+
+        push!(cell_neighbourCells[ownerID], neighbourID)
+        push!(cell_neighbourCells[neighbourID], ownerID)
     end
 
-    
+    n_faceIDs = sum(length.(cell_facesIDs))
+    cell_faces = zeros(TI, n_faceIDs)
+    cell_nsign = zeros(TI, n_faceIDs)
+    cell_neighbours = zeros(TI, n_faceIDs)
+    cell_faces_range = UnitRange{TI}[UnitRange{TI}(0,0) for _ ∈ 1:n_cells]
 
-    return cell_faces
+    facei = 0 # face counter (not ID)
+    # for (cID, facesIDs) ∈ enumerate(cell_facesIDs)
+    for cID ∈ eachindex(cell_facesIDs)
+        facesIDs = cell_facesIDs[cID]
+        neighbourCells = cell_neighbourCells[cID]
+        normalSign = cell_normalSign[cID]
+
+        startIdx = TI(facei)
+        for i ∈ eachindex(facesIDs, neighbourCells, normalSign)
+            facei += 1
+            cell_faces[facei] = facesIDs[i]
+            cell_neighbours[facei] = neighbourCells[i]
+            cell_nsign[facei] = normalSign[i]
+        end
+        endIdx = TI(facei)
+        cell_faces_range[cID] = UnitRange{TI}(startIdx + one(TI), endIdx)
+    end
+
+    return cell_faces, cell_faces_range, cell_neighbours, cell_nsign
+end
+
+function connect_cell_nodes()
+    nothing
 end
 

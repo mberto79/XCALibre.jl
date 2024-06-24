5f93404a7a5c91d93a8c204906c42a7d8b4e0c70
Humberto Medina <h.medina@aerofluids.org>
2024-06-25 00:24:56 +0100

    cell node connectivity implemented

--- src/FoamMesh/FoamMesh_1_connectivity.jl
+++ src/FoamMesh/FoamMesh_1_connectivity.jl
@@ -7,11 +7,15 @@ function connect_mesh(foamdata, TI, TF)
     
     cell_faces, cell_faces_range, cell_neighbours, cell_nsign  = connect_cell_faces(foamdata, TI, TF)
 
+    cell_nodes, cell_nodes_range = connect_cell_nodes(foamdata, TI, TF)
+
     (
         out1 = cell_faces,
         out2 = cell_faces_range,
         out3 = cell_neighbours,
         out4 = cell_nsign,
+        out5 = cell_nodes,
+        out6 = cell_nodes_range,
     )
 end
 
@@ -22,7 +26,7 @@ function connect_cell_faces(foamdata, TI, TF)
     cell_normalSign = Vector{TI}[TI[] for _ ∈ 1:n_cells]
     cell_neighbourCells = Vector{TI}[TI[] for _ ∈ 1:n_cells]
 
-    # collect internal faces IDs for all cells
+    # collect internal faces IDs, neighbours and normal signs for all cells
     for fi ∈ 1:n_ifaces 
         face = faces[fi]
         ownerID = face.owner
@@ -31,7 +35,7 @@ function connect_cell_faces(foamdata, TI, TF)
         push!(cell_facesIDs[ownerID], fID)
         push!(cell_facesIDs[neighbourID], fID)
 
-        push!(cell_normalSign[ownerID], one(TI))
+        push!(cell_normalSign[ownerID], one(TI)) # positive by definition
         push!(cell_normalSign[neighbourID], -one(TI))
 
         push!(cell_neighbourCells[ownerID], neighbourID)
@@ -44,8 +48,8 @@ function connect_cell_faces(foamdata, TI, TF)
     cell_neighbours = zeros(TI, n_faceIDs)
     cell_faces_range = UnitRange{TI}[UnitRange{TI}(0,0) for _ ∈ 1:n_cells]
 
+    # write to single arrays and define equivalent access ranges as UnitRange
     facei = 0 # face counter (not ID)
-    # for (cID, facesIDs) ∈ enumerate(cell_facesIDs)
     for cID ∈ eachindex(cell_facesIDs)
         facesIDs = cell_facesIDs[cID]
         neighbourCells = cell_neighbourCells[cID]
@@ -65,7 +69,37 @@ function connect_cell_faces(foamdata, TI, TF)
     return cell_faces, cell_faces_range, cell_neighbours, cell_nsign
 end
 
-function connect_cell_nodes()
-    nothing
+function connect_cell_nodes(foamdata, TI, TF)
+    (;n_cells, n_ifaces, n_bfaces, faces) = foamdata 
+
+    cell_nodesIDs = Vector{TI}[TI[] for _ ∈ 1:n_cells]
+
+    # collect nodes IDs for internal faces for all cells
+    for face ∈ faces
+        ownerID = face.owner
+        neighbourID = face.neighbour
+        # Use fi to index here since using face data load from foamMesh
+        push!(cell_nodesIDs[ownerID], face.nodesID...)
+        push!(cell_nodesIDs[neighbourID], face.nodesID...)
+    end
+    cell_nodesIDs = intersect.(cell_nodesIDs)
+
+    # define cell nodesIDs access ranges 
+    n_nodeIDs = sum(length.(cell_nodesIDs))
+    cell_nodes_range = UnitRange{TI}[UnitRange{TI}(0,0) for _ ∈ 1:n_nodeIDs]
+
+    startIndex = 1
+    endIndex = 0
+    for (cID, nodesIDs) ∈ enumerate(cell_nodesIDs)
+        n_nodeIDs = length(nodesIDs)
+        endIndex = startIndex + n_nodeIDs - one(TI)
+        cell_nodes_range[cID] = UnitRange{TI}(startIndex, endIndex)
+        startIndex += n_nodeIDs #- one(TI)
+    end
+
+    # flatten cell_nodesIDs to cell_nodes
+    cell_nodes = reduce(vcat, cell_nodesIDs)
+
+    return cell_nodes, cell_nodes_range
 end
 

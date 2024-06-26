40fa1f4137be7888103a3442df7c7f97f780e921
Humberto <h.medina@aerofluids.org>
2024-06-26 02:51:06 +0100

    generalise VTK writer logic

--- src/VTK/VTK_writer_3D.jl
+++ src/VTK/VTK_writer_3D.jl
@@ -122,23 +122,33 @@ function write_vtk(name, mesh::Mesh3, args...)
         write(io,"     </DataArray>\n")
         write(io,"     <DataArray type=\"$(I64)\" Name=\"$(face_offsets)\" format=\"$(format)\">\n")
 
-        for i=1:length(cells_cpu)
-            #Tet
-            if length(cells_cpu[i].nodes_range)==4 #No. of Nodes in a Tet Cell
-                x=x+17 
-                write(io,"     $(x)\n")
+        for fIDs ∈ all_cell_faces
+            totalNodes = 0
+            for fID ∈ fIDs
+                node_count = length(faces_cpu[fID].nodes_range)
+                totalNodes += node_count
             end
-            #Hexa
-            if length(cells_cpu[i].nodes_range)==8 #No. of Nodes in a Hexa Cell
-                x=x+31
-                write(io,"     $(x)\n")
-            end
-            #PRISM
-            if length(cells_cpu[i].nodes_range)==6 # No. of Nodes in a Prism cell
-                x=x+24
-                write(io,"     $(x)\n")
-            end
+            x += 1 + length(fIDs) + totalNodes
+            write(io,"     $(x)\n")
         end
+
+        # for i=1:length(cells_cpu)
+        #     #Tet
+        #     if length(cells_cpu[i].nodes_range)==4 #No. of Nodes in a Tet Cell
+        #         x=x+17 
+        #         write(io,"     $(x)\n")
+        #     end
+        #     #Hexa
+        #     if length(cells_cpu[i].nodes_range)==8 #No. of Nodes in a Hexa Cell
+        #         x=x+31
+        #         write(io,"     $(x)\n")
+        #     end
+        #     #PRISM
+        #     if length(cells_cpu[i].nodes_range)==6 # No. of Nodes in a Prism cell
+        #         x=x+24
+        #         write(io,"     $(x)\n")
+        #     end
+        # end
 
         write(io,"     </DataArray>\n")
         write(io,"     <DataArray type=\"$(I64)\" Name=\"$(types)\" format=\"$(format)\">\n")

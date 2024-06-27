e24f38911acef1cad1c21e2f64fa84a8510a1b4f
Humberto <h.medina@aerofluids.org>
2024-06-27 18:49:23 +0100

    3D BFS case updated to include scaling of FOAM mesh

--- CASE_3D_BFS.jl
+++ CASE_3D_BFS.jl
@@ -9,7 +9,7 @@ mesh_file = "unv_sample_meshes/bfs_unv_tet_10mm.unv"
 @time mesh = build_mesh3D(mesh_file, scale=0.001)
 
 mesh_file = "unv_sample_meshes/bfs_OF_tet_meshes/5mm/polyMesh/"
-@time mesh = FOAM3D_mesh(mesh_file, integer_type=Int64, float_type=Float64)
+@time mesh = FOAM3D_mesh(mesh_file, scale=0.001, integer_type=Int64, float_type=Float64)
 
 mesh_gpu = adapt(CUDABackend(), mesh)
 

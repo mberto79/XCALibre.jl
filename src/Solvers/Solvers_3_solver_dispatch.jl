54fffbde81bf052af662455666071d356195f670
Humberto <h.medina@aerofluids.org>
2024-07-05 21:48:14 +0100

    pass pref to solvers - incompressible cases with no pressure given

--- src/Solvers/Solvers_3_solver_dispatch.jl
+++ src/Solvers/Solvers_3_solver_dispatch.jl
@@ -2,37 +2,37 @@ export run!
 
 # Incompressible solver (steady)
 run!(
-    model::Physics{T,F,M,Tu,E,D,BI}, config
+    model::Physics{T,F,M,Tu,E,D,BI}, config; pref=nothing
     ) where{T<:Steady,F<:Incompressible,M,Tu,E,D,BI} = 
 begin
-    Rx, Ry, Rz, Rp, model = simple!(model, config); #, pref=0.0)
+    Rx, Ry, Rz, Rp, model = simple!(model, config, pref=pref)
     return Rx, Ry, Rz, Rp, model
 end
 
 # Incompressible solver (transient)
 run!(
-    model::Physics{T,F,M,Tu,E,D,BI}, config
+    model::Physics{T,F,M,Tu,E,D,BI}, config; pref=nothing
     ) where{T<:Transient,F<:Incompressible,M,Tu,E,D,BI} = 
 begin
-    Rx, Ry, Rz, Rp, model = piso!(model, config); #, pref=0.0)
+    Rx, Ry, Rz, Rp, model = piso!(model, config, pref=pref); #, pref=0.0)
     return Rx, Ry, Rz, Rp, model
 end
 
 # Weakly Compressible solver (steady)
 run!(
-    model::Physics{T,F,M,Tu,E,D,BI}, config
+    model::Physics{T,F,M,Tu,E,D,BI}, config; pref=nothing
     ) where{T<:Steady,F<:WeaklyCompressible,M,Tu,E,D,BI} = 
 begin
-    Rx, Ry, Rz, Rp, Re, model = simple_comp!(model, config); #, pref=0.0)
+    Rx, Ry, Rz, Rp, Re, model = simple_comp!(model, config, pref=pref); #, pref=0.0)
     return Rx, Ry, Rz, Rp, Re, model
 end
 
 # Compressible solver (steady)
 run!(
-    model::Physics{T,F,M,Tu,E,D,BI}, config
+    model::Physics{T,F,M,Tu,E,D,BI}, config; pref=nothing
     ) where{T<:Steady,F<:Compressible,M,Tu,E,D,BI} = 
 begin
-    Rx, Ry, Rz, Rp, Re, model = simple_comp!(model, config); #, pref=0.0)
+    Rx, Ry, Rz, Rp, Re, model = simple_comp!(model, config, pref=pref); #, pref=0.0)
     return Rx, Ry, Rz, Rp, Re, model
 end
 

9943ac80da8d07be86145c1b28dcd8edd811e5ec
Christopher D. Ellis <25395795+tallchellis@users.noreply.github.com>
2024-08-08 21:26:15 +0100

    Density added to model and kw model converted to density - Need to update compressible solvers and energy to make better use of rho implementation in fluid

--- src/ModelPhysics/2_fluid_models.jl
+++ src/ModelPhysics/2_fluid_models.jl
@@ -1,33 +1,36 @@
 export AbstractFluid, AbstractIncompressible, AbstractCompressible
 export Incompressible, WeaklyCompressible, Compressible
-export _nu
+export _nu, _rho
 export _R, _Cp, _mu, _Pr
 
 abstract type AbstractFluid end
 abstract type AbstractIncompressible <: AbstractFluid end
 abstract type AbstractCompressible <: AbstractFluid end
 
-@kwdef struct Incompressible{T} <: AbstractIncompressible
+@kwdef struct Incompressible{T, F} <: AbstractIncompressible
     nu::T
+    rho::F
 end
 Adapt.@adapt_structure Incompressible
 
 _nu(fluid::AbstractIncompressible) = fluid.nu
-# rho(fluid::Incompressible) = fluid.nu
+_rho(fluid::AbstractIncompressible) = fluid.rho
 
-@kwdef struct WeaklyCompressible{T} <: AbstractCompressible
+@kwdef struct WeaklyCompressible{T, F} <: AbstractCompressible
     mu::T
     cp::T
     gamma::T
     Pr::T
+    rho::F
 end
 Adapt.@adapt_structure WeaklyCompressible
 
-@kwdef struct Compressible{T} <: AbstractCompressible
+@kwdef struct Compressible{T, F} <: AbstractCompressible
     mu::T
     cp::T
     gamma::T
     Pr::T
+    rho::F
 end
 Adapt.@adapt_structure Compressible
 
@@ -35,3 +38,4 @@ _R(fluid::AbstractCompressible) = ConstantScalar(fluid.cp.values*(1.0 - (1.0/flu
 _Cp(fluid::AbstractCompressible) = fluid.cp
 _mu(fluid::AbstractCompressible) = fluid.mu
 _Pr(fluid::AbstractCompressible) = fluid.Pr
+_rho(fluis::AbstractCompressible) = fluid.rho

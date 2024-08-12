b4c4e637521c0778872d14ca264919c63e3317c3
Humberto <h.medina@aerofluids.org>
2024-08-12 04:09:10 +0100

    add unsteady case (BFS Periodic)

---
+++ CASE_3D_BFS_periodic_U.jl
@@ -0,0 +1,109 @@
+using FVM_1D
+
+mesh_file = "unv_sample_meshes/BFS_UNV_3D_hex_5mm.unv"
+mesh = UNV3D_mesh(mesh_file, scale=0.001)
+
+mesh_dev = mesh
+
+velocity = [0.8, 0.0, 0.8]
+nu = 1e-4
+Re = velocity[1]*0.1/nu
+
+model = Physics(
+    time = Transient(),
+    fluid = Incompressible(nu = ConstantScalar(nu)),
+    turbulence = RANS{Laminar}(),
+    energy = ENERGY{Isothermal}(),
+    domain = mesh_dev
+    )
+
+periodic1, periodic2 = construct_periodic(model, :side1, :side2)
+    
+@assign! model momentum U (
+    Dirichlet(:inlet, velocity),
+    Neumann(:outlet, 0.0),
+    # Dirichlet(:wall, [0.0, 0.0, 0.0]),
+    Wall(:wall, [0.0, 0.0, 0.0]),
+    Dirichlet(:top, [0.0, 0.0, 0.0]),
+    # Neumann(:top, 0.0),
+    periodic1, periodic2
+)
+
+@assign! model momentum p (
+    Neumann(:inlet, 0.0),
+    Dirichlet(:outlet, 0.0),
+    Neumann(:wall, 0.0),
+    Neumann(:top, 0.0),
+    periodic1, periodic2
+)
+
+schemes = (
+    U = set_schemes(time=Euler, divergence=Upwind, gradient=Midpoint),
+    p = set_schemes(gradient=Midpoint)
+    # p = set_schemes()
+)
+
+
+solvers = (
+    U = set_solver(
+        model.momentum.U;
+        solver      = BicgstabSolver, #CgSolver, # BicgstabSolver, GmresSolver, #CgSolver
+        preconditioner = Jacobi(),
+        convergence = 1e-7,
+        # relax       = 0.8,
+        relax       = 1.0,
+        rtol = 1e-3,
+        atol = 1e-10
+    ),
+    p = set_solver(
+        model.momentum.p;
+        solver      = CgSolver, #GmresSolver, #CgSolver, # BicgstabSolver, GmresSolver
+        preconditioner = Jacobi(),
+        convergence = 1e-7,
+        # relax       = 0.2,
+        relax       = 1.0,
+        rtol = 1e-3,
+        atol = 1e-10
+    )
+)
+
+runtime = set_runtime(
+    iterations=2000, time_step=1e-3, write_interval=50)
+
+# hardware = set_hardware(backend=CUDABackend(), workgroup=32)
+hardware = set_hardware(backend=CPU(), workgroup=4)
+
+config = Configuration(
+    solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
+
+GC.gc(true)
+
+initialise!(model.momentum.U, [0.0,0.0,0.0])
+initialise!(model.momentum.p, 0.0)
+
+Rx, Ry, Rz, Rp, model_out = run!(model, config)
+
+plot(; xlims=(0,1000))
+plot!(1:length(Rx), Rx, yscale=:log10, label="Ux")
+plot!(1:length(Ry), Ry, yscale=:log10, label="Uy")
+plot!(1:length(Rp), Rp, yscale=:log10, label="p")
+
+# # PROFILING CODE
+
+using Profile, PProf
+
+GC.gc()
+initialise!(model.momentum.U, velocity)
+initialise!(model.momentum.p, 0.0)
+
+Profile.Allocs.clear()
+Profile.Allocs.@profile sample_rate=1 begin 
+    Rx, Ry, Rz, Rp, model = run!(model, config)
+end
+
+PProf.Allocs.pprof()
+
+test(::Nothing, a) = print("nothing")
+test(b, a) = print(a*a)
+
+test(nothing, 1)

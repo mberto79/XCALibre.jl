21c8a8c72ae026cc926cd6b5cb7de104e7503a2d
Humberto <h.medina@aerofluids.org>
2024-08-16 18:34:22 +0100

    working symmetry BC

--- src/Discretise/boundary_conditions/symmetry.jl
+++ src/Discretise/boundary_conditions/symmetry.jl
@@ -3,16 +3,23 @@
 
 @define_boundary Symmetry Laplacian{Linear} begin
     phi = term.phi 
-    velocity_cell = phi[cellID]
+    # velocity_cell = phi[cellID]
+    vc = phi[cellID]
     J = term.flux[fID]
     (; area, delta, normal) = face 
 
-    norm_vel= (velocity_cell⋅normal)
-    norm_vel = norm_vel - velocity_cell[component.value]*normal[component.value]
-    flux = J*area/delta
-    ap = term.sign[1]*(-flux)
+    # norm_vel= (velocity_cell⋅normal)
+    # norm_vel = norm_vel - velocity_cell[component.value]*normal[component.value]
+    # flux = J*area/delta
+    # ap = term.sign[1]*(-flux)
 
-    (2.0)*ap*normal[component.value]*normal[component.value], (2.0)*ap*(norm_vel*normal[component.value])
+    # (2.0)*ap*normal[component.value]*normal[component.value]^2, (2.0)*ap*(norm_vel*normal[component.value])
+
+    flux = 2*J*area/delta
+    ap = term.sign[1]*(-flux)
+    vn = (vc⋅normal)*normal
+    vp = vc - vn
+    ap, ap*vp[component.value]
 end
 
 @define_boundary Symmetry Divergence{Linear} begin

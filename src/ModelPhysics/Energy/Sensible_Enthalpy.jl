e6aec2676d28c6b41050e89759091b8e9b9ace5c
Christopher D. Ellis <25395795+tallchellis@users.noreply.github.com>
2024-08-30 21:45:15 +0100

    Check doc string updates references documents.

--- src/ModelPhysics/Energy/Sensible_Enthalpy.jl
+++ src/ModelPhysics/Energy/Sensible_Enthalpy.jl
@@ -47,6 +47,25 @@ return_thingy(::Type{SensibleEnthalpy}, fluid, Tref) = begin
     end
 end
 
+"""
+    initialise(energy::SensibleEnthalpy, model::Physics{T1,F,M,Tu,E,D,BI}, mdotf, rho, peqn, config
+    ) where {T1,F,M,Tu,E,D,BI})
+
+Initialisation of energy transport equations.
+
+### Input
+- `energy` -- Energy model.
+- `model`  -- Physics model defined by user.
+- `mdtof`  -- Face mass flow.
+- `rho`    -- Density ScalarField.
+- `peqn`   -- Pressure equation.
+- `config` -- Configuration structure defined by user with solvers, schemes, runtime and 
+              hardware structures set.
+
+### Output
+- `Sensible_Enthalpy_Model'  -- Energy model struct containing energy equation.
+
+"""
 function initialise(
     energy::SensibleEnthalpy, model::Physics{T1,F,M,Tu,E,D,BI}, mdotf, rho, peqn, config
     ) where {T1,F,M,Tu,E,D,BI}

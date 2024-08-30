687b6c5b1dd9343d00023dee98bacd394f9aed33
Humberto <h.medina@aerofluids.org>
2024-08-30 17:14:07 +0100

    extend test skeleton

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,48 @@
 using XCALibre
 using Test
 
-@testset "XCALibre.jl" begin
+@testset "Mesh conversion" begin
     # Write your own tests here.
-    @testset "Dummy tests" begin
-        @test 2*2 == 4
-    end
+    @test 2 + 2 == 4
+    @test 2 + 2 == 4
+    @test 2 + 2 == 4
+end
+
+@testset "Gradient schemes" begin
+    # Write your own tests here.
+    @test 2 + 2 == 4
+    @test 2 + 2 == 4
+    @test 2 + 2 == 4
+end
+
+@testset "Upwind schemes" begin
+    # Write your own tests here.
+    @test 2 + 2 == 4
+    @test 2 + 2 == 4
+    @test 2 + 2 == 4
+end
+
+@testset "Incompressible (steady)" begin
+    # Write your own tests here.
+    @test 2 + 2 == 4
+end
+
+@testset "Incompressible (transient)" begin
+    # Write your own tests here.
+    @test 2 + 2 == 4
+end
+
+@testset "Compressible (steady)" begin
+    # Write your own tests here.
+    @test 2 + 2 == 4
+end
+
+@testset "Compressible (transient)" begin
+    # Write your own tests here.
+    @test 2 + 2 == 4
+end
+
+@testset "Boundary conditions" begin
+    # Write your own tests here.
+    @test 2 + 2 == 4
 end

c96f4b9880a9ed0f6f0a41c8128e5d97e5353e63
Christopher D. Ellis <25395795+tallchellis@users.noreply.github.com>
2024-09-06 11:40:41 +0100

    RANS model docstrings done!

--- src/ModelPhysics/Turbulence/turbulence_types.jl
+++ src/ModelPhysics/Turbulence/turbulence_types.jl
@@ -8,11 +8,26 @@ abstract type AbstractRANSModel <: AbstractTurbulenceModel end
 abstract type AbstractLESModel <: AbstractTurbulenceModel end
 
 # Models 
+"""
+    RANS <: AbstractRANSModel
 
+Abstract RANS model type for consturcting RANS models.
+
+### Fields
+- 'args' -- Model arguments.
+"""
 struct RANS{T,ARG} <:AbstractRANSModel 
     args::ARG
 end
 
+"""
+    LES <: AbstractLESModel
+
+Abstract LES model type for consturcting LES models.
+
+### Fields
+- 'args' -- Model arguments.
+"""
 struct LES{T,ARG} <:AbstractLESModel 
     args::ARG
 end

6a4a03d8b138ae5daa860b240b8f05607f5de1bc
Humberto Medina <h.medina@aerofluids.org>
2024-09-13 20:15:19 +0100

    start adding some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,28 @@ using XCALibre
 using Test
 
 @testset "Mesh conversion" begin
-    # Write your own tests here.
-    @test 2 + 2 == 4
-    @test 2 + 2 == 4
-    @test 2 + 2 == 4
+    examples_dir = pkgdir(XCALibre, "examples")
+    unv2_file = joinpath(
+        examples_dir, 
+        "2d_incompressible_laminar_backwards_step/backward_facing_step_10mm.unv"
+        )
+    mesh = UNV2D_mesh(unv2_file, scale=0.001)
+    msg = IOBuffer(); println(msg, mesh)
+    outputTest = String(take!(msg))
+    @testset "UNV" begin
+        @test outputTest == """
+2D Mesh
+-> 1800 cells
+-> 3720 faces
+-> 1921 nodes
+
+Boundaries
+-> inlet (faces: 1:10)
+-> outlet (faces: 11:30)
+-> wall (faces: 31:140)
+-> top (faces: 141:240)
+"""
+    end
 end
 
 @testset "Gradient schemes" begin
@@ -46,3 +64,10 @@ end
     # Write your own tests here.
     @test 2 + 2 == 4
 end
+
+
+a = """2D Mesh\n-> 1800 cells\n-> 3720 faces\n-> 1921 nodes\n\nBoundaries \n-> inlet (faces: 1:10)\n-> outlet (faces: 11:30)\n-> wall (faces: 31:140)\n-> top (faces: 141:240)\n\n\n"""
+
+b = """2D Mesh\n-> 1800 cells\n-> 3720 faces\n-> 1921 nodes\n\nBoundaries\n-> inlet (faces: 1:10)\n-> outlet (faces: 11:30)\n-> wall (faces: 31:140)\n-> top (faces: 141:240)\n\n\n"""
+
+a == b

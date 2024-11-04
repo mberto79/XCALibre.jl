2df1d5eec5864cef119a827a8eaebcb842359ed4
Humberto <h.medina@aerofluids.org>
2024-11-04 18:02:20 +0000

    temp changes to example 3D_BFS

--- examples/3D_BFS.jl
+++ examples/3D_BFS.jl
@@ -1,4 +1,3 @@
-using Plots
 using XCALibre
 using CUDA
 
@@ -8,19 +7,23 @@ grid = "bfs_unv_tet_5mm.unv"
 grid = "bfs_unv_tet_10mm.unv"
 
 mesh_file = "/home/humberto/foamCases/jCFD_benchmarks/3D_BFS/bfs_unv_tet_5mm.unv"
-mesh_file = "/home/humberto/foamCases/jCFD_benchmarks/3D_BFS/bfs_unv_tet_4mm.unv"
+# mesh_file = "/home/humberto/foamCases/jCFD_benchmarks/3D_BFS/bfs_unv_tet_4mm.unv"
 
 # mesh_file = joinpath(grids_dir, grid)
 
-@time mesh = UNV3D_mesh(mesh_file, scale=0.001)
+# mesh_file = "bfs_unv_tet_5mm.unv"
+mesh = UNV3D_mesh(mesh_file, scale=0.001)
 
-# @time mesh = FOAM3D_mesh(mesh_file, scale=0.001, integer_type=Int64, float_type=Float64)
+workgroup = 32
+backend = CUDABackend()
 
-mesh_dev = adapt(CUDABackend(), mesh)
+mesh_dev = adapt(backend, mesh)
 
+# Inlet conditions
 velocity = [0.5, 0.0, 0.0]
+noSlip = [0.0, 0.0, 0.0]
 nu = 1e-3
-Re = velocity[1]*0.1/nu
+Re = (0.2*velocity[1])/nu
 
 model = Physics(
     time = Steady(),
@@ -29,74 +32,70 @@ model = Physics(
     energy = Energy{Isothermal}(),
     domain = mesh_dev
     )
-    
 
-@assign! model momentum U (
-    # Dirichlet(:inlet, velocity),
-    # Neumann(:outlet, 0.0),0.0]),
-    # Dirichlet(:sides, [0.0, 0.0, 0.0])
+@assign! model momentum U ( 
     Dirichlet(:inlet, velocity),
-    Wall(:wall, [0.0, 0.0, 0.0]),
-    # Dirichlet(:wall, [0.0, 0.0, 0.0]),
     Neumann(:outlet, 0.0),
-    Neumann(:top, 0.0),
-    Neumann(:sides, 0.0)
+    Wall(:wall, noSlip),
+    Neumann(:sides, 0.0),
+    Neumann(:top, 0.0)
 )
 
 @assign! model momentum p (
     Neumann(:inlet, 0.0),
     Dirichlet(:outlet, 0.0),
     Neumann(:wall, 0.0),
-    Neumann(:top, 0.0),
-    Neumann(:sides, 0.0)
+    Neumann(:sides, 0.0),
+    Neumann(:top, 0.0)
 )
-
-schemes = (
-    U = set_schemes(divergence=Upwind, gradient=Orthogonal),
-    p = set_schemes(gradient=Orthogonal)
-    # p = set_schemes()
-)
-
 
 solvers = (
     U = set_solver(
         model.momentum.U;
-        solver      = BicgstabSolver, #CgSolver, # BicgstabSolver, GmresSolver, #CgSolver
-        preconditioner = Jacobi(), # Jacobi ILU0GPU
+        solver      = BicgstabSolver, # BicgstabSolver, GmresSolver
+        preconditioner = Jacobi(),
         convergence = 1e-7,
         relax       = 0.8,
-        rtol = 0.1,
+        rtol = 0.1
     ),
     p = set_solver(
         model.momentum.p;
-        solver      = CgSolver, #GmresSolver, #CgSolver, # BicgstabSolver, GmresSolver
-        preconditioner = Jacobi(), # Jacobi IC0GPU
+        solver      = CgSolver, # BicgstabSolver, GmresSolver
+        preconditioner = Jacobi(), #NormDiagonal(),
         convergence = 1e-7,
         relax       = 0.2,
         rtol = 0.1,
+        itmax = 1000
     )
 )
 
-runtime = set_runtime(
-    iterations=1500, time_step=1, write_interval=500)
+schemes = (
+    U = set_schemes(time=SteadyState, divergence=Upwind, gradient=Midpoint),
+    p = set_schemes(time=SteadyState, gradient=Midpoint)
+)
 
-hardware = set_hardware(backend=CUDABackend(), workgroup=32)
-# hardware = set_hardware(backend=CPU(), workgroup=4)
+hardware = set_hardware(backend=backend, workgroup=workgroup)
 
-config = Configuration(
-    solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
+# Run first to pre-compile
+
+runtime = set_runtime(iterations=1, write_interval=1, time_step=1)
+config = Configuration(solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
 
 GC.gc(true)
 
 initialise!(model.momentum.U, velocity)
 initialise!(model.momentum.p, 0.0)
 
-residuals = run!(model, config, limit_gradient=true)
-# residuals = run!(model, config, limit_gradient=false)
+residuals = run!(model, config)
 
-xrange = 1:runtime.iterations
-plot(; xlims=(0,runtime.iterations), ylims=(1e-7,0.2))
-plot!(xrange, residuals.Ux, yscale=:log10, label="Ux")
-plot!(xrange, residuals.Uy, yscale=:log10, label="Uy")
-plot!(xrange, residuals.Uz, yscale=:log10, label="Uz")
-plot!(xrange, residuals.p, yscale=:log10, label="p")
+# Now get timing information
+
+runtime = set_runtime(iterations=500, write_interval=500, time_step=1)
+config = Configuration(solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
+
+GC.gc(true)
+
+initialise!(model.momentum.U, velocity)
+initialise!(model.momentum.p, 0.0)
+
+residuals = run!(model, config)

ecd6e0c7d4a5ca276c9309788e6eef13c379f4b6
Humberto Medina <h.medina@aerofluids.org>
2025-03-28 01:23:43 +0000

    add error message if `Mesh3` is not used with `OpenFOAM` output format

--- src/IOFormats/OpenFOAM/OpenFOAM_writer.jl
+++ src/IOFormats/OpenFOAM/OpenFOAM_writer.jl
@@ -1,4 +1,7 @@
 initialise_writer(format::OpenFOAM, mesh::Mesh3) = FOAMWriter(nothing, nothing)
+initialise_writer(format::OpenFOAM, mesh) = error("
+The OpenFOAM format can only be used for 3D simulations. Use `output=VTK()` instead.
+")
 
 function write_results(iteration, mesh, meshData::FOAMWriter, args...)
     name = @sprintf "time_%.6d" iteration

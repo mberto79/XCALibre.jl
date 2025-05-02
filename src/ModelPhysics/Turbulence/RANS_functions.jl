92d8797ace43ba8f7cdd1aee7c9f3153b992d447
Joshua Leyland <egyjl6@nottingham.ac.uk>
2025-05-02 09:56:43 +0100

    Removed if statements and changed RANS_functions.jl generated function calls to use multiple dispatch

--- src/ModelPhysics/Turbulence/RANS_functions.jl
+++ src/ModelPhysics/Turbulence/RANS_functions.jl
@@ -236,13 +236,10 @@ end
     BCs = fieldBCs.parameters
     func_calls = Expr[]
     for i ∈ eachindex(BCs)
-        BC = BCs[i]
-        if BC <: KWallFunction
-            call = quote
-                set_production!(P, fieldBCs[$i], model, gradU, config)
-            end
-            push!(func_calls, call)
+        call = quote
+            set_production!(P, fieldBCs[$i], model, gradU, config)
         end
+        push!(func_calls, call)
     end
     quote
     $(func_calls...)
@@ -250,7 +247,9 @@ end
     end 
 end
 
-function set_production!(P, BC, model, gradU, config)
+set_production!(P, BC, model, gradU, config) = nothing
+
+function set_production!(P, BC::KWallFunction, model, gradU, config)
     # backend = _get_backend(mesh)
     (; hardware) = config
     (; backend, workgroup) = hardware
@@ -297,10 +296,61 @@ end
     mag_grad_U = mag(sngrad(U[cID], Uw, delta, normal))
     # mag_grad_U = mag(gradU[cID]*normal)
     if yplus > yPlusLam
-        values[cID] = (nu[cID] + nutw)*mag_grad_U*dUdy
+        values[cID] = (nu[cID] + nutw)*mag_grad_U*dUdy 
     else
         values[cID] = 0.0
     end
+end
+
+function set_production!(P, BC::NeumannFunction, model, gradU, config)
+    # backend = _get_backend(mesh)
+    (; hardware) = config
+    (; backend, workgroup) = hardware
+    
+    # Deconstruct mesh to required fields
+    mesh = model.domain
+    (; faces, boundary_cellsID, boundaries) = mesh
+
+    # Extract physics models
+    (; fluid, momentum, turbulence) = model
+
+    # facesID_range = get_boundaries(BC, boundaries)
+    boundaries_cpu = get_boundaries(boundaries)
+    facesID_range = boundaries_cpu[BC.ID].IDs_range
+    start_ID = facesID_range[1]
+
+    (; output, input, network, gradient) = BC.value
+    
+    # Execute apply boundary conditions kernel
+    kernel! = _set_production_NN!(backend, workgroup)
+    kernel!(
+        P.values, BC, fluid, momentum, turbulence, faces, boundary_cellsID, start_ID, gradU, ndrange=length(facesID_range)
+    )
+end
+
+@kernel function _set_production_NN!(
+    values, BC, fluid, momentum, turbulence, faces, boundary_cellsID, start_ID, gradU)
+    i = @index(Global)
+    fID = i + start_ID - 1 # Redefine thread index to become face ID
+
+    (; input, output, gradient, data_mean, data_std) = BC.value 
+    (; nu) = fluid
+    (; U) = momentum
+    (; k) = turbulence
+
+    Uw = SVector{3}(0.0,0.0,0.0)
+    cID = boundary_cellsID[fID]
+    face = faces[fID]
+    nuc = nu[cID]
+    (; delta, normal)= face
+    cmu = 0.09
+    yplus = y_plus(k[cID], nuc, delta, cmu)
+    input = (yplus .- data_mean) ./ data_std
+        
+    dUdy = ((cmu^0.25*sqrt(k[cID]))^2/nuc)*gradient
+    nutw = nuc.*(input./output)
+    mag_grad_U = mag(sngrad(U[cID], Uw, delta, normal))
+    values[cID] = nutw.*mag_grad_U.*dUdy
 end
 
 @generated function correct_eddy_viscosity!(νtf, nutBCs, model, config)
@@ -364,4 +414,51 @@ end
     else
         values[fID] = 0.0
     end
+end
+
+function correct_nut_wall!(νtf, BC::NeumannFunction, model, config)
+    # backend = _get_backend(mesh)
+    (; hardware) = config
+    (; backend, workgroup) = hardware
+    
+    # Deconstruct mesh to required fields
+    mesh = model.domain
+    (; faces, boundary_cellsID, boundaries) = mesh
+
+    # Extract physics models
+    (; fluid, turbulence) = model
+
+    # facesID_range = get_boundaries(BC, boundaries)
+    boundaries_cpu = get_boundaries(boundaries)
+    facesID_range = boundaries_cpu[BC.ID].IDs_range
+    start_ID = facesID_range[1]
+
+    (; output, input, network) = BC.value
+
+    # Execute apply boundary conditions kernel
+    kernel! = _correct_nut_wall_NN!(backend, workgroup)
+    kernel!(
+        νtf.values, fluid, turbulence, BC, faces, boundary_cellsID, start_ID, ndrange=length(facesID_range)
+    )
+end
+
+@kernel function _correct_nut_wall_NN!(
+    values, fluid, turbulence, BC, faces, boundary_cellsID, start_ID)
+i = @index(Global)
+    fID = i + start_ID - 1 # Redefine thread index to become face ID
+    
+    (; input, output, data_mean, data_std) = BC.value  
+    (; nu) = fluid
+    (; k, nutf) = turbulence
+    
+    cID = boundary_cellsID[fID]
+    face = faces[fID]
+    nuc = nu[cID]
+    (; delta, normal)= face
+    cmu = 0.09
+    yplus = y_plus(k[cID], nuc, delta, cmu)
+    input = (yplus .- data_mean) ./ data_std
+        
+    nutw = nuc.*(input./output)
+    values[fID] = nutw
 end

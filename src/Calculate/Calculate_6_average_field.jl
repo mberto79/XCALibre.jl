03666bd3d1bf448e0f985ed4dc97f8ccb89d79fd
James <j4m3sknight@gmail.com>
2025-05-21 09:50:04 +0100

    removed second moment calculation

--- src/Calculate/Calculate_6_average_field.jl
+++ src/Calculate/Calculate_6_average_field.jl
@@ -2,21 +2,34 @@ export calculate_field_average!
 export Mean
 
 struct Mean{T<:AbstractArray,I<:Integer}
-    velocity::T
-    second_moment::T 
+    value::T
     start::I
     finish::I
 end
 
+
+
+
 function calculate_field_average!(mean_field::Mean,current_velocity,iteration)
     if mean_field.start <= iteration && iteration <= mean_field.finish
-        Δ = Array(current_velocity) .- mean_field.velocity
-        current_average_velocity = (iteration-1)/iteration .* mean_field.velocity .+ Array(current_velocity) ./iteration
-        mean_field.velocity .= current_average_velocity
-        current_second_moment = mean_field.second_moment .+ (Δ .* (Array(current_velocity) .- current_average_velocity)) 
-        mean_field.second_moment .= current_second_moment
+        calculated_mean = (iteration-1)/iteration .* mean_field.value .+ Array(current_velocity) ./iteration
+        mean_field.value .= calculated_mean
     end
     return nothing 
 end
 
 
+
+
+
+
+# function calculate_field_average!(mean_field::Mean,current_velocity,iteration)
+#     if mean_field.start <= iteration && iteration <= mean_field.finish
+#         Δ = Array(current_velocity) .- mean_field.velocity
+#         current_average_velocity = (iteration-1)/iteration .* mean_field.velocity .+ Array(current_velocity) ./iteration
+#         mean_field.velocity .= current_average_velocity
+#         current_second_moment = mean_field.second_moment .+ (Δ .* (Array(current_velocity) .- current_average_velocity)) 
+#         mean_field.second_moment .= current_second_moment
+#     end
+#     return nothing 
+# end

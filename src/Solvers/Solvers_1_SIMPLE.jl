f701ab5675b8212d78941baad52a52a0c88307dd
Humberto Medina <h.medina@aerofluids.org>
2025-08-22 22:51:00 +0100

    IMPORTNAT: undo previous changes to SIMPLE  algorithm  - remember NOT to uncomment  these lines again (makes SIMPLE unstable)

--- src/Solvers/Solvers_1_SIMPLE.jl
+++ src/Solvers/Solvers_1_SIMPLE.jl
@@ -223,9 +223,9 @@ function SIMPLE(
         # new approach
 
         # 1. using velocity from momentum equation
-        interpolate!(Uf, U, config)
-        correct_boundaries!(Uf, U, boundaries.U, time, config)
-        flux!(mdotf, Uf, config)
+        # interpolate!(Uf, U, config)
+        # correct_boundaries!(Uf, U, boundaries.U, time, config)
+        # flux!(mdotf, Uf, config)
         correct_mass_flux(mdotf, p, rDf, config)
         correct_velocity!(U, Hv, ∇p, rD, config)
 

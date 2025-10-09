faa52f486ad619a79319233f6f95565f7e2ee5cc
James <j4m3sknight@gmail.com>
2025-10-09 17:19:36 +0100

    adding tests for the time average and field RMS capabilities

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,7 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 *  High fidelity thermal conductivity models for H2 and N2 [#72](@ref)
 *  `SetFields` utility that allows to set a field to desired value within a box / circle / sphere [#73](@ref)
 *  Helmholtz Energy equation of state and supporting framework for it for H2 and N2 [#75](@ref)
-
+*  Time-average and RMS of Vector and Scalar fields [#78]
 
 ### Fixed
 * The `UNV3D_mesh` reader has been updated to ensure that the ordering of face nodes is determined in a more robust manner. This resolves some issues when loading a `UNV` mesh that is later used to store simulation results in the `OpenFOAM` format [#64](@ref)
---
+++ test/unit_test_field_time_average.jl
@@ -0,0 +1,96 @@
+using XCALibre
+
+grids_dir = pkgdir(XCALibre, "examples/0_GRIDS")
+grid = "quad40.unv"
+
+mesh_file = joinpath(grids_dir, grid)
+mesh = UNV2D_mesh(mesh_file, scale=0.001)
+@test typeof(mesh) <: Mesh2
+
+backend = CPU()
+workgroup = 1024
+mesh_dev = adapt(backend, mesh)
+
+U0 = 0.3
+frequency = 1
+velocity = [2*U0, U0, 0.0]
+A = 0.1
+nu = 1e-3
+Re = velocity[1]*0.1/nu
+
+model = Physics(
+    time = Transient(),
+    fluid = Fluid{Incompressible}(nu=nu),
+    turbulence = RANS{Laminar}(),
+    energy = Energy{Isothermal}(),
+    domain = mesh_dev
+    )
+
+@inline inflow(vec, t, i) = begin
+    u = U0*(1 + A * cospi(2*frequency*t))
+    v = U0*(1+ A * sinpi(2*frequency*t))
+    return velocity = SVector{3}(u, v, 0.0)
+end
+
+BCs = assign(
+    region=mesh_dev,
+    (
+        U = [
+            DirichletFunction(:inlet, inflow),
+            Extrapolated(:outlet),
+            # Wall(:wall, [0.0, 0.0, 0.0]),
+            Symmetry(:bottom, [0.0, 0.0, 0.0]),
+            Symmetry(:top, [0.0, 0.0, 0.0])
+    ],
+        p = [
+            Extrapolated(:inlet),
+            Dirichlet(:outlet, 0.0),
+            # Extrapolated(:wall),
+            Symmetry(:bottom),
+            Symmetry(:top)
+        ]
+    )
+)
+
+schemes = (
+    U = Schemes(time=Euler),
+    p = Schemes()
+)
+
+
+solvers = (
+    U = SolverSetup(
+        solver      = Bicgstab(), # Bicgstab(), Gmres()
+        preconditioner = Jacobi(), # DILU(), TEMPORARY!
+        convergence = 1e-7,
+        relax       = 1.0,
+        rtol = 1e-3
+    ),
+    p = SolverSetup(
+        solver      = Gmres(), #Cg(), # Bicgstab(), Gmres()
+        preconditioner = Jacobi(), #LDL(), TEMPORARY!
+        convergence = 1e-7,
+        relax       = 1.0,
+        rtol = 1e-3
+    )
+)
+iterations = 100
+
+runtime = Runtime(iterations=iterations, time_step=0.05, write_interval=-1)
+hardware = Hardware(backend=backend,workgroup = workgroup)
+
+postprocess = FieldAverage(model.momentum.U;name="Umean",start=51,write_interval=2)
+config = Configuration(solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware, boundaries=BCs,postprocess=postprocess)
+
+@test initialise!(model.momentum.U, velocity) === nothing
+@test initialise!(model.momentum.p, 0.0) === nothing
+foreach(rm, (f for f in readdir() if endswith(f, ".vtk")))
+residuals = run!(model, config);
+
+u_mean_exact = U0
+v_mean_exact = U0
+u_mean = sum(postprocess.mean.x.values[end-30:end-10])/length(postprocess.mean.x.values[end-30:end-10])
+v_mean = sum(postprocess.mean.y.values[end-30:end-10])/length(postprocess.mean.y.values[end-30:end-10])
+
+@test u_mean ≈ u_mean_exact atol = 0.005
+@test v_mean ≈ v_mean_exact atol = 0.05

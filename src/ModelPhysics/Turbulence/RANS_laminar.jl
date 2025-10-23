6730ec4840924a7fdb16829e7a63f949af5116c0
Artem <egyaa20@nottingham.ac.uk>
2025-10-23 23:43:14 +0100

    `Added` appropriate `save_output` dispatch for multiphase solver

--- src/ModelPhysics/Turbulence/RANS_laminar.jl
+++ src/ModelPhysics/Turbulence/RANS_laminar.jl
@@ -103,6 +103,19 @@ function save_output(model::Physics{T,F,SO,M,Tu,E,D,BI}, outputWriter, iteration
 end
 
 function save_output(model::Physics{T,F,SO,M,Tu,E,D,BI}, outputWriter, iteration, time, config
+    ) where {T,F<:Multiphase,SO,M,Tu<:Laminar,E<:Nothing,D,BI}
+
+    args = (
+        ("U", model.momentum.U), 
+        ("p", model.momentum.p),
+        ("alpha", model.fluid.alpha),
+        ("rho", model.fluid.rho),
+        # ("p_rgh", model.fluid.p_rgh)
+    )
+    write_results(iteration, time, model.domain, outputWriter, config.boundaries, args...)
+end
+
+function save_output(model::Physics{T,F,SO,M,Tu,E,D,BI}, outputWriter, iteration, time, config
     ) where {T,F,SO,M,Tu<:Laminar,E<:Nothing,D,BI}
     args = (
         ("U", model.momentum.U), 

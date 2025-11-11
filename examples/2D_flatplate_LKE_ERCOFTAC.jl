389520139df37e572ca5535ebaa2a3029f1607b0
BSmith1402 <bcsmithy1408@hotmail.co.uk>
2025-11-11 13:41:37 +0000

    Activating Skin Friction Functions for Data Comparison

--- examples/2D_flatplate_LKE_ERCOFTAC.jl
+++ examples/2D_flatplate_LKE_ERCOFTAC.jl
@@ -7,7 +7,7 @@ using XCALibre
 # mesh_file = "unv_sample_meshes/cylinder_d10mm_5mm.unv"
 
 grids_dir = pkgdir(XCALibre, "examples", "0_GRIDS")
-grid = "Flatplate_2D_Mesh_BSTest2.unv"
+grid = "EROFATC_Plate_Example.unv"
 mesh_file = joinpath(grids_dir, grid)
 
 mesh = UNV2D_mesh(mesh_file, scale=0.001)
@@ -19,10 +19,10 @@ hardware = Hardware(backend=backend, workgroup=workgroup)
 mesh_dev = adapt(backend, mesh)
 
 # Turbulence Model
-velocity = [0,5.4,0]
+velocity = [5.4,0,0]
 nu = 1.48e-5
-Re = 10*1/nu
-νR = 15
+Re = 2e5
+νR = 13.9
 Tu = 0.03
 k_inlet = 0.0575 #3/2*(Tu*velocity[1])^2
 kL_inlet = 0.0115 #1/2*(Tu*velocity[1])^2
@@ -42,50 +42,50 @@ BCs = assign(
     region=mesh_dev,
     (
         U = [
-            Dirichlet(:Bottom, velocity),
-            Zerogradient(:Top),
-            Wall(:Right, [0.0, 0.0, 0.0]),
-            Extrapolated(:Left)
+            Dirichlet(:Inlet, velocity),
+            Zerogradient(:Outlet),
+            Wall(:Wall, [0.0, 0.0, 0.0]),
+            Extrapolated(:Top)
             # Zerogradient(:top)
             # Symmetry(:top)
         ],
         p = [
-            Zerogradient(:Bottom),
-            Dirichlet(:Top, 0.0),
-            Wall(:Right),
-            Extrapolated(:Left)
+            Zerogradient(:Inlet),
+            Dirichlet(:Outlet, 0.0),
+            Wall(:Wall),
+            Extrapolated(:Top)
             # Zerogradient(:top)
             # Symmetry(:top)
         ],
         k = [
-            Dirichlet(:Bottom, k_inlet),
-            Zerogradient(:Top),
-            Dirichlet(:Right, 0.0),
-            Extrapolated(:Left)
+            Dirichlet(:Inlet, k_inlet),
+            Zerogradient(:Outlet),
+            Dirichlet(:Wall, 0.0),
+            Extrapolated(:Top)
             # Zerogradient(:top)
             # Symmetry(:top)
         ],
         kl = [
-            Dirichlet(:Bottom, kL_inlet),
-            Zerogradient(:Top),
-            Dirichlet(:Right, 0.0),
-            Extrapolated(:Left)
+            Dirichlet(:Inlet, kL_inlet),
+            Zerogradient(:Outlet),
+            Dirichlet(:Wall, 0.0),
+            Extrapolated(:Top)
             # Zerogradient(:top)
             # Symmetry(:top)
         ],
         omega = [
-            Dirichlet(:Bottom, ω_inlet),
-            Zerogradient(:Top),
-            OmegaWallFunction(:Right),
-            Extrapolated(:Left)
+            Dirichlet(:Inlet, ω_inlet),
+            Zerogradient(:Outlet),
+            OmegaWallFunction(:Wall),
+            Extrapolated(:Top)
             # Zerogradient(:top)
             # Symmetry(:top)
         ],
         nut = [
-            Dirichlet(:Bottom, k_inlet/ω_inlet),
-            Extrapolated(:Top),
-            Dirichlet(:Right, 0.0), 
-            Extrapolated(:Left)
+            Dirichlet(:Inlet, k_inlet/ω_inlet),
+            Extrapolated(:Outlet),
+            Dirichlet(:Wall, 0.0), 
+            Extrapolated(:Top)
             # Zerogradient(:top)
             # Symmetry(:top)
         ]
@@ -174,29 +174,29 @@ residuals = run!(model, config); #, pref=0.0) # 9.39k allocs
      display(p)
  end
 
-# using DelimitedFiles
-# using LinearAlgebra
-# using Plots 
-# # OF_data = readdlm("flatplate_OF_wall_kOmega_lowRe.csv", ',', Float64, skipstart=1)
-# # oRex = OF_data[:,7].*velocity[1]./nu[1]
-# # oCf = sqrt.(OF_data[:,12].^2 + OF_data[:,13].^2)/(0.5*velocity[1]^2)
+ using DelimitedFiles
+ using LinearAlgebra
+ using Plots 
+ OF_data = readdlm("flatplate_OF_wall_kOmega_lowRe.csv", ',', Float64, skipstart=1)
+ oRex = OF_data[:,7].*velocity[1]./nu[1]
+ oCf = sqrt.(OF_data[:,12].^2 + OF_data[:,13].^2)/(0.5*velocity[1]^2)
 
-# model_cpu = adapt(CPU(), model)
+ #model_cpu = adapt(CPU(), model)
 
-# tauw, pos = wall_shear_stress(:wall, model_cpu)
-# tauMag = [norm(tauw[i]) for i ∈ eachindex(tauw)]
-# tauMag = [tauw.x[i] for i ∈ eachindex(tauw)]
-# x = [pos[i][1] for i ∈ eachindex(pos)]
-# Rex = velocity[1].*x./nu
+ #tauw, pos = wall_shear_stress(:Right, model_cpu)
+ #tauMag = [norm(tauw[i]) for i ∈ eachindex(tauw)]
+ #tauMag = [tauw.x[i] for i ∈ eachindex(tauw)]
+ #x = [pos[i][1] for i ∈ eachindex(pos)]
+ #Rex = velocity[1].*x./nu
 
-# x_corr = [0:0.0002:2;]
-# Rex_corr = velocity[1].*x_corr/nu
-# Cf_corr = 0.0576.*(Rex_corr).^(-1/5)
-# Cf_laminar = 0.664.*(Rex_corr).^(-1/2)
+ x_corr = [0:0.0002:2;]
+ Rex_corr = velocity[1].*x_corr/nu
+ Cf_corr = 0.0576.*(Rex_corr).^(-1/5)
+ Cf_laminar = 0.664.*(Rex_corr).^(-1/2)
 
-# plot(; xaxis="Rex", yaxis="Cf")
-# plot!(Rex_corr, Cf_corr, color=:red, ylims=(0, 0.01), xlims=(0,6e5), label="Turbulent",lw=1.5)
-# plot!(Rex_corr, Cf_laminar, color=:green, ylims=(0, 0.01), xlims=(0,6e5), label="Laminar",lw=1.5)
+ plot(; xaxis="Rex", yaxis="Cf")
+ plot!(Rex_corr, Cf_corr, color=:red, ylims=(0, 0.01), xlims=(0,6e5), label="Turbulent",lw=1.5)
+ plot!(Rex_corr, Cf_laminar, color=:green, ylims=(0, 0.01), xlims=(0,6e5), label="Laminar",lw=1.5)
 # # plot!(oRex, oCf, color=:green, lw=1.5, label="OpenFOAM") # |> display
 # plot!(Rex,tauMag./(0.5*velocity[1]^2), color=:blue, lw=1.5,label="Code") |> display
 

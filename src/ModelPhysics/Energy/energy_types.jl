cd8884b6731772c3489d22f169078ada5b39e240
Artem <egyaa20@nottingham.ac.uk>
2025-12-18 20:12:04 +0000

    - Changed `Isothermal` energy model to let user optionally define `ConstantScalar` value for temperature field.

--- src/ModelPhysics/Energy/energy_types.jl
+++ src/ModelPhysics/Energy/energy_types.jl
@@ -13,20 +13,21 @@ end
 
 # Isothermal
 
-#### THS IS A DUMMY IMOLEENTATIUO PLEASE SORT OUT ARTEM! TA
-struct Isothermal{T<:AbstractFloat} <: AbstractEnergyModel 
-    T_iso::T
+struct Isothermal{T<:AbstractScalarField} <: AbstractEnergyModel 
+    T::T
 end 
 Adapt.Adapt.@adapt_structure Isothermal
 
-
-# These constructors might need changing
-Energy{Isothermal}() = begin
-    args = nothing
-    ARGS = typeof(args)
-    Energy{Isothermal,ARGS}(args)
+# Set the defualt value unless user specifies it - compatible with other solvers and energy models.
+Energy{Isothermal}(; T=300.0) = begin
+    coeffs = (T=T,)
+    ARG = typeof(coeffs)
+    Energy{Isothermal,ARG}(coeffs)
 end
 
 (energy::Energy{EnergyModel, ARG})(mesh, fluid) where {EnergyModel<:Isothermal,ARG} = begin
-    nothing
+    T_val = energy.args.T
+    
+    T = ConstantScalar(T_val)
+    Isothermal(T)
 end

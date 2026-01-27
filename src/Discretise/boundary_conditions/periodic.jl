f4b3b86c78f73ec197aa76306bc0f27a90798243
Humberto Medina <h.medina@aerofluids.org>
2026-01-27 01:28:57 +0000

    draft implementation of fully implicit `Periodic` boundaries

--- src/Discretise/boundary_conditions/periodic.jl
+++ src/Discretise/boundary_conditions/periodic.jl
@@ -164,36 +164,138 @@ function periodic_matrix_connectivity(BC::PeriodicParent, mesh)
     return i, j
 end
 
+# @define_boundary Union{PeriodicParent,Periodic} Laplacian{Linear} begin
+
+#     phi = term.phi
+#     mesh = phi.mesh 
+#     (; faces, cells) = mesh
+#     values = get_values(phi, component)
+
+#     # determine id of periodic cell and interpolate face value
+#     pfID = bc.value.face_map[i] # id of periodic face 
+#     pface = faces[pfID]
+#     pcellID = pface.ownerCells[1]
+
+#     # for improved accuracy this needs to include the discretisation used for noncorrection
+#     delta1 = face.delta
+#     delta2 = pface.delta
+#     delta = delta1 + delta2
+    
+#     # Retrieve term flux and extract fields from workitem face
+#     (; area, normal) = face
+#     ap = term.sign*(term.flux[fID]*area)/delta
+#     ac = -ap
+#     an = ap
+
+#     # Playing with implicit version
+#     # fzcellID = spindex(rowptr, colval, cellID, pcellID)
+#     # nzval[fzcellID] = an
+#     # ac, 0.0
+
+#     ac, -an*values[pcellID] # explicit this works
+# end
+
 @define_boundary Union{PeriodicParent,Periodic} Laplacian{Linear} begin
 
     phi = term.phi
     mesh = phi.mesh 
     (; faces, cells) = mesh
     values = get_values(phi, component)
+    
+    (; area, normal, e) = face
 
     # determine id of periodic cell and interpolate face value
     pfID = bc.value.face_map[i] # id of periodic face 
     pface = faces[pfID]
     pcellID = pface.ownerCells[1]
+    C1 = cell.centre
+    C2 = cells[pcellID].centre + normal*bc.value.distance
 
     # for improved accuracy this needs to include the discretisation used for noncorrection
-    delta1 = face.delta
-    delta2 = pface.delta
-    delta = delta1 + delta2
+    delta = norm(C1 - C2)
     
-    # Retrieve term flux and extract fields from workitem face
-    (; area, normal) = face
+    # # Use form below to ensure correctness, could be simplified for performance
+    # # Sf = ns*area*normal # original
+    # # e = ns*e # original
+    # Sf = area*normal # original
+    # e = e # original
+    # Ef = ((Sf⋅Sf)/(Sf⋅e))*e # original
+    # Ef_mag = norm(Ef)
+    # ap = term.sign*(term.flux[fID]*Ef_mag)/delta
+
     ap = term.sign*(term.flux[fID]*area)/delta
+    
+    # Increment sparse array
     ac = -ap
     an = ap
 
     # Playing with implicit version
     # fzcellID = spindex(rowptr, colval, cellID, pcellID)
-    # nzval[fzcellID] = an
-    # ac, 0.0
+    # nzval[fzcellID] += an
+    # return ac, 0.0
 
-    ac, -an*values[pcellID] # explicit this works
+    # if typeof(bc) <: PeriodicParent
+    #     # fzcellID = spindex(rowptr, colval, cellID, pcellID)
+    #     # nzval[fzcellID] += an
+    #     # return ac, 0.0
+
+    #     # Periodic cell
+    #     pcell_zID = spindex(rowptr, colval, pcellID, pcellID)
+    #     pneighbour_zID = spindex(rowptr, colval, pcellID, cellID)
+    #     nzval[pcell_zID] += -an
+    #     nzval[pneighbour_zID] += -ac
+
+    #     # Current Parent Cell
+    #     neighbour_zID = spindex(rowptr, colval, cellID, pcellID)
+    #     nzval[neighbour_zID] += an
+    #     return ac, 0.0
+    # else
+        # fzcellID = spindex(rowptr, colval, cellID, pcellID)
+        # nzval[fzcellID] += an
+        # return ac, 0.0
+        # return 0.0, 0.0
+    # end
+    fzcellID = spindex(rowptr, colval, cellID, pcellID)
+    nzval[fzcellID] += an
+    return ac, 0.0
+    # return ac, an
 end
+
+# @define_boundary Union{PeriodicParent,Periodic} Divergence{Linear} begin
+#     phi = term.phi
+#     mesh = phi.mesh 
+#     (; faces, cells) = mesh
+#     values = get_values(phi, component)
+
+#     # determine id of periodic cell and interpolate face value
+#     pfID = bc.value.face_map[i] # id of periodic face 
+#     pface = faces[pfID]
+#     pcellID = pface.ownerCells[1]
+
+
+#     delta1 = face.delta
+#     delta2 = pface.delta
+#     delta = delta1 + delta2
+    
+#     weight = delta2/delta
+#     one_minus_weight = one(eltype(weight)) - weight
+
+#     # Calculate ap value to increment
+#     flux = term.flux[fID]
+#     ap = term.sign*(flux)
+#     ac = weight*ap
+#     an = one_minus_weight*ap
+
+#     # Playing with implicit version
+#     # fzcellID = spindex(rowptr, colval, cellID, pcellID)
+#     # nzval[fzcellID] = an
+#     # ac, 0.0
+#     if typeof(bc) <: PeriodicParent
+#         return ac, -an*values[pcellID] # explicit this works
+#     else
+#         return ac, -an*values[pcellID] # explicit this works
+#     end
+# end
 
 @define_boundary Union{PeriodicParent,Periodic} Divergence{Linear} begin
     phi = term.phi
@@ -206,26 +308,40 @@ end
     pface = faces[pfID]
     pcellID = pface.ownerCells[1]
 
+    # Retrieve mesh centre values
+    f = face.centre
+    C = cell.centre
+    N = cells[pcellID].centre + face.normal*bc.value.distance
 
-    delta1 = face.delta
-    delta2 = pface.delta
-    delta = delta1 + delta2
+    # calculate distance vectors
+    d_fC = C - f 
+    d_fN = N - f
     
-    weight = delta2/delta
+    # Calculate weights using normal functions
+    weight = norm(d_fN)/(norm(d_fC) + norm(d_fN))
     one_minus_weight = one(eltype(weight)) - weight
 
-    # Calculate ap value to increment
-    flux = term.flux[fID]
-    ap = term.sign*(flux)
-    ac = weight*ap
-    an = one_minus_weight*ap
+    # Calculate required increment
+    # ap = term.sign*(term.flux[fID]*ns)
+    ap = term.sign*(term.flux[fID])
+    # ac = ap*one_minus_weight
+    # an = ap*weight
+    ac = ap*weight
+    an = ap*one_minus_weight
+    # return ac, an
 
     # Playing with implicit version
-    # fzcellID = spindex(rowptr, colval, cellID, pcellID)
-    # nzval[fzcellID] = an
-    # ac, 0.0
-
-    ac, -an*values[pcellID] # explicit this works
+    # if typeof(bc) <: PeriodicParent
+    #     fzcellID = spindex(rowptr, colval, cellID, pcellID)
+    #     nzval[fzcellID] += an
+    #     return ac, 0.0
+    # else
+        fzcellID = spindex(rowptr, colval, cellID, pcellID)
+        nzval[fzcellID] += an
+        return ac, 0.0
+        # return 0.0, 0.0
+    # end
+    # ac, -an*values[pcellID] # explicit this works
 end
 
 @define_boundary Union{PeriodicParent,Periodic} Divergence{Upwind} begin

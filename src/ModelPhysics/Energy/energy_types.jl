c3a3cdf78ab560b82802548231db4d436c0ba9b2
Artem <egyaa20@nottingham.ac.uk>
2026-02-18 20:57:35 +0000

    final update

--- src/ModelPhysics/Energy/energy_types.jl
+++ src/ModelPhysics/Energy/energy_types.jl
@@ -1,4 +1,3 @@
-# export AbstractEnergyModel, Energy
 export Energy
 export AbstractEnergyModel
 export Isothermal
@@ -12,22 +11,10 @@ struct Energy{T,ARG}
 end
 
 # Isothermal
+struct Isothermal <: AbstractEnergyModel end
 
-struct Isothermal{T<:AbstractScalarField} <: AbstractEnergyModel 
-    T::T
-end 
-Adapt.Adapt.@adapt_structure Isothermal
-
-# Set the defualt value unless user specifies it - compatible with other solvers and energy models.
-Energy{Isothermal}(; T=300.0) = begin
-    coeffs = (T=T,)
-    ARG = typeof(coeffs)
-    Energy{Isothermal,ARG}(coeffs)
-end
-
-(energy::Energy{EnergyModel, ARG})(mesh, fluid) where {EnergyModel<:Isothermal,ARG} = begin
-    T_val = energy.args.T
-    
-    T = ConstantScalar(T_val)
-    Isothermal(T)
+Energy{Isothermal}() = begin
+    args = nothing
+    ARGS = typeof(args)
+    Energy{Isothermal,ARGS}(args)
 end

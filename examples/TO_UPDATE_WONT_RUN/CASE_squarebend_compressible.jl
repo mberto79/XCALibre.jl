405eff4899712dd708814486e115d5825b52a343
Humberto Medina <h.medina@aerofluids.org>
2026-03-08 10:52:20 +0000

    add square bend test/example case

--- examples/TO_UPDATE_WONT_RUN/CASE_squarebend_compressible.jl
+++ examples/TO_UPDATE_WONT_RUN/CASE_squarebend_compressible.jl
@@ -1,106 +1,128 @@
-using Plots
+# using Plots
 using XCALibre
 # using CUDA # Run this if using NVIDIA GPU
 # using AMDGPU # Run this if using AMD GPU
 
 # quad, backwardFacingStep_2mm, backwardFacingStep_10mm, trig40
-mesh_file = "unv_sample_meshes/OF_squareBend_laminar/constant/polyMesh/"
-mesh = FOAM3D_mesh(mesh_file, scale=1.0, integer_type=Int64, float_type=Float64)
+# mesh_file = "unv_sample_meshes/OF_squareBend_laminar/constant/polyMesh/"
+
+grids_dir = pkgdir(XCALibre, "examples/0_GRIDS")
+grid = "OF_squareBend/polyMesh"
+mesh_file = joinpath(grids_dir, grid)
+mesh = FOAM3D_mesh(mesh_file)
 
 
-# mesh_dev = adapt(CUDABackend(), mesh)
+backend = CPU(); workgroup = 1024
+# backend = CUDABackend(); workgroup = 32 
+# backend = ROCBackend(); workgroup = 32
+
+
+mesh_dev = adapt(backend, mesh)
 
 # Inlet conditions
 
 # Not working
 
-velocity = [400, 0.0, 0.0]
+velocity = [200, 0.0, 0.0]
 noSlip = [0.0, 0.0, 0.0]
 nu = 1e-3
 Re = (0.2*velocity[1])/nu
 gamma = 1.4
 cp = 1005.0
-temp = 300.0
-pressure = 110000.0
+temp = 1000 # 300.0
+pressure = 110000
 Pr = 0.7
 
 model = Physics(
     time = Steady(),
-    fluid = Compressible(
-        mu = nu,
-        cp = ConstantScalar(cp),
-        gamma = ConstantScalar(gamma),
-        Pr = ConstantScalar(Pr)
+    fluid = Fluid{Compressible}(
+    # fluid = Fluid{WeaklyCompressible}(
+        nu = nu,
+        cp = cp,
+        gamma = gamma,
+        Pr = Pr
         ),
     turbulence = RANS{Laminar}(),
-    energy = Energy{SensibleEnthalpy}(Tref = 298.15),
-    domain = mesh
+    energy = Energy{SensibleEnthalpy}(Tref=298.15),
+    domain = mesh_dev
     )
 
-@assign! model momentum U ( 
-    Dirichlet(:inlet, velocity),
-    Neumann(:outlet, 0.0),
-    Wall(:walls, noSlip)
-)
-
-@assign! model momentum p (
-    Dirichlet(:inlet, pressure),
-    Neumann(:outlet, 0.0),
-    Neumann(:walls, 0.0)
-)
-
-@assign! model energy h (
-    FixedTemperature(:inlet, T=300.0, Enthalpy(cp=cp, Tref=298.15)),
-    Neumann(:outlet, 0.0),
-    Neumann(:walls, 0.0)
+boundaries = assign(
+    region = mesh,
+    (
+        U = [
+            Dirichlet(:inlet, velocity),
+            Zerogradient(:outlet),
+            # Extrapolated(:outlet),
+            Wall(:walls, noSlip)
+            # Symmetry(:walls)
+            # Slip(:walls)
+        ],
+        p = [
+            # Dirichlet(:inlet, pressure),
+            Zerogradient(:inlet),
+            # Extrapolated(:outlet),
+            Dirichlet(:outlet, pressure),
+            Wall(:walls)
+            # Slip(:walls)
+        ],
+        h = [
+            FixedTemperature(:inlet, T=temp, Enthalpy(cp=cp, Tref=298.15)),
+            Zerogradient(:outlet),
+            # Extrapolated(:outlet),
+            Wall(:walls)
+            # Slip(:walls)
+        ]
+    )
 )
 
 solvers = (
     U = SolverSetup(
-        model.momentum.U;
         solver      = Bicgstab(), # Bicgstab(), Gmres()
         preconditioner = Jacobi(),
         convergence = 1e-7,
-        relax       = 0.7,
+        relax       = 0.6,
         rtol = 1e-2,
-        atol = 1e-4
+        # atol = 1e-4
     ),
     p = SolverSetup(
-        model.momentum.p;
-        solver      = Bicgstab(), # Bicgstab(), Gmres()
+        solver      = Cg(), # Bicgstab(), Gmres(), Cg() # WeaklyCompressible
+        # solver      = Bicgstab(), # Bicgstab(), Gmres(), Cg() # Compressible
         preconditioner = Jacobi(),
         convergence = 1e-7,
-        relax       = 0.3,
+        relax       = 0.2,
         limit = (1000.0, 1000000.0),
         rtol = 1e-2,
-        atol = 1e-4
+        # atol = 1e-4
     ),
     h = SolverSetup(
-        model.energy.h;
         solver      = Bicgstab(), # Bicgstab(), Gmres()
         preconditioner = Jacobi(),
         convergence = 1e-7,
-        relax       = 0.7,
+        relax       = 0.2,
         # limit = (100.0, 1000.0),
         rtol = 1e-2,
-        atol = 1e-4
+        # atol = 1e-4
     )
 )
 
+divergence = Upwind # Upwind LUST
 schemes = (
-    U = Schemes(divergence=BoundedUpwind),#, gradient=Midpoint),
-    p = Schemes(divergence=Upwind, gradient=Midpoint),
-    h = Schemes(divergence=BoundedUpwind)#, gradient=Midpoint)
+    U = Schemes(divergence=BoundedUpwind),
+    p = Schemes(divergence=Upwind),
+    h = Schemes(divergence=BoundedUpwind)
+
+    # U = Schemes(divergence=divergence),
+    # p = Schemes(divergence=divergence),
+    # h = Schemes(divergence=divergence)
 )
 
-runtime = Runtime(iterations=5, write_interval=1, time_step=1)
+runtime = Runtime(iterations=1000, write_interval=50, time_step=1)
 
-hardware = Hardware(backend=CPU(), workgroup=4)
-# hardware = Hardware(backend=CUDABackend(), workgroup=32)
-# hardware = Hardware(backend=ROCBackend(), workgroup=32)
+hardware = Hardware(backend=backend, workgroup=workgroup)
 
-config = Configuration(
-    solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware, boundaries=BCs)
+config = Configuration(;
+    solvers, schemes, runtime, hardware, boundaries)
 
 GC.gc(true)
 
@@ -108,10 +130,12 @@ initialise!(model.momentum.U, [0.0, 0.0, 0.0])
 initialise!(model.momentum.p, pressure)
 initialise!(model.energy.T, temp)
 
-residuals = run!(model, config); #, pref=0.0)
+residuals = run!(model, config, output=VTK()); #, pref=0.0)
 
-plot(; xlims=(0,runtime.iterations), ylims=(1e-8,0))
-plot!(1:length(Rx), Rx, yscale=:log10, label="Ux")
-plot!(1:length(Ry), Ry, yscale=:log10, label="Uy")
-plot!(1:length(Rp), Rp, yscale=:log10, label="p")
-plot!(1:length(Rh), Rh, yscale=:log10, label="h")
+
+
+# plot(; xlims=(0,runtime.iterations), ylims=(1e-8,1))
+# plot!(1:length(residuals.Ux), residuals.Ux, yscale=:log10, label="Ux")
+# plot!(1:length(residuals.Uy), residuals.Uy, yscale=:log10, label="Uy")
+# plot!(1:length(residuals.p), residuals.p, yscale=:log10, label="p")
+# plot!(1:length(residuals.e), residuals.e, yscale=:log10, label="h")

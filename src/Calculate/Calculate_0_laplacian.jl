cd4f9edee8a221e64ff0dd44259a27536b990e43
Humberto Medina <h.medina@aerofluids.org>
2026-05-09 15:34:47 +0100

    implement surface gradient to calculate laplacian as div(snGrad)

--- src/Calculate/Calculate_0_laplacian.jl
+++ src/Calculate/Calculate_0_laplacian.jl
@@ -1,4 +1,40 @@
-export laplacian!
+export laplacian!, surface_gradient!
+
+function surface_gradient!(gradf, phif, phi, BCs, time, config)
+    interpolate!(phif, phi, config)
+    correct_boundaries!(phif, phi, BCs, time, config)
+
+    mesh = phi.mesh
+    (; hardware) = config
+    (; backend, workgroup) = hardware
+
+    ndrange = length(mesh.faces)
+    n_bfaces = length(mesh.boundary_cellsID)
+    kernel! = _surface_gradient!(_setup(backend, workgroup, ndrange)...)
+    kernel!(gradf, phif, phi, mesh.faces, mesh.boundary_cellsID, n_bfaces)
+end
+
+@kernel function _surface_gradient!(gradf, phif, phi, faces, boundary_cellsID, n_bfaces)
+    fID = @index(Global)
+
+    @uniform begin
+        gx, gy, gz = gradf.x, gradf.y, gradf.z
+    end
+
+    @inbounds begin
+        face = faces[fID]
+        (; ownerCells, normal, delta) = face
+        snGrad = if fID <= n_bfaces
+            (phif[fID] - phi[boundary_cellsID[fID]]) / delta
+        else
+            (phi[ownerCells[2]] - phi[ownerCells[1]]) / delta
+        end
+
+        gx[fID] = snGrad * normal[1]
+        gy[fID] = snGrad * normal[2]
+        gz[fID] = snGrad * normal[3]
+    end
+end
 
 function laplacian!(phi_out, phif_in ,phi_in, BCs, time, config; disp_warn=true)
     interpolate!(phif_in, phi_in, config)
